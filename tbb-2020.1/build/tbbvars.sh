629712be135f3036f0d7e684202c4c2bc432b882
visionadmin <postmaster@wuhetech.com>
2020-11-17 18:51:52 +0800

    tbb

---
+++ tbb-2020.1/build/tbbvars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export TBBROOT="/home/hanyu/tbb-2020.1/build" #
+tbb_bin="/home/hanyu/tbb-2020.1/build" #
+if [ -z "$CPATH" ]; then #
+    export CPATH="${TBBROOT}/include" #
+else #
+    export CPATH="${TBBROOT}/include:$CPATH" #
+fi #
+if [ -z "$LIBRARY_PATH" ]; then #
+    export LIBRARY_PATH="${tbb_bin}" #
+else #
+    export LIBRARY_PATH="${tbb_bin}:$LIBRARY_PATH" #
+fi #
+if [ -z "$LD_LIBRARY_PATH" ]; then #
+    export LD_LIBRARY_PATH="${tbb_bin}" #
+else #
+    export LD_LIBRARY_PATH="${tbb_bin}:$LD_LIBRARY_PATH" #
+fi #
+ #
---
+++ tbb-2020.1/release_path.sh
@@ -0,0 +1,1 @@
+cd ./build/macos_intel64_clang_cc12.0.0_os10.15.7_release
