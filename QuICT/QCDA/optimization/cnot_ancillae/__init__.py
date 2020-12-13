fcdddcd0cd8e30165b9c23533487a8daab7736b9
visionadmin <postmaster@wuhetech.com>
2020-12-13 21:44:54 +0800

    qcda

--- QuICT/QCDA/optimization/cnot_ancillae/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf8 -*-
-# @TIME    : 2020/11/5 8:54 上午
-# @Author  : Han Yu
-# @File    : __init__.py
-
-from .cnot_ancillae import CnotAncillae
--- QuICT/QCDA/optimization/topological_cnot_rz/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf8 -*-
-# @TIME    : 2020/11/5 8:59 上午
-# @Author  : Han Yu
-# @File    : __init__.py
-
-from .topological_cnot_rz import TopologicalCnotRz
