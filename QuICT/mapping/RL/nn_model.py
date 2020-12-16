33207179e62d33be059e34f337efabb14784dca0
Jia Zhang <zhangjia@microsoft.com>
2020-12-16 21:20:32 +0800

    refine header and experience pool

--- QuICT/mapping/RL/nn_model.py
+++ QuICT/mapping/RL/nn_model.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @Time    :   2020/12/16 20:52:36
+# @Author  :   Jia Zhang 
+# @Contact :   jialrs.z@gmail.com
+# @File    :   nn_model.py
+
 import torch.nn as nn
 import torch.nn.functional as F
 class SparseGATLayer(nn.Module):
