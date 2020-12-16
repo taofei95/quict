f50d3f855cdad7873ce682305b8ee8f84c22ed16
shoulifu <871840351@qq.com>
2020-12-16 17:02:51 +0800

    a rough version of MCTS

--- QuICT/mapping/_mcts_base.py
+++ QuICT/mapping/_mcts_base.py
@@ -1,93 +1,318 @@
-import numpy as np
 #from collections import defaultdict
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Dict
+from copy import copy, deepcopy
+
 from QuICT.models import *
+from QuICT.models._gate import *
+from QuICT.exception import *
 
 import numpy as np
 import networkx as nx
-class Dag:
-    instances_count = 0
-    def __init__(self, circuit : Circuit):
+
+class MappingLayoutException(Exception):
+    """
+
+    """
+    def __init__(self):
+        string = str("The control and target qubit of the gate is not adjacent on the physical device")
+        Exception.__init__(self, string)
+
+class DAG:
+    def __init__(self, circuit: Circuit = None):
         if circuit is not None:
-            self.dag = _transform_from_circuit(circuit = circuit)
-        else 
+            self.dag = self._transform_from_circuit(circuit = circuit)
+        else:
             self.dag = nx.DiGraph()
     
     def get_front_layer(self):
         pass
+  
+    def get_successor_nodes(self, vertex: int):
+        pass
+
+    def get_node_attributes(self, vertex: int):
+        pass
 
     def read_from_qasm(self, file_name :str):
         pass
-    def _tranform_from_circuit(self, circuit : Circuit):
-        pass
 
+    def _transform_from_circuit(self, circuit : Circuit):
+        return nx.DiGraph()
+
+class CouplingGraph:
+    def __init__(self, coupling_graph: List[Tuple] = None):
+        if coupling_graph is not None:
+            self._coupling_graph = self._transform_from_list(coupling_graph = coupling_graph)
+        else:
+            self._coupling_graph = nx.Graph()
+        self._shortest_path = self._cal_shortest_path()
+        
+    @property
+    def coupling_graph(self):
+        """
+        """
+        return self._coupling_graph
+
+    @property
+    def shortest_path(self):
+        """
+        """
+        return self._shortest_path
+    
+    @property
+    def is_adjacent(self, vertex_i: int, vertex_j: int) -> bool:
+        """
+
+        """
+        if self._coupling_graph[vertex_i, vertex_j] is not None:
+            return True
+        else:
+            return False
+
+    def _transform_from_list(self,  coupling_graph : List[Tuple]) -> nx.Graph:
+        """
+        """
+        res_graph = nx.Graph()
+
+        return res_graph
+
+    def _cal_shortest_path(self):
+        """
+        """
+        res = Tuple[List[int]]
+        return res
+    
 
 class MCTSNode:
-    def __init__(self, circuit: Dag, layout: List[int], parent=None):
+
+    def __init__(self, circuit: DAG = None, front_layer: List[int] = None, layout: List[int] = None, 
+                 parent: MCTSNode =None):
         """
         Parameters
         ----------
         circuit : 
         parent : 
         """
-        self.circuit = circuit
-        self.layout = layout
-        self.parent = parent
+        #self._physical_circuit: Circuit  
+        self._circuit = circuit
+        self._front_layer = front_layer
+        self._layout = layout
+        # self._coupling_graph = coupling_graph
+        self._parent = parent
         #self.epsilon = epsilon
-        self.children = []
-        self.front_layer = []
-        self.excution_list = []
-        self.candidate_swap_list = []
-        
-        
+        self._children: List[Tuple[MCTSNode, SwapGate]] = [] 
+        #self._front_layer = []
+        if circuit is not None and layout is not None:
+            self._excution_list = []
+            self._front_layer = []
+            self._excution_list, self._front_layer = self._get_excution_list()
+            self._candidate_swap_list = self._get_candidate_swap_list()
+        else:
+            self._excution_list = []
+            self._front_layer = []
+            self._candidate_swap_list = []
 
-        self.v = 0
-        self.q = 0
+        self._visit_count = 0 
+        self._NNC = None
+        self._c = 0
+        self._value = 0
+        self._reward = 0
+        self._v = 0
+        self._q = 0
     
+    @property
+    def circuit(self):
+        """
+        """
+        return self._circuit
+    @property
+    def reward(self):
+        """
+        """
+        return self._reward
+    
+    @property
+    def value(self):
+        """
+        """
+        return self._value
+
     @property
     def q(self):
         """
         score of the current node
         """
-        pass
-
+        return self._q
+    
 
     @property
     def v(self):
         """
         value of the current node. v = argmax(q)
         """
-        pass
+        return self._v
+    @property
+    def c(self):
+        """
+        value of the current node. v = argmax(q)
+        """
+        return self._c
+
+    @property
+    def front_layer(self):
+        """
+
+        """
+        return self._front_layer
+
+    @property 
+    def excution_list(self):
+        """
+        """
+        return self._excution_list
+
+    @property
+    def candidate_swap_list(self):
+        """
+        """
+        return self._candidate_swap_list
+
+    # @property
+    # def logical_circuit(self):
+    #     """
+    #     logic circuit corresponding to the current node
+    #     """
+    #     return self._logical_circuit
+
+    # @property
+    # def physical_circuit(self):
+    #     """
+    #     """
+    #     return self._physical_circuit
+
+    # @property
+    # def coupling_graph(self):
+    #     """
+    #     """
+    #     return self._coupling_graph
+    
+    @property
+    def layout(self):
+        """
+        the layout of physical qubits to logical qubits
+        """
+        return self._layout
+
+    @layout.setter
+    def layout(self, layout: List[int]):
+        """
+        """
+        self._layout = layout
 
 
     @property
-    def circuit(self):
+    def visit_count(self):
         """
-        logic circuit corresponding to the current node
+        the number of  times the node has been visted
         """
-        pass
+        return self._visit_count
+    @property
+    def parent(self):
+        """
 
+        """
+        return self._parent
 
+    @property
+    def children(self):
+        """
 
-    def _add_child_node(self, swap: BasicGate):
+        """
+        return self._children
+
+    @property
+    def upper_confidence_bound(self):
+        return self.q + self.v + self.c* np.sqrt(np.log2(self.parent.visit_count)/ self.visit_count)
+    
+    def add_child_node(self, swap: SwapGate, coupling_graph: CouplingGraph):
         """
         add a child node by applying the swap gate
         """
-        pass
+        if isinstance(swap, SwapGate):
+            l_control = swap.carg
+            l_target = swap.targ
+            p_control = self.layout[l_control]
+            p_target  = self.layout[l_target]
+            if coupling_graph.is_adjacent(p_control, p_target):
+                layout = self.layout
+                layout[l_control] = p_target
+                layout[l_target] = p_control
+                child_node = MCTSNode(circuit = self.circuit, front_layer = self.front_layer, layout = layout, parent = self)
+                self._children.append(child_node)
+            else:
+                raise MappingLayoutException()
+        else:
+            raise TypeException("swap gate","other gate")
+        
     
+    def neareast_neighbour_count(self, coupling_graph: CouplingGraph)-> int:
+        """
+        """
+        if self._NNC is None:
+            res: int = 0
+
+            self._NNC = res
+            return self._NNC
+        else:
+            return self._NNC
+
 
     def is_leaf_node(self):
         """
         
         """
-        pass
-
-    def _best_child(self, **params):
+        if len(self.children):
+            return False
+        else:
+            return True
+            
+    def is_terminal_node(self):
         """
 
         """
-        pass
+        if len(self.front_layer):
+            return False
+        else:
+            return True
+        
+
+    def select_next_child(self):
+        """
+        return the child_node with highest UCB score
+        """
+        score = -1 
+        node = MCTSNode()
+        for child in self._children:
+            if child[0].upper_confidence_bound > score:
+                node = child[0] 
+                score = node.upper_confidence_bound
+        return  node  
+    
+    def decide_best_child(self):
+        """
+        return the child_node with highest v value and its corresponding swap operation
+        """
+        node = MCTSNode()
+        res_swap =  BasicGate()
+        score = -1
+        for child in self._children:
+            if child[0].v >score:
+                node = child[0]
+                score = node.v
+                res_swap = child[1]
+
+        return node, res_swap
 
 
     def _get_candidate_swap_list(self):
@@ -96,13 +321,13 @@ class MCTSNode:
         """
         pass 
     
-    def _get_excution_list(self):
+    def _get_excution_list(self, coupling_graph: CouplingGraph)-> List[BasicGate]:
         """
         the gates that can be excuted immediately  with the qubit layout
         """
-        pass
+        
 
-    def _update_node(self):
+    def update_node(self):
         """
         remove the gates in excution list and update the circuit dag of the node
         """
@@ -110,8 +335,10 @@ class MCTSNode:
 
 
 
+
+
 class MCTSBase:
-    @abstractmethod
+
     def __init__(self, coupling_graph:List[Tuple] = None,**params):
         """
         initialize the Monte Carlo tree search with the given parameters 
@@ -121,47 +348,49 @@ class MCTSBase:
 
 
     @abstractmethod
-    def search(self, root_node : MCTSNode):
+    def search(self, cur_node : MCTSNode):
         """
         """
         pass
 
     @abstractmethod
-    def _expand(self):
+    def _expand(self, cur_node : MCTSNode):
         """
         open all child nodes of the  current node by applying the swap gates in candidate swap list
         """
         pass
 
     @abstractmethod
-    def _rollout(self, method: str):
+    def _rollout(self, cur_node : MCTSNode, method: str):
         """
         complete a heuristic search from the current node
         """
         pass
     
     @abstractmethod
-    def _backpropagate(self, reward: float):
+    def _backpropagate(self, cur_node : MCTSNode):
         """
         use the result of the rollout to update the score in the nodes on the path from the current node to the root 
         """
         pass
 
     @abstractmethod
-    def _select(self):
+    def _select(self, cur_node : MCTSNode):
         """
         select the child node with highest score
         """
         pass
     @abstractmethod
-    def _eval(self):
+    def _eval(self, cur_node : MCTSNode):
         """
         evaluate the vlaue of the current node by DNN method
         """
         pass
 
-    def _cal_shortest_path(self.coupling_graph):
+    @abstractmethod 
+    def _decide(self,cur_node : MCTSNode):
         """
+
         """
         pass
 
---
+++ QuICT/mapping/table_based_mcts.py
@@ -0,0 +1,112 @@
+from  ._mcts_base import  *
+
+class TableBasedMCTS(MCTSBase):
+    def __init__(self, play_times: int =20, mode: str = None, mode_sim :List[int] = None,
+                selection_times: int = 50, coupling_graph : List[Tuple] = None,  **params):
+        super.__init__(coupling_graph)
+        self.play_times = play_times 
+        self.mode = mode
+        self.mode_sim = mode_sim
+        self.selection_times = selection_times 
+    
+    @property
+    def physical_circuit(self):
+        """
+        the physical circuit of the current root node
+        """
+        return self._root_node.circuit
+
+    @property
+    def root_node(self):
+        """
+
+        """
+        return self._root_node
+    
+    def search(self, circuit: Circuit, layout: List[int]):
+        self._logical_circuit = DAG(circuit = circuit) 
+        self._physical_circuit =Circuit(circuit.wires)
+        self._root_node = MCTSNode(circuiy = self._logical_circuit ,front_layer = self._logical_circuit.get_front_layer(),
+                                  layout = layout)
+        
+        self._add_excutable_gates(self.root_node)
+        self._expand(node = self._root_node)
+        while self._root_node.is_terminal_node():
+            self._search(root_node = self._root_node)
+            self._root_node, swap_gate = self._decide(node = self._root_node)
+            self._physical_circuit.append(swap_gate)
+
+    def _search(self, root_node : MCTSNode):
+        for i in range(self.selection_times):
+            cur_node = self._select(node = root_node)
+            self._expand(node = cur_node)
+            self._rollout(node = cur_node, method = "random")
+            self._backpropagate(cur_node)
+
+    def _select(self, node : MCTSNode):
+        """
+        select the child node with highes score to expand
+        """
+        cur_node = node 
+        while cur_node.is_leaf_node() is not True:
+            cur_node = cur_node.get_best_child()
+        return cur_node
+
+    def _expand(self, node : MCTSNode):
+        """
+        open all child nodes of the  current node by applying the swap gates in candidate swap list
+        """
+        for swap in node.candidate_swap_list:
+            node.add_child_node(swap)
+            
+     
+
+    def _rollout(self, node : MCTSNode, method: str):
+        """
+        complete a heuristic search from the current node
+        """
+        if method == "random":
+            pass
+
+    
+    def _backpropagate(self,node : MCTSNode):
+        """
+        use the result of the rollout to update the score in the nodes on the path from the current node to the root 
+        """
+        cur_node = node
+        while cur_node.parent is not None:
+            cur_node.update_node()
+            cur_node = cur_node._parent
+
+
+
+    def _eval(self, node : MCTSNode):
+        """
+        evaluate the vlaue of the current node by DNN method
+        """
+        pass
+
+    def _decide(self,node : MCTSNode):
+        """
+        decide which child node to move into 
+        """
+        return node.decide_best_child() 
+
+    def _add_excutable_gates(self, node: MCTSNode):
+        excution_list = self._root_node.excution_list()
+        self._physical_circuit.extend(excution_list)
+
+    def _random_simulation(self, node: MCTSNode, ):
+        pass
+        
+    
+    
+    
+
+
+        
+
+
+    
+
+
