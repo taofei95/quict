407d7d31fe246b43b585333a61d784c288b2fc78
visionadmin <postmaster@wuhetech.com>
2020-12-18 08:13:14 +0800

    add document

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+project = 'QuICT'
+
+extensions = ['sphinx.ext.autodoc', 'm2r', 'recommonmark']
+numpydoc_class_members_toctree = False
+master_doc = 'index'
+html_theme = "classic"
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown'
+}
+
