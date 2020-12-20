4d25351cfaeafc18a78adb38cf77a26718d99aca
Jia Zhang <zhangjia@microsoft.com>
2020-12-20 16:40:36 +0800

    "Refine experience_pool and modify rl_based_mcts"

--- QuICT/qcda/mapping/RL/experience_pool.py
+++ QuICT/qcda/mapping/RL/experience_pool.py
@@ -9,16 +9,16 @@ import numpy as np
 class ExperiencePool(object):
     def __init__(self, max_capacity = 20000):
         self.max_capacity = max_capacity
-        self._father_list = []
-        self._children_list = []
+        self._state_list = []
+        self._next_states_list = []
         self._rewards_list = []
         self._sim_val = []
 
-    def push(self, father, children, rewards, sim_val = 0):
-        while len(self._father_list) >= self.max_capacity:
+    def push(self, state, next_states, rewards, sim_val = 0):
+        while len(self._state_list) >= self.max_capacity:
             self.pop() # Maybe to pop random one instead of the first one
-        self._father_list.append(father)
-        self._children_list.append(children)
+        self._state_list.append(state)
+        self._next_states_list.append(next_states)
         self._rewards_list.append(rewards)
         self._sim_val.append(sim_val)
 
@@ -26,11 +26,11 @@ class ExperiencePool(object):
         self._sim_val[index] = sim_val
 
     def pop(self, index = 0):
-        self._father_list.pop(index)
+        self._state_list.pop(index)
 
     def get_batch(self, batch_size = 32):
-        indices = np.random.choice(range(len(self._father_list), batch_size))
-        return np.choose(indices, self._father_list), \
-               np.choose(indices, self._children_list), \
+        indices = np.random.choice(range(len(self._state_list), batch_size))
+        return np.choose(indices, self._state_list), \
+               np.choose(indices, self._next_states_list), \
                np.choose(indices, self._rewards_list), \
                np.choose(indices, self._sim_val)
