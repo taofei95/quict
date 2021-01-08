032f664df6fc3b61ddc0c29e135cd3ee5bb75f89
visionadmin <postmaster@wuhetech.com>
2021-01-08 15:39:05 +0800

    Rzz debug

--- example/python/Rzz.py
+++ example/python/Rzz.py
@@ -2,4 +2,16 @@
 # -*- coding:utf8 -*-
 # @TIME    : 2021/1/8 3:29 下午
 # @Author  : Han Yu
-# @File    : Rzz.py.py
+# @File    : Rzz.py
+
+import numpy as np
+
+from QuICT import *
+from QuICT.algorithm import *
+
+circuit = Circuit(2)
+H | circuit
+RZZ(np.pi / 2) | circuit
+Phase(np.pi / 4) | circuit(0)
+amplitude = Amplitude.run(circuit)
+print(amplitude)
