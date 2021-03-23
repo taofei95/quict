c279a810e6b414aca4d16dc9f73b1bbf3323cf5a
GaryChan1997 <chenzihang15@mails.ucas.ac.cn>
2021-03-23 20:13:30 +0800

    comments

--- QuICT/qcda/synthesis/unitary_transform/two_qubit_transform.py
+++ QuICT/qcda/synthesis/unitary_transform/two_qubit_transform.py
@@ -4,6 +4,22 @@ Decomposition of SU(4) with Cartan KAK Decomposition
 
 from .._synthesis import Synthesis
 
+class CartanKAKDecomposition:
+    """Cartan KAK Decomposition in SU(4)
+    ∀ U∈SU(4), ∃ KL0, KL1, KR0, KR1∈SU(2), a, b, c∈ℝ, s.t.
+    U = (KL0⊗KL1).exp(i(a XX + b YY + c ZZ)).(KR0⊗KR1)
+
+    Proof of this proposition in general cases is too 'mathematical' even for TCS researchers.
+    [2] gives a proof for U(4), which is a little more friendly for researchers without 
+    mathematical background. However, be aware that [2] uses a different notation.
+
+    The process of this part is taken from [1].
+    Reference:
+        [1] arxiv.org/abs/0806.4015
+        [2] arxiv.org/abs/quant-ph/0507171
+    """
+
+
 class TwoQubitTransform(Synthesis):
     """
     Decompose a matrix U∈SU(4) with Cartan KAK Decomposition to 
@@ -11,6 +27,7 @@ class TwoQubitTransform(Synthesis):
 
     Reference:
         arxiv.org/abs/0806.4015
+        arxiv.org/abs/quant-ph/0308006
     """
     def __call__(self, matrix, eps=1e-15):
         """
@@ -27,7 +44,7 @@ class TwoQubitTransform(Synthesis):
     def build_gate(self):
         """
         Return:
-            circuit(Circuit): Decomposed circuit
+            Tuple(gates): Decomposed gates
         """
         matrix = self.pargs[0]
         eps = self.pargs[1]
