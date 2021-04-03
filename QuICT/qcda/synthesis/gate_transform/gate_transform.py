ad4905cda16e8c10573205efa6f98a7198dfc027
visionadmin <postmaster@wuhetech.com>
2021-04-04 00:37:52 +0800

    add transform process

--- QuICT/qcda/synthesis/gate_transform/gate_transform.py
+++ QuICT/qcda/synthesis/gate_transform/gate_transform.py
@@ -4,12 +4,30 @@
 # @Author  : Han Yu
 # @File    : gate_transform.py
 
+import numpy as np
+
 from .._synthesis import Synthesis
 from .instruction_set import InstructionSet
-from .special_set import IBMQSet
+from .special_set import USTCSet
 from .transform_rule import TransformRule
 
-def GateTransformModel(circuit, instruction_set = IBMQSet):
+from QuICT.core import *
+
+def _two_qubit_transform(source, instruction_set):
+    """ transform source gate into target gate with function
+
+    if function is None, find the default rule
+
+    Args:
+        source(BasicGate): the source gate
+        instruction_set(InstructionSet): the target instruction set
+
+    Returns:
+        TransformRule: the gate list which contains only 2-qubit gates in target instruction set and one qubit gates
+    """
+    return instruction_set.select_transform_rule(source)
+
+def GateTransformModel(circuit, instruction_set = USTCSet):
     """ equivalently transfrom circuit into goal instruction set
 
     The algorithm will try two possible path, and return a better result:
@@ -23,7 +41,29 @@ def GateTransformModel(circuit, instruction_set = IBMQSet):
     Returns:
         circuit(Circuit): the equivalent circuit with goal instruction set
     """
-    pass
+    gateSet = GateSet(circuit.gates, with_copy = False)
 
+    # trans 2-qubits gate
+    gateSetStep1 = GateSet()
+    for gate in gateSet:
+        if gate.targets + gate.controls > 2:
+            raise Exception("gate_transform only support 2-qubit and 1-qubit gate now.")
+        if gate.targets + gate.controls == 2:
+            rule = _two_qubit_transform(gate, self)
+            gateSetStep1.extend(rule.transform(gate))
+        else:
+            gateSetStep1.append(gate)
+
+    # trans one qubit gate
+    gateSetStep2 = GateSet()
+    unitaries = [np.identity(2, dtype=np.complex) for i in range(circuit.circuit_width())]
+    for gate in gateSetStep1:
+        if gate.targets + gate.controls == 2:
+            gateSetStep2.extend(instruction_set.SU2_rule.transform(unitaries[gate.targ]))
+            gateSetStep2.extend(instruction_set.SU2_rule.transform(unitaries[gate.carg]))
+            unitaries[gate.targ] = np.identity(2, dtype=np.complex)
+            unitaries[gate.carg] = np.identity(2, dtype=np.complex)
+    for i in range(circuit.circuit_width()):
+        gateSetStep2.extend(instruction_set.SU2_rule.transform(unitaries[i]))
 
 GateTransform = Synthesis(GateTransformModel)
