61026057adb6b6a3226195f77d5babf391dde661
visionadmin <postmaster@wuhetech.com>
2021-04-13 11:06:14 +0800

    remove files

--- QuICT/qcda/synthesis/gate_transform/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf8 -*-
-# @TIME    : 2021/3/20 4:22 下午
-# @Author  : Han Yu
-# @File    : __init__.py
-
-from .instruction_set import InstructionSet
-from .gate_transform import GateTransform
-from .transform_rule import *
--- QuICT/qcda/synthesis/gate_transform/transform_rule/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf8 -*-
-# @TIME    : 2021/3/20 4:24 下午
-# @Author  : Han Yu
-# @File    : __init__.py
-
-from .transform_rule import TransformRule
-from .SU2_rules import *
-from .two_qubit_gate_rules import *
