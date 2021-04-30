be55d4108483ba971cd8bddf2d97f48146570ee6
likaiqi <yinyangshiqiqiaiai@163.com>
2021-04-30 21:07:38 +0800

    add docs, add gpuarray generator

--- QuICT/ops/linalg/gpu_calculator.py
+++ QuICT/ops/linalg/gpu_calculator.py
@@ -90,31 +90,50 @@ TENSOR_MATRIX_TEMPLATE = SourceModule(r"""
 
 
 class GPUCalculator:
+    """ Based matrix algorithms for running in GPU. """
     @staticmethod
     def dot(A, B):
+        """ dot matrix A and matrix B
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            B(np.array<np.complex>): the matrix B
+
+        Returns:
+            np.array<np.complex>: A * B
+        """
         row_a, col_a = A.shape
         row_b, col_b = B.shape
         assert(col_a == row_b)
 
         gpu_A = gpuarray.to_gpu(A)
         gpu_B = gpuarray.to_gpu(B)
-        gpu_out = gpuarray.to_gpu(np.zeros([row_a, col_b], dtype=np.complex_))
+        gpu_out = gpuarray.zeros((row_a, col_b), dtype=np.complex_)
         gpu_size = gpuarray.to_gpu(np.array([row_a, col_a, row_b, col_b], dtype=np.int32))
 
         gpu_dot = DOT_TEMPLATE.get_function("dot")
 
-        block = (min(row_a, 64), min(col_b, 64), 1)
+        block = (min(row_a, 32), min(col_b, 32), 1)
         gpu_dot(gpu_out, gpu_A, gpu_B, gpu_size, block=block)
 
         return gpu_out.get()
 
     @staticmethod
     def tensor(A, B):
+        """ tensor A and B
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            B(np.array<np.complex>): the matrix B
+
+        Returns:
+            np.array<np.complex>: the tensor result A ⊗ B
+        """
         row_a, col_a = A.shape
         row_b, col_b = B.shape
         gpu_A = gpuarray.to_gpu(A)
         gpu_B = gpuarray.to_gpu(B)
-        gpu_out = gpuarray.to_gpu(np.zeros([row_a*row_b, col_a*col_b], dtype=np.complex_))
+        gpu_out = gpuarray.zeros((row_a*row_b, col_a*col_b), dtype=np.complex_)
         gpu_size = gpuarray.to_gpu(np.array([row_a, col_a, row_b, col_b], dtype=np.int32))
 
         gpu_tensor = TENSOR_TEMPLATE.get_function("tensor")
@@ -127,9 +146,19 @@ class GPUCalculator:
 
     @staticmethod
     def matrix_tensor(A, n, m):
+        """ tensor I^n and A and I^m
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            n(int): the index of indentity
+            m(int): the index of indentity
+
+        Returns:
+            np.array<np.complex>: the tensor result I^n ⊗ A ⊗ I^m
+        """
         row_a, col_a = A.shape
         gpu_A = gpuarray.to_gpu(A)
-        gpu_out = gpuarray.to_gpu(np.zeros([n*m*A.shape[0], n*m*A.shape[1]], dtype=np.complex_))
+        gpu_out = gpuarray.zeros((n*m*A.shape[0], n*m*A.shape[1]), dtype=np.complex_)
         gpu_size = gpuarray.to_gpu(np.array([row_a, col_a, n, m], dtype=np.int32))
 
         block = (min(row_a, 32), min(col_a, 32), 1)
@@ -140,6 +169,15 @@ class GPUCalculator:
 
     @staticmethod
     def vector_permutation(A, mapping, inplace):
+        """ permutaion A with mapping, inplace
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            mapping(list<int>): the qubit mapping
+            inplace(bool): whether changes in A
+        Returns:
+            np.array<np.complex>: the result of Permutation
+        """
         mapping = np.array(mapping, dtype=np.int64)
 
         if not A.shape[0] == 1 << mapping.shape[0]:
@@ -156,6 +194,14 @@ class GPUCalculator:
 
     @staticmethod
     def matrix_permutation(A, mapping, inplace):
+        """ permute mat with mapping, inplace
+
+        Args:
+            mat: Matrix to be permuted.
+            mapping: An array-like object indicating bit ordering.
+            changeInput: Whether change the input matrix.
+
+        """
         mapping = np.array(mapping, dtype=np.int64)
 
         if not A.shape[0] == 1 << mapping.shape[0]:
