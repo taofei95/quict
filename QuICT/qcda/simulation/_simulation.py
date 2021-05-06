4584de5c95fbc3a97d70ac49e2a754f109661753
visionadmin <postmaster@wuhetech.com>
2021-05-06 20:15:31 +0800

    simulation1

--- QuICT/qcda/simulation/_simulation.py
+++ QuICT/qcda/simulation/_simulation.py
@@ -4,8 +4,39 @@
 # @Author  : Han Yu
 # @File    : _simulation
 
+import numpy as np
+
+from QuICT.core import *
+from QuICT.ops.linalg import multiply
+
 class BasicSimulator(object):
 
-    def simulate(self, circuit):
-        pass
+    @staticmethod
+    def pretreatment(self, circuit):
+        """
+        :param
+            circuit(Circuit): the circuit needs pretreatment.
+        :return:
+            CompositeGate: the gates after pretreatment
+        """
+        gates = CompositeGate()
+        gateSet = [np.identity(2, dtype=np.complex) for _ in range(circuit.circuit_width())]
+        tangle = [i for i in range(circuit.circuit_width())]
+        for gate in circuit.gates:
+            if gate.target + gate.controls >= 3:
+                raise Exception("only support 2-qubit gates and 1-qubit gates.")
+            if gate.target + gate.controls == 1:
+                target = gate.target
+                if tangle[target] == target:
+                    gateSet[target] = multiply(gate.matrix, gateSet[target])
+                elif tangle[target] < target:
+                    gateSet[target] = multiply(np.kron(np.identity(2, dtype=np.complex), gate.matrix), gateSet[target])
+                else:
+                    gateSet[target] = multiply(np.kron(gate.matrix, np.identity(2, dtype=np.complex)), gateSet[target])
+            else:
+                target1 = gate.target1
+                target2 = gate.target2
+                if tangle[target1] == target2:
+                    pass
 
+        return gates
