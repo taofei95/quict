fe13827448be29cf5cb8696a9c01c09d269a259d
LinHeLurking <linhe.lurking@gmail.com>
2021-05-12 10:40:58 +0800

    Python wrapper added.

--- QuICT/utility/graph_structure/graph_structure.py
+++ QuICT/utility/graph_structure/graph_structure.py
@@ -24,3 +24,43 @@ class GraphStructure:
             self._instance = graph_structure.directed_graph_vertex_label_str()
         else:
             raise NotImplementedError("Only support int/str vertex label for now")
+
+    # print_info
+    def print_info(self):
+        self._instance.print_info()
+
+    # add_edge
+    def add_edge(self, from_, to_, data_):
+        self._instance.add_edge(from_, to_, data_)
+
+    # add_vertex
+    def add_vertex(self, v):
+        self._instance.add_vertex(v)
+
+    # edge_cnt
+    def edge_cnt(self) -> int:
+        return self._instance.edge_cnt()
+
+    # edges_from
+    def edges_from(self, v):
+        return self._instance.edges_from(v)
+
+    # edges_to
+    def edges_to(self, v):
+        return self._instance.edges_to(v)
+
+    # out_deg_of
+    def out_deg_of(self, v) -> int:
+        return self._instance.out_deg_of(v)
+
+    # in_deg_of
+    def in_deg_of(self, v) -> int:
+        return self._instance.in_deg_of(v)
+
+    # edges
+    def edges(self):
+        return self._instance.edges()
+
+    # dijkstra
+    def dijkstra(self, source, inf):
+        return self._instance.dijkstra(source, inf)
--- install.sh
+++ install.sh
@@ -24,4 +24,4 @@ if [[ $OS =~ "Darwin" ]];then
 fi
 
 cd $prj_build_dir && \
-  $PYTHON3 ../setup.py install
+  $PYTHON3 ../setup.py install $@
