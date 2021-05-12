a786c236f0061cdd844e9894fda09c2b3a4a16a1
visionadmin <postmaster@wuhetech.com>
2021-05-12 15:23:39 +0800

    add ut for layer 1

--- QuICT/qcda/simulation/_simulation.py
+++ QuICT/qcda/simulation/_simulation.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding:utf8 -*-
 # @TIME    : 2021/4/27 2:02 下午
 # @Author  : Han Yu
 # @File    : _simulation
 
+from functools import lru_cache
+
 import numpy as np
 
 from QuICT.core import *
 from QuICT.ops.linalg import dot, MatrixPermutation
+
 
 # tool function
 def build_2qubit_gate(compositeGate, unitary, target1, target2):
@@ -15,25 +18,37 @@ def build_2qubit_gate(compositeGate, unitary, target1, target2):
         target1, target2 = target2, target1
     ugate = Unitary(unitary).copy()
     ugate.targs = [target1, target2]
+    print(np.round(unitary))
+    print(ugate.targs)
     compositeGate.append(ugate)
+
 
 def build_1qubit_gate(compositeGate, unitary, target1):
     ugate = Unitary(unitary).copy()
     ugate.targs = [target1]
     compositeGate.append(ugate)
 
+
 # cost function
+@lru_cache(1000)
 def tensor_cost(a, b):
     return 1.0 * ((1 << b) ** 2 - (1 << a) ** 2)
 
+
+@lru_cache(1000)
 def tensor_both_cost(a, b):
     return 1.0 * ((1 << (b + a)) ** 2)
 
+
+@lru_cache(1000)
 def multiply_cost(k):
     return 1.0 * ((1 << k) ** 3)
 
+
+@lru_cache(1000)
 def multiply_vector_cost(k):
     return 1.0 * ((1 << k) ** 2)
+
 
 # tool class
 class dp:
@@ -54,34 +69,39 @@ class dp:
     def amplitude_cost(self, width):
         return self.value + tensor_cost(self.length, width) + multiply_vector_cost(width)
 
+
 class BasicSimulator(object):
 
     @staticmethod
     def pretreatment(circuit):
         """
-        :param
+
+        Args:
             circuit(Circuit): the circuit needs pretreatment.
-        :return:
+
+        Return:
             CompositeGate: the gates after pretreatment
         """
         gates = CompositeGate()
         circuit_width = circuit.circuit_width()
-        gateSet = [np.identity(2, dtype=np.complex) for _ in range(circuit_width)]
+        gateSet = [np.identity(2, dtype=np.complex128) for _ in range(circuit_width)]
         tangle = [i for i in range(circuit.circuit_width())]
         for gate in circuit.gates:
-            if gate.target + gate.controls >= 3:
+            if gate.targets + gate.controls >= 3:
                 raise Exception("only support 2-qubit gates and 1-qubit gates.")
             # 1-qubit gate
-            if gate.target + gate.controls == 1:
-                target = gate.target
+            if gate.targets + gate.controls == 1:
+                target = gate.targ
                 if tangle[target] == target:
                     gateSet[target] = dot(gate.matrix, gateSet[target])
                 else:
                     if tangle[target] < target:
-                        gateSet[target] = dot(np.kron(np.identity(2, dtype=np.complex), gate.matrix), gateSet[target])
+                        gateSet[target] = dot(np.kron(np.identity(2, dtype=np.complex128), gate.matrix),
+                                              gateSet[target])
                     else:
-                        gateSet[target] = dot(np.kron(gate.matrix, np.identity(2, dtype=np.complex)), gateSet[target])
-                    gateSet[tangle[target]] = target
+                        gateSet[target] = dot(np.kron(gate.matrix, np.identity(2, dtype=np.complex128)),
+                                              gateSet[target])
+                    gateSet[tangle[target]] = gateSet[target]
             # 2-qubit gate
             else:
                 affectArgs = gate.affectArgs
@@ -89,7 +109,7 @@ class BasicSimulator(object):
                 if target1 < target2:
                     matrix = gate.compute_matrix
                 else:
-                    matrix = MatrixPermutation(gate.compute_matrix, [1, 0])
+                    matrix = MatrixPermutation(gate.compute_matrix, np.array([1, 0]))
 
                 if tangle[target1] == target2:
                     gateSet[target1] = dot(matrix, gateSet[target1])
@@ -109,34 +129,37 @@ class BasicSimulator(object):
                     else:
                         revive = target1
                         target = target2
-                    if not np.isclose(np.identity(4, dtype=np.complex), gateSet[target]):
-                        build_2qubit_gate(gates, gateSet[target], target, tangle[target])
-                    gateSet[tangle[target]] = np.identity(2, dtype=np.complex)
-                    gateSet[target] = np.identity(2, dtype=np.complex)
+                    build_2qubit_gate(gates, gateSet[target], target, tangle[target])
+                    gateSet[tangle[target]] = np.identity(2, dtype=np.complex128)
+                    gateSet[target] = np.identity(2, dtype=np.complex128)
                     tangle[tangle[target]] = tangle[target]
                     tangle[target] = target
 
                     if tangle[revive] == revive:
-                        if revive >= target1 and revive >= target2:
-                            target_matrix = np.kron(gateSet[revive], np.identity(2, dtype=np.complex))
+                        if revive <= target1 and revive <= target2:
+                            target_matrix = np.kron(gateSet[revive], np.identity(2, dtype=np.complex128))
                         else:
-                            target_matrix = np.kron(np.identity(2, dtype=np.complex), gateSet[revive])
+                            target_matrix = np.kron(np.identity(2, dtype=np.complex128), gateSet[revive])
                         gateSet[target1] = dot(matrix, target_matrix)
                         gateSet[target2] = gateSet[target1]
+                        tangle[revive], tangle[target] = target, revive
                     else:
-                        target = revive
-                        if not np.isclose(np.identity(4, dtype=np.complex), gateSet[target]):
-                            build_2qubit_gate(gates, gateSet[target], target, tangle[target])
-                        gateSet[tangle[target]] = np.identity(2, dtype=np.complex)
-                        gateSet[target] = np.identity(2, dtype=np.complex)
-                        tangle[tangle[target]] = tangle[target]
-                        tangle[target] = target
+                        build_2qubit_gate(gates, gateSet[revive], revive, tangle[revive])
+                        gateSet[tangle[revive]] = np.identity(2, dtype=np.complex128)
+                        gateSet[revive] = np.identity(2, dtype=np.complex128)
+                        tangle[tangle[revive]] = tangle[revive]
+                        tangle[revive] = revive
+
+                        gateSet[target1] = matrix
+                        gateSet[target2] = gateSet[target1]
+                        tangle[revive], tangle[target] = target, revive
+
         for i in range(circuit_width):
             if tangle[i] == i:
-                if not np.isclose(np.identity(2, dtype=np.complex), gateSet[i]):
+                if not np.allclose(np.identity(2, dtype=np.complex128), gateSet[i]):
                     build_1qubit_gate(gates, gateSet[i], i)
             elif tangle[i] > i:
-                if not np.isclose(np.identity(4, dtype=np.complex), gateSet[i]):
+                if not np.allclose(np.identity(4, dtype=np.complex128), gateSet[i]):
                     build_2qubit_gate(gates, gateSet[i], i, tangle[i])
         return gates
 
@@ -167,7 +190,7 @@ class BasicSimulator(object):
         for gate in small_gates:
             gates.append(gate.affectArgs.copy())
         # gates as input
-        f, pre = unitary_merge_layer(gates)
+        f, pre = BasicSimulator.unitary_merge_layer(gates)
 
         order = []
 
@@ -179,7 +202,7 @@ class BasicSimulator(object):
             pre_search(l, stick)
             pre_search(stick + 1, r)
 
-        pre_search(0, gate_length - 1)
+        pre_search(0, len(gates) - 1)
         order.reverse()
         return order, small_gates
 
@@ -190,7 +213,13 @@ class BasicSimulator(object):
         for gate in small_gates:
             gates.append(gate.affectArgs.copy())
         # gates as input
-        f, pre = unitary_merge_layer(gates)
+        f, pre = BasicSimulator.unitary_merge_layer(gates)
+
+        gate_length = len(gates)
+        width = circuit.circuit_width()
+
+        amplitude_f = []
+        pre_amplitude = []
 
         for i in range(gate_length):
             pre_temp = 0
@@ -224,3 +253,4 @@ class BasicSimulator(object):
         pre_amplitude_search(gate_length - 1)
         order.reverse()
         return order, small_gates
+
