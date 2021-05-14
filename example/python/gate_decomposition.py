fc85679abe9b55a3242b5c8cc0c69e09a83d3c75
GaryChan1997 <chenzihang15@mails.ucas.ac.cn>
2021-05-14 12:14:39 +0800

    example for gate decomposition

---
+++ example/python/gate_decomposition.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from scipy.stats import unitary_group
+
+from QuICT.core import *
+from QuICT.qcda.synthesis.gate_decomposition import GateDecomposition
+
+if __name__ == "__main__":
+    circuit = Circuit(3)
+    CX | circuit([0, 2])
+    CCRz(np.pi / 3) | circuit([0, 1, 2])
+    CSwap | circuit([0, 1, 2])
+    matrix = unitary_group.rvs(2 ** 3)
+    Unitary(matrix) | circuit([0, 1, 2])
+    circuit.draw()
+
+    gates_decomposed = GateDecomposition.execute(circuit)
+    for gate in gates_decomposed:
+        assert isinstance(gate, BasicGate) and not isinstance(gate, ComplexGate)
+    circuit_decomposed = Circuit(3)
+    circuit_decomposed.set_exec_gates(gates_decomposed)
+    circuit_decomposed.draw()
