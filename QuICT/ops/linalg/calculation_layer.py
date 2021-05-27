35c641eac0d4bcaaf9cddad78631f07d8fab3374
likaiqi <yinyangshiqiqiaiai@163.com>
2021-05-27 16:32:16 +0800

    fill calculationlayer docs

--- QuICT/ops/linalg/calculation_layer.py
+++ QuICT/ops/linalg/calculation_layer.py
@@ -8,6 +8,19 @@ from . import gpu_calculator as GPUCalculator
 
 
 class CalculationLayer:
+    """ The context class of GPU linear algorithm and GPU memory management.
+
+    Args:
+        gpu_device (int): Indicated GPU device.
+        memory_limit (int): the limitation of memory pool. Unit is GiB
+
+    Examples:
+        with CalculationLayer(gpu_device=0, memory_limit=4) as CL:
+            gpu_A = cp.array(cpu_A)
+            gpu_B = cp.array(cpu_B)
+
+            result = CL.dot(gpu_A, gpu_B, gpu_out=True)
+    """
     def __init__(self, gpu_device: int = 0, memory_limit: int = None):
         if gpu_device != cp.cuda.runtime.getDevice() and gpu_device < cp.cuda.runtime.getDeviceCount():
             dev = cp.cuda.Device(gpu_device)
@@ -17,7 +30,7 @@ class CalculationLayer:
         self.pinned_mempool = cp.get_default_pinned_memory_pool()
 
         if memory_limit:
-            self.mempool.set_limit(memory_limit*1024**3)     # memory_limit GiB
+            self.mempool.set_limit(memory_limit*1024**3)
 
         # Set Memory allocator
         cp.cuda.set_allocator(self.mempool.malloc)
@@ -31,7 +44,16 @@ class CalculationLayer:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.clear_memory()
 
-    def clear_memory(self): 
+    @property
+    def memory_pool(self):
+        return self.mempool
+
+    @property
+    def pinned_memory_pool(self):
+        return self.pinned_mempool
+
+    def clear_memory(self):
+        """ released unused memory in current memory pool. """
         self.mempool_used.clear()
         self.mempool.free_all_blocks()
         self.pinned_mempool.free_all_blocks()
@@ -77,6 +99,16 @@ class CalculationLayer:
             return wr_result
 
     def dot(self, A, B, gpu_out: bool = True):
+        """ dot matrix A and matrix B
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            B(np.array<np.complex>): the matrix B
+            gpu_out(bool): return result from GPU into CPU
+
+        Returns:
+            np.array<np.complex>: A * B
+        """
         gpu_A = self._var_normalized(A)
         gpu_B = self._var_normalized(B)
 
@@ -85,6 +117,16 @@ class CalculationLayer:
         return self._result_normalized(result, gpu_out)
 
     def tensor(self, A, B, gpu_out: bool = True):
+        """ tensor A and B
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            B(np.array<np.complex>): the matrix B
+            gpu_out(bool): return result from GPU into CPU
+
+        Returns:
+            np.array<np.complex>: the tensor result A ⊗ B
+        """
         gpu_A = self._var_normalized(A)
         gpu_B = self._var_normalized(B)
 
@@ -93,6 +135,17 @@ class CalculationLayer:
         return self._result_normalized(result, gpu_out)
 
     def MatrixTensorI(self, A, n, m, gpu_out: bool = True):
+        """ tensor I^n and A and I^m
+
+        Args:
+            A(np.array<np.complex>): the matrix A
+            n(int): the index of indentity
+            m(int): the index of indentity
+            gpu_out(bool): return result from GPU into CPU
+
+        Returns:
+            np.array<np.complex>: the tensor result I^n ⊗ A ⊗ I^m
+        """
         gpu_A = self._var_normalized(A)
 
         result = GPUCalculator.MatrixTensorI(gpu_A, n, m, gpu_out)
@@ -100,6 +153,17 @@ class CalculationLayer:
         return self._result_normalized(result, gpu_out)
 
     def VectorPermutation(self, A, mapping, changeInput: bool = False, gpu_out: bool = True):
+        """ permutaion A with mapping, inplace
+
+        Args:
+            A(np.array<np.complex>): the matrix A.
+            mapping(np.array<int>): the qubit mapping.
+            changeInput(bool): whether changes in A.
+            gpu_out(bool): return result from GPU.
+
+        Returns:
+            np.array<np.complex>: the result of Permutation
+        """
         gpu_A = self._var_normalized(A)
 
         result = GPUCalculator.VectorPermutation(gpu_A, mapping, changeInput, gpu_out)
@@ -107,6 +171,14 @@ class CalculationLayer:
         return self._result_normalized(result, gpu_out)
 
     def MatrixPermutation(self, A, mapping, changeInput: bool = False, gpu_out: bool = True):
+        """ permute mat with mapping, inplace
+
+        Args:
+            A(np.array<np.complex>): the matrix A.
+            mapping(np.array<int>): the qubit mapping.
+            changeInput(bool): whether changes in A.
+            gpu_out(bool): return result from GPU.
+        """
         gpu_A = self._var_normalized(A)
 
         result = GPUCalculator.MatrixPermutation(gpu_A, mapping, changeInput, gpu_out)
@@ -114,6 +186,17 @@ class CalculationLayer:
         return self._result_normalized(result, gpu_out)
 
     def vectordot(self, A, V, mapping, gpu_out: bool = True):
+        """ dot matrix A and vector V
+
+        Args:
+            A(np.array<np.complex>): the matrix A.
+            V(np.array<np.complex>): the vector V.
+            mapping(np.array<int>): the qubit mapping.
+            gpu_out(bool): return result from GPU into CPU.
+
+        Returns:
+            np.array<np.complex>: the vector with length 2^n
+        """
         gpu_A = self._var_normalized(A)
         gpu_V = self._var_normalized(V)
 
