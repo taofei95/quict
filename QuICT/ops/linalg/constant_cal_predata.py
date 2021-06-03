3f1025aef6b4721609ffcf32339708ef75b89ce5
likaiqi <yinyangshiqiqiaiai@163.com>
2021-06-03 10:10:03 +0800

    use pre-data for vector dot matrix

---
+++ QuICT/ops/linalg/constant_cal_predata.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# @TIME    : 2021/6/1 下午3:34
+# @Author  : Han Yu
+# @File    : gpu_constant_calculator_refine
+
+import numba
+from numba import cuda
+import numpy as np
+import cmath
+
+from QuICT.core import *
+
+@cuda.jit()
+def _H_large_vec_kernel(
+    index: int,
+    mat,
+    vec
+):
+    label = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    _0 = (label & ((1 << index) - 1)) + (label >> index << (index + 1))
+    _1 = _0 + (1 << index)
+    vec[_0], vec[_1] = vec[_0]*mat[0,0] + vec[_1]*mat[0,1], vec[_0]*mat[1,0] + vec[_1]*mat[1,1]
+
+@cuda.jit()
+def _CRz_large_vec_kernel1(
+    cindex,
+    tindex,
+    mat,
+    vec
+):
+    label = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    gw = label >> cindex << (cindex + 1)
+    _0 = (1 << cindex) + (gw & ((1 << tindex) - (1 << cindex))) + (gw >> tindex << (tindex + 1)) + \
+         (label & ((1 << cindex) - 1))
+    _1 = _0 + (1 << tindex)
+    vec[_0] = vec[_0] * mat[3,3]
+    vec[_1] = vec[_1] * mat[4,4]
+
+@cuda.jit()
+def _CRz_large_vec_kernel2(
+    cindex,
+    tindex,
+    mat,
+    vec
+):
+    label = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    gw = label >> tindex << (tindex + 1)
+    _0 = (1 << cindex) + (gw & ((1 << cindex) - (1 << tindex))) + (gw >> cindex << (cindex + 1)) + \
+         (label & ((1 << tindex) - 1))
+    _1 = _0 + (1 << tindex)
+    vec[_0] = vec[_0] * mat[3,3]
+    vec[_1] = vec[_1] * mat[4,4]
+
+def gate_dot_vector_predata(
+    gate : BasicGate,
+    mat,
+    vec,
+    vec_bit
+):
+    if gate.type() == GATE_ID["H"]:
+
+        task_number = 1 << (vec_bit - 1)
+        thread_per_block = min(256, task_number)
+        block_num = task_number // thread_per_block
+
+        _H_large_vec_kernel[[block_num, 1, 1], [thread_per_block, 1, 1]](
+            vec_bit - 1 - gate.targ,
+            mat,
+            vec
+        )
+    elif gate.type() == GATE_ID["CRz"]:
+        cindex = vec_bit - 1 - gate.carg
+        tindex = vec_bit - 1 - gate.targ
+
+        task_number = 1 << (vec_bit - 2)
+        thread_per_block = min(256, task_number)
+        block_num = task_number // thread_per_block
+
+        if tindex > cindex:
+            _CRz_large_vec_kernel1[[block_num, 1, 1], [thread_per_block, 1, 1]](
+                cindex,
+                tindex,
+                mat,
+                vec
+            )
+        else:
+            _CRz_large_vec_kernel2[[block_num, 1, 1], [thread_per_block, 1, 1]](
+                cindex,
+                tindex,
+                mat,
+                vec
+            )
+    else:
+        raise Exception("ss")
