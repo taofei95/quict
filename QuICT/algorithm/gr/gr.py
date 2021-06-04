e6c18ba3b999c795d11293bf97c8974c1fe6074b
PSR <ulyssesvaner@outlook.com>
2021-06-04 10:05:05 +0800

    add gr module, including PartialGrover & StandardGrover

--- QuICT/algorithm/gr/gr.py
+++ QuICT/algorithm/gr/gr.py
@@ -7,13 +7,24 @@ from .._algorithm import Algorithm
 from QuICT import *
 from QuICT.qcda.synthesis.mct import MCTLinearOneDirtyAux
 
-def calcuate_r(nSolution,nSpace):
+
+def calculate_r(nSolution, nSpace):
     '''caculate R
 
     QCQI p253
     '''
     theta = 2*np.arccos(np.sqrt(1-nSolution/nSpace))
     return round((np.arccos(np.sqrt(nSolution/nSpace))/theta))
+
+
+def calculate_r1_r2_one_target(N, K, eps):
+    sin_theta = 2*np.sqrt(N-1)/N
+    sqrt_K_mul_alpha_yt = np.sqrt(K-sin_theta*sin_theta*(K-1))
+    r1 = np.sqrt(N)*np.pi*0.25*(1-eps)
+    r2 = (np.sqrt(N/K)*0.5)*(np.arcsin(sin_theta/sqrt_K_mul_alpha_yt) +
+                             np.arcsin(sin_theta*(K-2)/(2*sqrt_K_mul_alpha_yt)))
+    return round(r1), round(r2)
+
 
 def run_standard_grover(f, oracle):
     """ standard grover search with one target
@@ -28,14 +39,14 @@ def run_standard_grover(f, oracle):
     # state space size = (1<<n)
     n = int(np.ceil(np.log2(len(f))))
     circuit = Circuit(n + 1)
-    qreg    = circuit([i for i in range(0,n)])
+    qreg = circuit([i for i in range(0, n)])
     ancilla = circuit(n)
     # step 1
     H | qreg
     X | ancilla
     H | ancilla
     # step 2: repeat grover iteration R times
-    r = calcuate_r(1,1<<n)
+    r = calculate_r(1, 1 << n)
     for i in range(r):
         # inversion about target
         oracle(f, qreg, ancilla)
@@ -43,7 +54,8 @@ def run_standard_grover(f, oracle):
         H | qreg
         X | qreg
         H | qreg(n - 1)
-        MCTLinearOneDirtyAux.execute(n + 1) | (qreg([j for j in range(0,n - 1)]),qreg(n - 1), ancilla)
+        MCTLinearOneDirtyAux.execute(
+            n + 1) | (qreg([j for j in range(0, n - 1)]), qreg(n - 1), ancilla)
         H | qreg(n - 1)
         X | qreg
         H | qreg
@@ -52,8 +64,76 @@ def run_standard_grover(f, oracle):
     circuit.exec()
     return int(qreg)
 
-def run_partial_grover():
-    pass
+
+def run_partial_grover(f, n, k, oracle):
+    """ partial grover search with one target
+
+    Args:
+        f(list<int>): the function to be decided
+        n(int):       bits length of global address
+        k(int):       bits length of block address
+        oracle(function):   the oracle
+    Returns:
+        int: the target address, big endian
+    """
+    K = 1 << k
+    N = 1 << n
+    eps = 1/np.sqrt(K)  # can use other epsilon
+    r1, r2 = calculate_r1_r2_one_target(N, K, eps)
+
+    circuit = Circuit(n + 3)
+    qreg = circuit([i for i in range(n)])
+    ancilla = circuit(n)
+    dirty = circuit(n+1)
+    ctarget = circuit(n+2)
+    cqreg = circuit([n+2]+[i for i in range(n)])
+    # step 1
+    H | qreg
+    X | ancilla
+    H | ancilla
+    for i in range(r1):
+        # global inversion about target
+        oracle(f, qreg, ancilla)
+        # global inversion about average
+        H | qreg
+        X | qreg
+        H | qreg(n - 1)
+        MCTLinearOneDirtyAux.execute(
+            n + 1) | (qreg([j for j in range(0, n - 1)]), qreg(n - 1), dirty)
+        H | qreg(n - 1)
+        X | qreg
+        H | qreg
+    # step 2
+    for i in range(r2):
+        # global inversion about target
+        oracle(f, qreg, ancilla)
+        # local inversion about average
+        local_n = n-k
+        local_qreg = qreg([j for j in range(k, k+local_n)])
+        H | local_qreg
+        X | local_qreg
+        H | local_qreg(local_n - 1)
+        MCTLinearOneDirtyAux.execute(
+            local_n + 1) | (local_qreg([j for j in range(0, local_n - 1)]), local_qreg(local_n - 1), dirty)
+        H | local_qreg(local_n - 1)
+        X | local_qreg
+        H | local_qreg
+    # step 3
+    oracle(f, qreg, ctarget)
+    # controlled inversion about average
+    CH | (qreg, ctarget)
+    CX | (qreg, ctarget)
+    CH | (qreg(n - 1), ctarget)
+    MCTLinearOneDirtyAux.execute(
+        n + 2) | (cqreg([j for j in range(0, n)]), qreg(n - 1), ancilla)
+    CH | (qreg(n - 1), ctarget)
+    CX | (qreg, ctarget)
+    CH | (qreg, ctarget)
+    # Measure
+    Measure | qreg
+    circuit.exec()
+    return int(qreg)
+
 
 class StandardGrover(Algorithm):
     """ standard grover search with one target
@@ -72,18 +152,23 @@ class StandardGrover(Algorithm):
         """
         return run_standard_grover(f, oracle)
 
+
 class PartialGrover(Algorithm):
     """ partial grover search with one target
 
-    TODO: ref
+    https://arxiv.org/abs/quant-ph/0407122
     """
     @classmethod
-    def run(cls):
+    def run(cls, f, n, k, oracle):
         """ partial grover search with one target
 
-        Args:TODO
-
+        Args:
+            f(list<int>): the function to be decided
+            n(int):       bits length of global address
+            k(int):       bits length of block address
+            oracle(function):   the oracle
         Returns:
-            int: the target state
+            int: the target block address
         """
-        return run_partial_grover()
+        assert len(f) == (1 << n)
+        return run_partial_grover(f, n, k, oracle)
