de9abb3d9e2808f4a8bac14f35f6842ac62ff6fc
Magikaaarp <87381136+Magikaaarp@users.noreply.github.com>
2021-08-17 22:50:24 +0800

    Update encoder.py

--- QuICT/chemistry/operator/encoder.py
+++ QuICT/chemistry/operator/encoder.py
@@ -2,8 +2,7 @@
 Encoders transform ladder operators to CompositeGate on zero state of qubits.
 """
 
-from qubit_operator import QubitOperator
-from fermion_operator import FermionOperator
+from QuICT.chemistry.operator.qubit_operator import QubitOperator
 import numpy as np
 
 class Encoder:
@@ -11,13 +10,13 @@ class Encoder:
     Superclass of encoding methods.
     """
     @classmethod
-    def encoder(cls, fermion_operator, maxN = 10):
+    def encoder(cls, fermion_operator, n_fermions):
         """
         Encoders transform ladder operators to Qubit Operators.
 
         Args:
             fermion_operator(FermionOperator): FermionOperator to be transformed.
-            maxN(integer): the number of all the fermions.
+            n_fermions(integer): the number of all the fermions.
 
         Returns:
             QubitOperator: The corresponding operators on qubits.
@@ -26,26 +25,26 @@ class Encoder:
         for mono_f in fermion_operator.operators:
             mono_q = QubitOperator([],mono_f[1])
             for operator in mono_f[0]:
-                mono_q *= cls.encoder_single(operator[0], operator[1], maxN)
+                mono_q *= cls.encoder_single(operator[0], operator[1], n_fermions)
             ans += mono_q
         return ans
     
     @classmethod
-    def encoder_single(cls, target, kind, maxN):
-        '''
+    def encoder_single(cls, target, kind, n_fermions):
+        """
         Encode a single ladder operator (To be overrided).
 
         Args:
             latter_operator(tuple): a single ladder operator to be transformed.
-            maxN(integer): the number of all the fermions.
+            n_fermions(integer): the number of all the fermions.
 
         Returns:
             QubitOperator: The corresponding operators on qubits.
-        '''
+        """
         raise Exception("The encoder is not realized.")
     
-def Trans_01(target):
-    '''
+def trans_01(target):
+    """
     Construct the operator |0><1| on the target qubit.
     The next three functions are similar.
 
@@ -54,16 +53,16 @@ def Trans_01(target):
     
     Returns:
         QubitOperator: '0.5X+0.5iY' on the target qubit
-    '''
+    """
     return QubitOperator([(target, 1)], 0.5) + QubitOperator([(target, 2)], 0.5j)
 
-def Trans_10(target):
+def trans_10(target):
     return QubitOperator([(target, 1)], 0.5) + QubitOperator([(target, 2)], -0.5j)
 
-def Trans_00(target):
+def trans_00(target):
     return QubitOperator([], 0.5) + QubitOperator([(target, 3)], 0.5)
 
-def Trans_11(target):
+def trans_11(target):
     return QubitOperator([], 0.5) + QubitOperator([(target, 3)], -0.5)
 
 
@@ -71,15 +70,15 @@ def Trans_11(target):
 class JordanWigner(Encoder):
     """
     Implement the Jordan-Wigner encoding method
-    """
+    """         
     @classmethod
-    def encoder_single(cls, target, kind, maxN):
+    def encoder_single(cls, target, kind, n_fermions):
         Zlist = [(i, 3) for i in range(target)]
         ans = QubitOperator(Zlist)
         if kind == 0:               #annihilation
-            ans *= Trans_01(target)
+            ans *= trans_01(target)
         else:                       #creation
-            ans *= Trans_10(target)
+            ans *= trans_10(target)
         return ans
 
 class Parity(Encoder):
@@ -87,49 +86,49 @@ class Parity(Encoder):
     Implement the parity encoding method
     """
     @classmethod
-    def encoder_single(cls, target, kind, maxN):
-        Xlist = [(i, 1) for i in range(target+1, maxN)]
+    def encoder_single(cls, target, kind, n_fermions):
+        Xlist = [(i, 1) for i in range(target+1, n_fermions)]
         ans = QubitOperator(Xlist)
         if kind == 0:               #annihilation
             if target == 0:
-                ans *= Trans_01(target)
+                ans *= trans_01(target)
             else:
-                ans *= Trans_00(target - 1) * Trans_01(target) - Trans_11(target - 1) * Trans_10(target)
+                ans *= trans_00(target - 1) * trans_01(target) - trans_11(target - 1) * trans_10(target)
         else:                       #creation
             if target == 0:
-                ans *= Trans_10(target)
+                ans *= trans_10(target)
             else:
-                ans *= Trans_00(target - 1) * Trans_10(target) - Trans_11(target - 1) * Trans_01(target)
+                ans *= trans_00(target - 1) * trans_10(target) - trans_11(target - 1) * trans_01(target)
         return ans
 
-def update(x, maxN):
-    '''
-    Construct a list of indexes involved in update n_x
+def flip(x, n_fermions):
+    """
+    Construct a list of indexes involved in flipping n_x
 
     Args:
         x(integer): id of the target qubit.
-        maxN(integer): the number of all the fermions.
+        n_fermions(integer): the number of all the fermions.
     
     Returns:
-        list: the indexes involved in update
-    '''
+        list: the indexes involved in flipping n_x
+    """
     index = []
     x += 1
-    while (x <= maxN):
+    while (x <= n_fermions):
         index.append(x - 1)
         x += x&(-x)
     return index
 
 def sumup(x):
-    '''
+    """
     Construct a list of indexes involved in summation n_1,...,n_x
 
     Args:
         x(integer): id of the target qubit.
     
     Returns:
-        list: the indexes involved in summation
-    '''
+        list: the indexes involved in summation n_1,...,n_x
+    """
     index = []
     x += 1
     while (x > 0):
@@ -142,8 +141,8 @@ class BravyiKitaev(Encoder):
     Implement the Bravyi-Kitaev encoding method
     """
     @classmethod
-    def encoder_single(cls, target, kind, maxN):
-        Xlist = [(i,1) for i in update(target, maxN)]
+    def encoder_single(cls, target, kind, n_fermions):
+        Xlist = [(i,1) for i in flip(target, n_fermions)]
         ans = QubitOperator(Xlist, 0.5)
         Zlist1 = [(i,3) for i in sumup(target - 1)]
         Zlist2 = [(i,3) for i in sumup(target)]
