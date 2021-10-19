70b1a5495768366c38cbd1b00aef47605428822f
LinHeLurking <LinHe.Lurking@gmail.com>
2021-10-20 03:45:12 +0800

    Fast q-state merge

--- QuICT/qcda/simulation/backends/q_state.h
+++ QuICT/qcda/simulation/backends/q_state.h
@@ -18,15 +18,18 @@ namespace QuICT {
     public:
         uint64_t id_;
         uint64_t qubit_num_;
-        std::map <uint64_t, uint64_t> qubit_mapping_;
+        std::map<uint64_t, uint64_t> qubit_mapping_;
         Precision *real_;
         Precision *imag_;
 
-        QState(uint64_t id, qubit_num) : id_(id), qubit_num_(qubit_num) {
+        QState(uint64_t id, uint64_t qubit_num) : id_(id), qubit_num_(qubit_num) {
             real_ = new Precision[1ULL << qubit_num];
             imag_ = new Precision[1ULL << qubit_num];
             real_[0] = 1.0;
         }
+
+        static inline QState<Precision>
+        *merge_q_state(const QState<Precision> *a, const QState<Precision> *b, const uint64_t new_id);
 
         QState(uint64_t id) : QState(id, 1) {
             qubit_mapping_[id] = 0;
@@ -46,7 +49,7 @@ namespace QuICT {
                 : fa_(qubit_num), rank_(qubit_num, 1) {
             for (uint64_t i = 0; i < qubit_num; ++i) {
                 fa_[i] = i;
-                states_.push_back(new QState(i));
+                states_.push_back(new QState<Precision>(i));
             }
         }
 
@@ -63,8 +66,8 @@ namespace QuICT {
         inline QState<Precision> *merge_q_state(uint64_t qubit_id_a, uint64_t qubit_id_b);
 
     private:
-        std::vector <uint64_t> fa_;
-        std::vector <uint64_t> rank_;
+        std::vector<uint64_t> fa_;
+        std::vector<uint64_t> rank_;
         std::vector<QState<Precision> *> states_;
 
         inline uint64_t find(uint64_t id);
@@ -102,23 +105,59 @@ namespace QuICT {
             rank_[rt_a]++;
         }
         fa_[rt_b] = rt_a;
-        auto s = new QState<Precision>(rt_a, states_[rt_a]->qubit_num_ + states_[rt_b]->qubit_num_);
-        // TODO: complete with a fast tensor product
-        for (uint64_t i = 0; i < (1ULL << states_[rt_a]->qubit_num_); i += 1) {
-            for (uint64_t j = 0; j < (1ULL << states_[rt_b]->qubit_num_); j += 1) {
-
-            }
-        }
-        for (const auto[k, v]: states_[rt_a]->qubit_mapping_) {
-            s->qubit_mapping_[k] = v;
-        }
-        for (const auto[k, v]: states_[rt_b]->qubit_mapping_) {
-            s->qubit_mapping_[k] = v + states_[rt_a]->qubit_num_;
-        }
+        auto s = QState<Precision>::merge_q_state(states_[rt_a], states_[rt_b], rt_a);
         delete_and_set_null(states_[rt_a]);
         delete_and_set_null(states_[rt_b]);
         states_[rt_a] = s;
         return states_[rt_a];
+    }
+
+    template<typename Precision>
+    QState<Precision> *QState<Precision>::merge_q_state(
+            const QState<Precision> *a,
+            const QState<Precision> *b,
+            const uint64_t new_id
+    ) {
+        auto s = new QState<Precision>(new_id, a->qubit_num_ + b->qubit_num_);
+#define COMPLEX_MUL_REAL(a_r, a_i, b_r, b_i) ((a_r) * (b_r) - (a_i) * (b_i))
+#define COMPLEX_MUL_IMAG(a_r, a_i, b_r, b_i) ((a_r) * (b_i) + (a_i) * (b_r))
+        for (uint64_t i = 0; i < (1ULL << a->qubit_num_); i += 1) {
+            auto blk_len = (1ULL << b->qubit_num_);
+            if (b->qubit_num_ <= 2) {
+                for (uint64_t j = 0; j < blk_len; j += 1) {
+                    s->real_[i * blk_len + j] = COMPLEX_MUL_REAL(a->real_[i],
+                                                                 a->imag_[i],
+                                                                 b->real_[j],
+                                                                 b->imag_[j]);
+                    s->imag_[i * blk_len + j] = COMPLEX_MUL_IMAG(a->real_[i],
+                                                                 a->imag_[i],
+                                                                 b->real_[j],
+                                                                 b->imag_[j]);
+                }
+            } else {
+                __m256d ymm0 = _mm256_broadcast_sd(&a->real_[i]);
+                __m256d ymm1 = _mm256_broadcast_sd(&a->imag_[i]);
+                uint64_t batch_size = 4;
+                for (uint64_t j = 0; j < blk_len; j += batch_size) {
+                    __m256d ymm2 = _mm256_loadu_pd(&b->real_[j]);
+                    __m256d ymm3 = _mm256_loadu_pd(&b->imag_[j]);
+                    __m256d ymm4, ymm5; // res_r, res_i
+                    COMPLEX_YMM_MUL(ymm0, ymm1, ymm2, ymm3, ymm4, ymm5);
+                    _mm256_storeu_pd(&s->real_[i * blk_len + j], ymm4);
+                    _mm256_storeu_pd(&s->imag_[i * blk_len + j], ymm5);
+                }
+            }
+        }
+#undef COMPLEX_MUL_REAL
+#undef COMPLEX_MUL_IMAG
+
+        for (const auto[k, v]: a->qubit_mapping_) {
+            s->qubit_mapping_[k] = v;
+        }
+        for (const auto[k, v]: b->qubit_mapping_) {
+            s->qubit_mapping_[k] = v + a->qubit_num_;
+        }
+        return s;
     }
 }
 
