30b148ae52dd1c5566596773b73629b8342c137b
likaiqi <yinyangshiqiqiaiai@163.com>
2022-01-25 14:13:39 +0800

    fixed synthetical unitary

--- QuICT/algorithm/__init__.py
+++ QuICT/algorithm/__init__.py
@@ -4,7 +4,7 @@
 # @Author  : Han Yu
 # @File    : __init__.py.py
 
-from .quantum_algorithm import *
-from .synthetical_unitary import *
-from .weight_decision import *
 from ._algorithm import Algorithm
+from .quantum_algorithm import *
+from .synthetical_unitary import SyntheticalUnitary
+from .weight_decision import WeightDecision
--- QuICT/algorithm/synthetical_unitary/synthetical_unitary.py
+++ QuICT/algorithm/synthetical_unitary/synthetical_unitary.py
@@ -11,26 +11,10 @@ from QuICT.core import Circuit
 
 
 class SyntheticalUnitary(Algorithm):
-    """ get the unitary matrix of the circuit
-
-
-    """
     @classmethod
     def run(cls, circuit: Circuit, showSU=False):
         """
-
-        Args:
-            circuit(Circuit)
-            showSU(bool): whether return an SU unitary
-        """
-        circuit.const_lock = True
-        params = cls.__run__(circuit, showSU)
-        circuit.const_lock = False
-        return params
-
-    @staticmethod
-    def __run__(circuit: Circuit, showSU=True):
-        """
+        get the unitary matrix of the circuit
 
         Args:
             circuit(Circuit)
@@ -38,13 +22,16 @@ class SyntheticalUnitary(Algorithm):
         """
         matrix = np.eye(1 << len(circuit.qubits))
         for gate in circuit.gates:
-            if gate.qasm_name == "measure":
+            if gate.controls + gate.targets == 0:
                 continue
+
             matrix = np.matmul(circuit.matrix_product_to_circuit(gate), matrix)
+
         if showSU:
             det = np.linalg.det(matrix)
             n = np.shape(matrix)[0]
             det = np.power(det, 1 / n)
             matrix[:] /= det
+
         matrix = matrix
         return np.array(matrix)
