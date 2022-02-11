c899779fe37cdff3195cf4ad71da1df924a33f52
likaiqi <yinyangshiqiqiaiai@163.com>
2022-02-11 16:05:43 +0800

    bug fixed

--- QuICT/core/circuit/circuit.py
+++ QuICT/core/circuit/circuit.py
@@ -63,7 +63,7 @@ class Circuit(object):
         self._gates = gates
 
     @property
-    def topology(self) -> list:
+    def topology(self) -> Layout:
         return self._topology
 
     @topology.setter
@@ -557,7 +557,7 @@ class Circuit(object):
         Args:
             gate(BasicGate): the gate to be extended.
         """
-        return matrix_product_to_circuit(len(self.qubits), gate)
+        return matrix_product_to_circuit(gate, len(self.qubits))
 
     def remapping(self, qureg: Qureg, mapping: list, circuit_update: bool = False):
         if not isinstance(qureg, Qureg):
