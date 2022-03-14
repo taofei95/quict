23a9b94fb1f9888e7523a80a10e73cf2f1556754
likaiqi <yinyangshiqiqiaiai@163.com>
2022-03-14 16:10:44 +0800

    fixed core/qcda/simu demo issue/ add alg. demo files

---
+++ example/demo/grover_demo.py
@@ -0,0 +1,51 @@
+import pytest
+
+from QuICT.algorithm.quantum_algorithm.grover import Grover, PartialGrover, GroverWithPriorKnowledge
+from QuICT.core import *
+import QuICT
+print(QuICT.__file__)
+
+
+def main_oracle(f, qreg, ancilla):
+    PermFx(f) | (qreg, ancilla)
+
+
+def calculate_base_vector(n: int, target: int):
+    """ include alpha, beta, phi, theta
+
+    Returns:
+        tuple: alpha, beta, phi
+    """
+    N = 2**n
+    M = 1
+    alpha = np.ones(N)
+    alpha[target] = 0.
+    alpha = alpha/np.sqrt(N-M)
+    beta = np.zeros(N)
+    beta[target] = 1.
+    beta = beta/np.sqrt(M)
+    phi = np.ones(N)/np.sqrt(N)
+    return alpha, beta, phi
+
+
+def test_grover():
+    print(
+        "standard grover demonstration: given f defined on [0,2**n-1], find the only x that f(x)=1:")
+    n = int(input("\tinput space size n: "))
+    target = int(input("\tinput target x: "))
+    N = 2**n
+    f = [0] * N
+    f[target] = 1
+    alpha, beta, phi = calculate_base_vector(n, target)
+    result = Grover.run(f, n, main_oracle, demo_mode=True,
+                        **{"alpha": alpha, "beta": beta, "phi": phi, "target": target})
+    if target != result:
+        print("Failed: For n = %d, target = %d, found = %d" %
+              (n, target, result))
+    else:
+        print("Succeed: found target")
+
+
+if __name__ == '__main__':
+    np.set_printoptions(formatter={'complexfloat': '{: 0.3f}'.format})
+    test_grover()
---
+++ example/demo/order_finding_demo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# @TIME    : 2020/11/5 9:58
+# @Author  : Han Yu
+# @File    : unit_test.py
+
+from QuICT.algorithm import HRS_order_finding, BEA_order_finding
+
+# N = int(input("Input the modulo N: "))
+# a = int(input("Input the element wanting the order: "))
+
+print('HRS order finding')
+order = HRS_order_finding.run(3,11,'demo')
+if order != 0:
+    print("HRS_order_finding found order", order)
+
+print('BEA order finding')
+order = BEA_order_finding.run(3,11,'demo')
+if order != 0:
+    print("BEA_order_finding found order", order)
