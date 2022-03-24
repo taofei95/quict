c2dab671264561e5366fe78f51796b313ebb788b
GaryChan <chenzihang15@mails.ucas.ac.cn>
2022-03-24 17:19:50 +0800

    symbolic comment

--- QuICT/qcda/optimization/symbolic_peephole_optimization/symbolic_peephole_optimization.py
+++ QuICT/qcda/optimization/symbolic_peephole_optimization/symbolic_peephole_optimization.py
@@ -4,6 +4,7 @@ Optimize Clifford circuits with symbolic Pauli gates
 
 from QuICT.qcda.optimization._optimization import Optimization
 
+
 class SymbolicPeepholeOptimization(Optimization):
     """
     By decoupling CNOT gates with projectors and symbolic Pauli gates, optimization
@@ -15,3 +16,19 @@ class SymbolicPeepholeOptimization(Optimization):
     @classmethod
     def execute(cls):
         pass
+
+
+class SymbolicPauliGate(object):
+    """
+    Symbolic Pauli gate gives another expression for controlled Pauli gates.
+
+    By definition, a controlled-U gate CU means:
+        if the control qubit is |0>, do nothing;
+        if the control qubit is |1>, apply U to the target qubit.
+    In general, CU = ∑_v |v><v| ⊗ U^v, where U^v is called a symbolic gate.
+
+    Here we focus only on symbolic Pauli gates, symbolic phase gates and their
+    composition, whose optimization is the core of SymbolicPeepholeOptimization.
+
+    More specifically, this class contains the following parts:
+    """
