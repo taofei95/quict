8e3c33e70b65ebd0641e63cbad3ba2937133d0da
GaryChan <chenzihang15@mails.ucas.ac.cn>
2022-03-28 14:26:07 +0800

    symbolic pauli gate

--- QuICT/qcda/utility/pauli_operator.py
+++ QuICT/qcda/utility/pauli_operator.py
@@ -13,29 +13,27 @@ class PauliOperator(object):
     """
     Pauli operator is a list of (I, X, Y, Z) with length n, which operates on n qubits.
 
-    In this class, we use a list of int to represent the operator, where the GateTypes stand
-    for the gates.
+    In this class, we use a list of GateType to represent the operator, where the GateTypes stand
+    for the gates. Despite the operator, the phase is also recorded.
     """
     def __init__(self, operator=None, phase=1 + 0j):
         """
-        Construct a PauliOperator with a list of int(GateType)
+        Construct a PauliOperator with a list of GateType
 
         Args:
-            operator(list, optional): the list of int(GateType) representing the PauliOperator
+            operator(list, optional): the list of GateType representing the PauliOperator
             phase(complex, optional): the global phase of the PauliOperator, ±1 or ±i
         """
         if operator is None:
             self._operator = []
         else:
-            if not isinstance(operator, list):
-                raise TypeError("operator must be list of int(GateType).")
-            for gatetype in operator:
-                if gatetype != GateType.id and gatetype not in PAULI_GATE_SET:
-                    raise ValueError("operator must contain Pauli gates only.")
+            assert isinstance(operator, list), TypeError("operator must be list of int(GateType).")
+            for gate_type in operator:
+                assert gate_type == GateType.id or gate_type in PAULI_GATE_SET,\
+                    ValueError("operator must contain Pauli gates only.")
             self._operator = operator
         phase_list = [1 + 0j, 0 + 1j, -1 + 0j, 0 - 1j]
-        if phase not in phase_list:
-            raise ValueError("phase must be ±1 or ±i")
+        assert phase in phase_list, ValueError("phase must be ±1 or ±i")
         self._phase = phase
 
     def __str__(self):
@@ -59,11 +57,10 @@ class PauliOperator(object):
 
     @operator.setter
     def operator(self, operator: list):
-        if not isinstance(operator, list):
-            raise TypeError("operator must be list of int(GateType).")
-        for gatetype in operator:
-            if gatetype != GateType.id and gatetype not in PAULI_GATE_SET:
-                raise ValueError("operator must contain Pauli gates only.")
+        assert isinstance(operator, list), TypeError("operator must be list of int(GateType).")
+        for gate_type in operator:
+            assert gate_type == GateType.id or gate_type in PAULI_GATE_SET,\
+                ValueError("operator must contain Pauli gates only.")
         self._operator = operator
 
     @property
@@ -73,8 +70,7 @@ class PauliOperator(object):
     @phase.setter
     def phase(self, phase: complex):
         phase_list = [1 + 0j, 0 + 1j, -1 + 0j, 0 - 1j]
-        if phase not in phase_list:
-            raise ValueError("phase must be ±1 or ±i")
+        assert phase in phase_list, ValueError("phase must be ±1 or ±i")
         self._phase = phase
 
     def gates(self, keep_id=False) -> CompositeGate:
