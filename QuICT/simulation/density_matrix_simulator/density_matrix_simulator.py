29453facf62e08439a47c4492110c3ff42235e74
chen-wanyan <201911081141@mail.bnu.edu.cn>
2022-04-13 19:10:41 +0800

    measure part ?

--- QuICT/simulation/density_matrix_simulator/density_matrix_simulator.py
+++ QuICT/simulation/density_matrix_simulator/density_matrix_simulator.py
@@ -1,7 +1,9 @@
 import abc
 from operator import matmul
 from statistics import mean
+from tabnanny import check
 from unittest.result import failfast
+from venv import create
 import numpy as np
 import pandas
 import math
@@ -10,6 +12,8 @@ from QuICT.core.circuit.circuit import Circuit
 from QuICT.simulation.unitary_simulator import UnitarySimulator
 import QuICT.ops.linalg.cpu_calculator as CPUCalculator
 import QuICT.core.utils
+from QuICT.core.gate import Measure, BasicGate
+from QuICT.core.utils import GateType
 
 class DensityMatrixSimulation:
 
@@ -28,12 +32,11 @@ class DensityMatrixSimulation:
             self._computer = GPUCalculator
 
 
-    def create_init_matrix(self, n):
-        self.initialM = np.zeros((2 ** n, 2 ** n), dtype = self._precision)
-        self.initialM[0,0] = 1
-        # return initialM
+    def create_init_density_matrix(self, n):
+        self.density_matrix = np.zeros((2 ** n, 2 ** n), dtype = self._precision)
+        self.density_matrix[0,0] = 1
 
-    def checkMatrix(matrix):
+    def check_matrix(matrix):
         if(matrix.T.conjugate() != matrix): 
             return False
 
@@ -47,32 +50,15 @@ class DensityMatrixSimulation:
 
         return True
 
-    def density_matrix_simu(self, circuit, density_matrix: np.ndarray = None):
-        # density_matrix is given or not
-        # if given, check correct
-        # if not given, generate initial density matrix
-        
-        # Assume no measure gate in circuit middle, measure gate only appear last
-        # circuit.gates [non-measure gates] [measure gate]
-        circuit_matrix = UnitarySimulator.get_unitary_matrix(circuit)
-        
-        # step ops
-        density_matrix = self._computer.dot(self._computer.dot(circuit_matrix, density_matrix), circuit_matrix.conj().T)
-        
-        # [measure gate] exist
-        self._measure(gate_list, circuit.width())
-
-        return density_matrix
-
-    def Measure(self, gate_list, qubits):
+    def measure(self, gate_list, qubits):
         
         P0 = np.mat([[1, 0], [0, 0]]) + "target qubits"
         P1 = np.mat([[0, 0], [0, 1]])
         mea_0 = mea_circuit.matrix_product_to_circuit(P0)
-        prob_0 = np.matmul(mea_0, matrix).trace()
+        prob_0 = self._computer.matmul(mea_0, matrix).trace()
         _0_1 = random() < prob_0
         if _0_1:
-            U = np.matmul(mea_0, np.eye(2**n) / np.sqrt(prob_0))
+            U = self._computer.matmul(mea_0, np.eye(2**n) / np.sqrt(prob_0))
             matrix = U.dot(matrix).dot(U.conj().T)
         else:
             mea_1 = mea_circuit.matrix_product_to_circuit(P1)
@@ -80,3 +66,29 @@ class DensityMatrixSimulation:
             matrix = U.dot(matrix).dot(U.conj().T)
 
         mea_circuit.qubits[gate.targ].measured = int(_0_1)
+
+
+    def density_matrix_simu(self, circuit:Circuit, n, density_matrix: np.ndarray = None):
+        if(density_matrix == None or self.check_matrix(density_matrix) == False):
+            density_matrix = self.create_init_density_matrix(n)
+        else:
+            self.density_matrix = density_matrix
+        
+        # Assume no measure gate in circuit middle, measure gate only appear last
+        # circuit.gates [non-measure gates] [measure gate]
+        measure_gate_list = []
+        for gate in circuit.gates:
+            if gate.type == GateType.measure:
+                measure_gate_list.append(gate) 
+                circuit.gates.remove(gate)  
+        circuit_matrix = UnitarySimulator.get_unitary_matrix(circuit)
+        
+        # step ops
+        self.density_matrix = self._computer.dot(self._computer.dot(circuit_matrix, density_matrix), circuit_matrix.conj().T)
+
+        # [measure gate] exist
+        self._measure(measure_gate_list, circuit.width())
+
+        return density_matrix
+
+
