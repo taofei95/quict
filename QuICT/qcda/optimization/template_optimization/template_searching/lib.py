3cfa16e4251bb35aa60138d486ce6ce88c611273
sunyuan2016 <sunyuan2016@ict.ac.cn>
2022-04-22 08:18:32 +0000

    update QuICT/qcda/optimization/template_optimization/template_searching/lib.py.

--- QuICT/qcda/optimization/template_optimization/template_searching/lib.py
+++ QuICT/qcda/optimization/template_optimization/template_searching/lib.py
@@ -1,117 +1,112 @@
 from QuICT.core.circuit import Circuit
 from QuICT.core.gate import *
 
+template_list = []
+
+circuit = Circuit(1)
+S | circuit(0)
+S | circuit(0)
+S | circuit(0)
+S | circuit(0)
+template_list.append(circuit)
+
+circuit = Circuit(1)
+H | circuit(0)
+H | circuit(0)
+template_list.append(circuit)
+
+circuit = Circuit(2)
+CX | circuit([0, 1])
+CX | circuit([0, 1])
+template_list.append(circuit)
+
+circuit = Circuit(2)
+H | circuit(0)
+H | circuit(1)
+CX | circuit([0, 1])
+H | circuit(0)
+H | circuit(1)
+CX | circuit([1, 0])
+template_list.append(circuit)
+
+circuit = Circuit(2)
+H | circuit(0)
+CX | circuit([0, 1])
+H | circuit(1)
+H | circuit(0)
+CX | circuit([1, 0])
+H | circuit(1)
+template_list.append(circuit)
+
+circuit = Circuit(2)
+H | circuit(0)
+CX | circuit([1, 0])
+H | circuit(1)
+H | circuit(0)
+CX | circuit([0, 1])
+H | circuit(1)
+template_list.append(circuit)
+
+circuit = Circuit(2)
+CX | circuit([0, 1])
+H | circuit(0)
+H | circuit(1)
+CX | circuit([1, 0])
+H | circuit(0)
+H | circuit(1)
+template_list.append(circuit)
+
+circuit = Circuit(3)
+CX | circuit([0, 1])
+CX | circuit([0, 2])
+CX | circuit([1, 2])
+CX | circuit([0, 1])
+CX | circuit([1, 2])
+template_list.append(circuit)
+
+circuit = Circuit(2)
+CX | circuit([0, 1])
+CX | circuit([1, 0])
+CX | circuit([0, 1])
+CX | circuit([1, 0])
+CX | circuit([0, 1])
+CX | circuit([1, 0])
+template_list.append(circuit)
+
+circuit = Circuit(3)
+CX | circuit([0, 1])
+CX | circuit([1, 0])
+CX | circuit([0, 2])
+CX | circuit([1, 0])
+CX | circuit([0, 1])
+CX | circuit([1, 2])
+template_list.append(circuit)
+
+circuit = Circuit(3)
+CX | circuit([0, 1])
+CX | circuit([1, 0])
+CX | circuit([2, 1])
+CX | circuit([1, 0])
+CX | circuit([0, 1])
+CX | circuit([2, 0])
+template_list.append(circuit)
+
 class TemplateLib:
 
     def __init__(self, qubit_num, size, depth):
-        self.qubit_num = qubit_num
-        self.size = size
-        self.depth = depth
         self.template = []
 
-        if self.size >= 4 and self.depth >= 4:
-            circuit = Circuit(self.qubit_num)
-            S | circuit(0)
-            S | circuit(0)
-            S | circuit(0)
-            S | circuit(0)
-            self.template.append(circuit)
+        for circuit in template_list:
+            if circuit.width() <= qubit_num and circuit.size() <= size and circuit.depth() <= depth:
+                self.template.append(circuit)
 
-        if self.size >= 2 and self.depth >= 2:
-            circuit = Circuit(self.qubit_num)
-            H | circuit(0)
-            H | circuit(0)
-            self.template.append(circuit)
+    def template_list(self):
+        return self.template
 
-        if self.qubit_num >= 2 and self.size >= 2 and self.depth >= 2:
-            circuit = Circuit(self.qubit_num)
-            CX | circuit([0,1])
-            CX | circuit([0,1])
-            self.template.append(circuit)
-
-        if self.qubit_num >= 2 and self.size >= 6 and self.depth >= 4:
-            circuit = Circuit(self.qubit_num)
-            H | circuit(0)
-            H | circuit(1)
-            CX | circuit([0, 1])
-            H | circuit(0)
-            H | circuit(1)
-            CX | circuit([1, 0])
-            self.template.append(circuit)
-
-        if self.qubit_num >= 2 and self.size >= 6 and self.depth >= 5:
-            circuit = Circuit(self.qubit_num)
-            H | circuit(0)
-            CX | circuit([0, 1])
-            H | circuit(1)
-            H | circuit(0)
-            CX | circuit([1, 0])
-            H | circuit(1)
-            self.template.append(circuit)
-
-        if self.qubit_num >= 2 and self.size >= 6 and self.depth >= 5:
-            circuit = Circuit(self.qubit_num)
-            H | circuit(0)
-            CX | circuit([1, 0])
-            H | circuit(1)
-            H | circuit(0)
-            CX | circuit([0, 1])
-            H | circuit(1)
-            self.template.append(circuit)
-
-        if self.qubit_num >= 2 and self.size >= 6 and self.depth >= 4:
-            circuit = Circuit(self.qubit_num)
-            CX | circuit([0, 1])
-            H | circuit(0)
-            H | circuit(1)
-            CX | circuit([1, 0])
-            H | circuit(0)
-            H | circuit(1)
-            self.template.append(circuit)
-
-        if self.qubit_num >= 3 and self.size >= 5 and self.depth >= 5:
-            circuit = Circuit(self.qubit_num)
-            CX | circuit([0, 1])
-            CX | circuit([0, 2])
-            CX | circuit([1, 2])
-            CX | circuit([0, 1])
-            CX | circuit([1, 2])
-            self.template.append(circuit)
-
-        if self.qubit_num >= 2 and self.size >= 6 and self.depth >= 6:
-            circuit = Circuit(self.qubit_num)
-            CX | circuit([0, 1])
-            CX | circuit([1, 0])
-            CX | circuit([0, 1])
-            CX | circuit([1, 0])
-            CX | circuit([0, 1])
-            CX | circuit([1, 0])
-            self.template.append(circuit)
-
-        if self.qubit_num >= 3 and self.size >= 6 and self.depth >= 6:
-            circuit = Circuit(self.qubit_num)
-            CX | circuit([0, 1])
-            CX | circuit([1, 0])
-            CX | circuit([0, 2])
-            CX | circuit([1, 0])
-            CX | circuit([0, 1])
-            CX | circuit([1, 2])
-            self.template.append(circuit)
-
-        if self.qubit_num >= 3 and self.size >= 6 and self.depth >= 6:
-            circuit = Circuit(self.qubit_num)
-            CX | circuit([0, 1])
-            CX | circuit([1, 0])
-            CX | circuit([2, 1])
-            CX | circuit([1, 0])
-            CX | circuit([0, 1])
-            CX | circuit([2, 0])
-            self.template.append(circuit)
-
-if __name__ == '__main__':
-    qubit_num, size, depth = eval(input('请输入参数（比特数，规模，深度）：'))
-    template = TemplateLib(qubit_num, size, depth).template
-    label = 1
-    for item_circuit in template:
-        print(item_circuit.draw('matp', str(label)))
-        label += 1
+# if __name__ == '__main__':
+#    qubit_num, size, depth = eval(input('请输入参数（比特数，规模，深度）：'))
+#    template = TemplateLib(qubit_num, size, depth).template
+#    label = 1
+#    for item_circuit in template:
+#        print(item_circuit.draw('matp', str(label)))
+#        label += 1
