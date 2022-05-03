90f4aae8b715bd8c08ddf5a22dea51f88f2fb4d3
Ou <1756099156@qq.com>
2022-05-03 08:56:17 +0800

    add a build_gate method for Thouless

--- QuICT/algorithm/variational_quantum_eigensolver/ansatz.py
+++ QuICT/algorithm/variational_quantum_eigensolver/ansatz.py
@@ -6,7 +6,10 @@
 
 from QuICT.core.gate import CompositeGate
 from QuICT.core import Circuit
-from QuICT.core.gate import MGR
+from QuICT.core.gate import Rz, sqiSwap
+from quict.QuICT.core.gate.gate import modified_Givens_rotation
+
+import numpy as np
 
 class Ansatz(object):
     """ ansatz used to generate composite gates"""
@@ -46,12 +49,28 @@ class Thouless(Ansatz):
     arXiv: 2004.04174v4 [quant-ph] 18 Sep 2020
     
     """
-    def build_circuit(n, angles):
+    @staticmethod
+    def modified_Givens_rotation(angle):
+        cgate = CompositeGate()
+
+        with cgate:
+            sqiSwap & [0, 1]
+            Rz(angle) & 0
+            Rz(np.pi - angle) & 1
+            sqiSwap & [0, 1]
+            Rz(np.pi) & 0
+        
+        return cgate
+
+    @staticmethod
+    def build_circuit(n, angles, num_electron_pairs, num_orbits):
         '''Quantum Circuits with n qubits and C(n 2)  R(theta)[p,q] gates
         
         Args:
             n(int): number of quantum qubits
             angles(List[float]): a list with C(N 2) parameter
+            num_electron_pairs(int): number of electron pairs
+            num_orbits(int): number of orbits
 
         Returns:
             Circuit: Quantum Circuits with N qubits and C(N 2) R gates
@@ -59,7 +78,7 @@ class Thouless(Ansatz):
         # circuits with n qubits
         ansatz = Circuit(n)
 
-        R = MGR
+        R = modified_Givens_rotation
 
         # add gates to the circuits in parallelization
         if(n == 1):
@@ -83,6 +102,47 @@ class Thouless(Ansatz):
                     index -= 2
                 i -= 1
         
+        return ansatz
+
+    def build_gate(n, angles, num_electron_pairs, num_orbits):
+        """ 
+        build a compositegate from a list of parameters
+
+        Args:
+            args(tuple): parameters of the ansatz builder
+        
+        Returns:
+            (CompositeGate): a composite gate built from the given arguments
+
+        """
+        # circuits with n qubits
+        ansatz = CompositeGate()
+
+        R = modified_Givens_rotation
+
+        with ansatz:
+            # add gates to the circuits in parallelization
+            if(n == 1):
+                return ansatz
+            elif(n == 2):
+                R(angles.pop()) & [0, 1] # assign the parameters to each circuit
+            elif(n > 2):
+                i = 0
+
+                while i < n - 1:
+                    index = i
+                    while index >= 0:
+                        R(angles.pop()) & [n - index - 2, n - index - 1]
+                        index -= 2
+                    i += 1
+
+                while i > 0:
+                    index = i-1
+                    while index >= 0:
+                        R(angles.pop()) & [n - index - 2, n - index - 1]
+                        index -= 2
+                    i -= 1
+            
         return ansatz
 
 
