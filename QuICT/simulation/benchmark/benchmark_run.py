68b54d1edefa6c3b49f6f89263b797b856962c46
LinHeLurking <LinHe.Lurking@gmail.com>
2022-06-24 10:37:31 +0000

    Small fixes for run scripts and unit tests.

--- QuICT/simulation/benchmark/benchmark_run.py
+++ QuICT/simulation/benchmark/benchmark_run.py
@@ -31,7 +31,6 @@ def qiskit_sim(scale: str):
     # Import Qiskit
     from qiskit import QuantumCircuit
     from qiskit import Aer, transpile
-    from qiskit.tools.visualization import plot_histogram, plot_state_city
     import qiskit.quantum_info as qi
 
     for f_path in load_circ(scale):
--- QuICT/simulation/benchmark/unit_test.py
+++ QuICT/simulation/benchmark/unit_test.py
@@ -1,18 +1,5 @@
 from .benchmark_run import quict_sim, qiskit_sim
 
 
-def test_qft():
-    for circ in Benchmarks.qft("small"):
-        simulator = CircuitSimulator()
-        _ = simulator.run(circ)
-
-
-def test_single():
-    for circ in Benchmarks.single_bit("small"):
-        simulator = CircuitSimulator()
-        _ = simulator.run(circ)
-
-
-def test_qasm_export():
-    for circ in Benchmarks.single_bit("small"):
-        print(circ.qasm())
+def test_quict():
+    quict_sim("small")
