unitary, dm single precision bug fixed

QuICT/simulation/density_matrix/density_matrix_simulator.py
@@ -149,10 +149,7 @@ class DensityMatrixSimulation:
         Args:
             circuit (Circuit): The circuit only have BasicGate.
         """
-        if self._precision == np.complex64:
-            circuit.convert_precision()
-
-        circuit_matrix = circuit.matrix(self._device)
+        circuit_matrix = circuit.matrix(self._device).astype(self._precision)
         self._density_matrix = self._computer.dot(
             self._computer.dot(circuit_matrix, self._density_matrix),
             circuit_matrix.conj().T

QuICT/simulation/unitary/unitary_simulator.py
@@ -68,11 +68,8 @@ class UnitarySimulator():
         """
         # Step 1: Generate the unitary matrix of the given circuit
         if isinstance(circuit, Circuit):
-            if self._precision == np.complex64:
-                circuit.convert_precision()
-
             self._qubits_num = circuit.width()
-            self._unitary_matrix = circuit.matrix(self._device)
+            self._unitary_matrix = circuit.matrix(self._device).astype(self._precision)
             assert 2 ** self._qubits_num == self._unitary_matrix.shape[0]
         else:
             row, col = circuit.shape