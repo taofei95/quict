fixed precision switch

QuICT/core/gate/gate.py
@@ -145,6 +145,10 @@ class BasicGate(object):
         return self.targs[0]
 
     @property
+    def precision(self):
+        return self._precision
+
+    @property
     def qasm_name(self):
         return self._qasm_name
 
@@ -1822,6 +1826,9 @@ class PermGate(BasicGate):
             assert len(targs) == self.targets + self.controls
             cgate & targs
 
+        if self._precision == np.complex64:
+            cgate.convert_precision()
+
         return cgate
 
 
@@ -1966,9 +1973,12 @@ class UnitaryGate(BasicGate):
 
         assert self.controls + self.targets > 0
         mapping_args = self.cargs + self.targs
-        composite_gate, _ = UnitaryTransform.execute(self.matrix, mapping=mapping_args)
+        cgate, _ = UnitaryTransform.execute(self.matrix, mapping=mapping_args)
 
-        return composite_gate
+        if self._precision == np.complex64:
+            cgate.convert_precision()
+
+        return cgate
 
 
 Unitary = UnitaryGate()
@@ -2035,6 +2045,9 @@ class CCXGate(BasicGate):
         if len(args) == self.controls + self.targets:
             cgate & args
 
+        if self._precision == np.complex64:
+            cgate.convert_precision()
+
         return cgate
 
 
@@ -2098,6 +2111,9 @@ class CCZGate(BasicGate):
         args = self.cargs + self.targs
         if len(args) == self.controls + self.targets:
             cgate & args
+
+        if self._precision == np.complex64:
+            cgate.convert_precision()
 
         return cgate
 
@@ -2175,6 +2191,9 @@ class CCRzGate(BasicGate):
         if len(args) == self.controls + self.targets:
             cgate & args
 
+        if self._precision == np.complex64:
+            cgate.convert_precision()
+
         return cgate
 
 
@@ -2232,6 +2251,9 @@ class QFTGate(BasicGate):
         if len(args) == targets:
             cgate & args
 
+        if self._precision == np.complex64:
+            cgate.convert_precision()
+
         return cgate
 
 
@@ -2273,6 +2295,9 @@ class IQFTGate(QFTGate):
         args = self.cargs + self.targs
         if len(args) == targets:
             cgate & args
+
+        if self._precision == np.complex64:
+            cgate.convert_precision()
 
         return cgate
 
@@ -2343,6 +2368,9 @@ class CSwapGate(BasicGate):
         args = self.cargs + self.targs
         if len(args) == self.controls + self.targets:
             cgate & args
+
+        if self._precision == np.complex64:
+            cgate.convert_precision()
 
         return cgate
 

QuICT/simulation/density_matrix/density_matrix_simulator.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 from QuICT.core.circuit.circuit import Circuit
-from QuICT.core.gate import BasicGate, UnitaryGate, Unitary
+from QuICT.core.gate import BasicGate, UnitaryGate, Unitary, CompositeGate
 from QuICT.core.noise import NoiseModel
 from QuICT.core.operator import NoiseGate
 from QuICT.core.utils import GateType, matrix_product_to_circuit
@@ -43,7 +43,6 @@ class DensityMatrixSimulation:
         """ Initial the qubits, quantum gates and state vector by given quantum circuit. """
         self._circuit = circuit if noise_model is None else noise_model.transpile(circuit)
         self._qubits = int(circuit.width())
-        self._last_call_per_qubit = [None] * self._qubits
 
         if self._precision == np.complex64:
             circuit.convert_precision()
@@ -105,11 +104,8 @@ class DensityMatrixSimulation:
             self.initial_density_matrix(self._qubits)
 
         # Start simulator
-        based_circuit = Circuit(self._qubits)
+        based_circuit = CompositeGate()
         for gate in self._circuit.gates:
-            for arg in gate.cargs + gate.targs:
-                self._last_call_per_qubit[arg] = gate.type
-
             # Store continuous BasicGates into based_circuit
             if isinstance(gate, BasicGate) and gate.type != GateType.measure:
                 gate | based_circuit
@@ -141,18 +137,18 @@ class DensityMatrixSimulation:
 
         return self._density_matrix
 
-    def apply_gates(self, circuit: Circuit):
+    def apply_gates(self, cgate: CompositeGate):
         """ Simulating Circuit with BasicGates
 
         dm = M*dm(M.conj)^T
 
         Args:
-            circuit (Circuit): The circuit only have BasicGate.
+            cgate (CompositeGate): The CompositeGate.
         """
-        circuit_matrix = circuit.matrix(self._device).astype(self._precision)
+        cgate_matrix = cgate.matrix(self._device)
         self._density_matrix = self._computer.dot(
-            self._computer.dot(circuit_matrix, self._density_matrix),
-            circuit_matrix.conj().T
+            self._computer.dot(cgate_matrix, self._density_matrix),
+            cgate_matrix.conj().T
         )
 
     def apply_noise(self, noise_gate: NoiseGate, qubits: int):
@@ -181,7 +177,11 @@ class DensityMatrixSimulation:
         error_matrix = gate.prob_mapping_operator(prob)
         gate_args = gate.cargs + gate.targs
 
-        return Unitary(error_matrix) & gate_args
+        unitary_gate = Unitary(error_matrix) & gate_args
+        if self._precision == np.complex64:
+            unitary_gate.convert_precision()
+
+        return unitary_gate
 
     def apply_measure(self, index: int):
         """ Simulating the MeasureGate.
@@ -215,9 +215,10 @@ class DensityMatrixSimulation:
         assert (self._density_matrix is not None)
         original_dm = self._density_matrix.copy()
         state_list = [0] * self._density_matrix.shape[0]
+        lastcall_per_qubit = self._circuit.get_lastcall_for_each_qubits()
         measured_idx = [
             i for i in range(self._qubits)
-            if self._last_call_per_qubit[i] != GateType.measure
+            if lastcall_per_qubit[i] != GateType.measure
         ]
 
         for _ in range(shots):

QuICT/simulation/state_vector/gpu_simulator/constant_statevector_simulator.py
@@ -81,7 +81,6 @@ class ConstantStateVectorSimulator:
         """ Initial the qubits, quantum gates and state vector by given quantum circuit. """
         self._circuit = circuit
         self._qubits = int(circuit.width())
-        self._last_call_per_qubit = [None] * self._qubits
         self._pipeline = []
 
         if self._precision == np.complex64:
@@ -165,9 +164,6 @@ class ConstantStateVectorSimulator:
         default_parameters = (self._vector, self._qubits, self._sync)
         if gate_type == GateType.id or gate_type == GateType.barrier:
             return
-
-        for args in gate.cargs + gate.targs:
-            self._last_call_per_qubit[args] = gate_type
 
         # Deal with quantum gate with more than 3 qubits.
         if (
@@ -516,9 +512,10 @@ class ConstantStateVectorSimulator:
         assert (self._circuit is not None)
         original_sv = self._vector.copy()
         state_list = [0] * (1 << self._qubits)
+        lastcall_per_qubit = self._circuit.get_lastcall_for_each_qubits()
         measured_idx = [
             i for i in range(self._qubits)
-            if self._last_call_per_qubit[i] not in [GateType.reset, GateType.measure]
+            if lastcall_per_qubit not in [GateType.reset, GateType.measure]
         ]
 
         for _ in range(shots):