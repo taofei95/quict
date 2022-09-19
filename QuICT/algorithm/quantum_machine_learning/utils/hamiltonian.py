41f2b28a5c5e0dfbeebf9c6b8cc86da3e13825ed
Ziying Guo <guoziying@ict.ac.cn>
2022-09-19 17:21:37 +0800

    modify class hamiltonian and VQA

--- QuICT/algorithm/quantum_machine_learning/utils/hamiltonian.py
+++ QuICT/algorithm/quantum_machine_learning/utils/hamiltonian.py
@@ -1,19 +1,84 @@
 from typing import Dict, List, Union
 import numpy as np
+import copy
 
 
 class Hamiltonian:
-    def __init__(self, hamiltonian: Union[np.ndarray, list]):
-        self._hamiltonian = hamiltonian
-        self._matrix_h = None
+    @property
+    def coefficients(self):
+        return self._coefficients
 
-        if isinstance(self._hamiltonian, np.ndarray):
-            self._matrix_h = hamiltonian
-            self._hamiltonian = self.matrix2paulistr()
+    @property
+    def pauli_gates(self):
+        return self._pauli_gates
 
-    def matrix2paulistr(self):
-        return self._jordan_wigner(self._hamiltonian)
+    @property
+    def qubit_indexes(self):
+        return self._qubit_indexes
 
-    def paulistr2matrix(self):
-        return
+    def __init__(self, pauli_str: list):
+        self._pauli_str = pauli_str
+        self._coefficients = []
+        self._pauli_gates = []
+        self._qubit_indexes = []
+
+        self._pauli_str_validation()
+
+    def __getitem__(self, indexes):
+        pauli_str = []
+        if isinstance(indexes, int):
+            indexes = [indexes]
+        for idx in indexes:
+            pauli_str.append(self._pauli_str[idx])
+        return Hamiltonian(pauli_str)
+
+    def __add__(self, other):
+        return Hamiltonian(self._pauli_str + other._pauli_str)
+
+    def __sub__(self, other):
+        return self.__add__(other.__mul__(-1))
+
+    def __mul__(self, other: float):
+        new_pauli_str = copy.deepcopy(self.pauli_str)
+        for pauli_operator in new_pauli_str:
+            pauli_operator[0] *= other
+        return Hamiltonian(new_pauli_str)
+
+    def _pauli_str_validation(self):
+        for pauli_operator in self._pauli_str:
+            self._pauli_operator_validation(pauli_operator)
+
+    def _pauli_operator_validation(self, pauli_operator):
+        assert isinstance(pauli_operator[0], int) or isinstance(
+            pauli_operator[0], float
+        ), "The coefficient of a Pauli operator must be integer or float."
+        self._coefficients.append(float(pauli_operator[0]))
+
+        assert len(pauli_operator) == len(
+            set(pauli_operator)
+        ), "The qubit indice of the same Pauli Gate cannot be the same."
+
+        indexes = []
+        pauli_gates = ""
+        for pauli_gate in pauli_operator[1:]:
+            assert (
+                pauli_gate[0] in ["X", "Y", "Z", "I"] and pauli_gate[1:].isdigit()
+            ), "The Pauli gate should be in the format of gate + index。 e.g. Z0, I5, Y3."
+            if pauli_gate[0] == "I":
+                continue
+            pauli_gates += pauli_gate[0]
+            indexes.append(int(pauli_gate[1:]))
+        self._qubit_indexes.append(indexes)
+        self._pauli_gates.append(pauli_gates)
+
+
+def main():
+    pauli_str = [[1, "Z0", "I2"], [0.05, "Z1", "Y2", "X4"]]
+    h = Hamiltonian(pauli_str)
+    print(h._pauli_gates)
+    print(h._qubit_indexes)
+
+
+if __name__ == "__main__":
+    main()
 
