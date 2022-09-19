1e53853f824c11002a76f6b8e7b0a9f559255819
Ziying Guo <guoziying@ict.ac.cn>
2022-09-19 17:24:11 +0800

    modify class hamiltonian and VQA

--- QuICT/algorithm/quantum_machine_learning/VQA/vqa.py
+++ QuICT/algorithm/quantum_machine_learning/VQA/vqa.py
@@ -1,25 +1,24 @@
-from signal import raise_signal
 import numpy as np
+import torch
 from utils.hamiltonian import Hamiltonian
 from torch.optim import Optimizer
 
 
 class VQA:
     def __init__(
-        self,
-        ansatz,
-        hamiltonian: Hamiltonian,
-        optimizer: Optimizer,
-        init_params: np.ndarray,
+        self, hamiltonian: Hamiltonian, init_params: np.ndarray,
     ):
-        self._ansatz = ansatz
         self._hamiltonian = hamiltonian
-        self._optimizer = optimizer
         self._init_params = init_params
 
-    def forward(self):
+    def cal_expect(self):
         raise NotImplementedError
 
-    def backward(self):
+    def construct_ansatz(self):
         raise NotImplementedError
 
+    def loss(self):
+        raise NotImplementedError
+
+    def run(self):
+        raise NotImplementedError
