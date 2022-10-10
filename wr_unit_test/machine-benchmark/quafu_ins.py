d0b1d014b86a48ce1b58384fce54024d60e1c535
wangrui <wangrui@example.com>
2022-10-10 12:33:06 +0800

    conda activate env_gpu

--- wr_unit_test/machine-benchmark/quafu_ins.py
+++ wr_unit_test/machine-benchmark/quafu_ins.py
@@ -1,7 +1,9 @@
 import os
 from copy import deepcopy
 from QuICT.core.circuit.circuit import Circuit
-from QuICT.qcda.mapping.mcts.mcts_mapping import MCTSMapping
+from QuICT.qcda.mapping.impl_wrapper import MCTSMapping
+from QuICT.qcda.mapping.mcts_legacy import MCTSMapping as MCTSMappingLegacy
+from QuICT.qcda.mapping.mcts_refactor import MCTSMapping as MCTSMappingRefactor
 from QuICT.qcda.optimization.auto_optimization import AutoOptimization
 from QuICT.qcda.synthesis import InstructionSet
 from QuICT.simulation.unitary import *
@@ -20,28 +22,37 @@ layout = Layout.load_file(
 qcda_workflow = QCDA()
 # qcda_workflow.add_default_synthesis(qf_iset)
 qcda_workflow.add_default_optimization()
-qcda_workflow.add_default_mapping(layout)
+# qcda_workflow.add_default_mapping(layout)
 
 single_typelist = [GateType.h, GateType.cx, GateType.rz]
 pro_typelist = [0.2,0.2,0.6]
 qubit_num = 5
 cir = Circuit(qubit_num)
-cir.random_append(rand_size=50, typelist=single_typelist, probabilities=pro_typelist)
+cir.random_append(rand_size=20, typelist=single_typelist, probabilities=pro_typelist, random_params=True)
 
+# sim = UnitarySimulator()
+# amp1 = sim.run(cir)
+# print(abs(amp1))
+
+MCTSMapping = MCTSMappingRefactor
 cir_ori = MCTSMapping(layout).execute(deepcopy(cir))
-cir_opt = qcda_workflow.compile(deepcopy(cir))
-# cir_map = MCTSMapping(layout).execute(cir_opt)
-
+circuit_ori = Circuit(5)
+cir_ori | circuit_ori
 
 sim = UnitarySimulator()
-amp1 = sim.run(cir_ori)
+amp1 = sim.run(circuit_ori)
 print(abs(amp1))
-print("\n")
-amp = sim.run(cir_opt)
-print(abs(amp))
 
-print(cir_ori.qasm())
-print(cir_opt.qasm())
+cir_opt = qcda_workflow.compile(deepcopy(cir_ori))
+cir_map = MCTSMapping(layout).execute(cir_opt)
+circuit_opt = Circuit(5)
+cir_map | circuit_opt
 
-print(cir_ori.size(), cir_opt.size())
-print(cir_ori.depth(), cir_opt.depth())
+
+
+
+print(circuit_ori.qasm())
+print(circuit_opt.qasm())
+
+print(circuit_ori.size(), circuit_opt.size())
+print(circuit_ori.depth(), circuit_opt.depth())
