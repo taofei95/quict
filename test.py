c2a559f111ca58b06422a972e50c203b601003d7
wangrui <wangrui@example.com>
2022-10-13 14:51:22 +0800

    quafu test load and KL

--- test.py
+++ test.py
@@ -34,56 +34,81 @@ user.save_apitoken('wRyYinzRHl-VDBRQkMWvi0GcQLpKUQVdMhou2iDtAGL.9JDMwczNxgjN2EjO
 # print(res.amplitudes) #amplitude
 # res.plot_amplitudes()
 
-from QuICT.tools.interface.qasm_interface import OPENQASMInterface
-#########################################################################
-cir = OPENQASMInterface.load_file("wr_unit_test/machine-benchmark/randomori.qasm").circuit
-qc = QuantumCircuit(5)
-test_cir = cir.qasm()
-qc.from_openqasm(test_cir)
+def load_circuit(path):
+    from QuICT.tools.interface.qasm_interface import OPENQASMInterface
+    #########################################################################
+    cir = OPENQASMInterface.load_file(path).circuit
 
-from quafu import Task
-task = Task()
-task.load_account()
-task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
-res = task.send(qc, name="ej")
+    return cir
 
-quafu_dict = res.amplitudes
-quafu_amp = [0] * (2 ** 5)
-for key, value in quafu_dict.items():
-    quafu_amp[int(key, 2)] = value
+
+def qcda_opt(cir):
+    pass
+
+
+def simu_circuit(cir):
+    sim = UnitarySimulator()
+    amp1 = sim.run(cir)
+    return amp1
+
+def quafu_run(cir):
+    qc = QuantumCircuit(5)
+    test_cir = cir.qasm()
+    qc.from_openqasm(test_cir)
+
+    from quafu import Task
+    task = Task()
+    task.load_account()
+    task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
+    res = task.send(qc, name="ej")
+
+    quafu_dict = res.amplitudes
+    quafu_amp = [0] * (2 ** 5)
+    for key, value in quafu_dict.items():
+        quafu_amp[int(key, 2)] = value
+
+    return quafu_amp
+
+
+path = "wr_unit_test/machine-benchmark/randomori.qasm"
+cir = load_circuit(path)
+amp = simu_circuit(cir)
+print(abs(amp))
+
+
 ########################################################################
-cir_opt = OPENQASMInterface.load_file("wr_unit_test/machine-benchmark/randomopt.qasm").circuit
-qc = QuantumCircuit(5)
-test_cir = cir_opt.qasm()
-qc.from_openqasm(test_cir)
+# cir_opt = OPENQASMInterface.load_file("wr_unit_test/machine-benchmark/randomopt.qasm").circuit
+# qc = QuantumCircuit(5)
+# test_cir = cir_opt.qasm()
+# qc.from_openqasm(test_cir)
 
-from quafu import Task
-task = Task()
-task.load_account()
-task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
-res = task.send(qc, name="ej1")
+# from quafu import Task
+# task = Task()
+# task.load_account()
+# task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
+# res = task.send(qc, name="ej1")
 
-quafu_dict2 = res.amplitudes
-quafu_amp2 = [0] * (2 ** 5)
-for key, value in quafu_dict2.items():
-    quafu_amp2[int(key, 2)] = value
+# quafu_dict2 = res.amplitudes
+# quafu_amp2 = [0] * (2 ** 5)
+# for key, value in quafu_dict2.items():
+#     quafu_amp2[int(key, 2)] = value
 
-#####################################################################
-sim = UnitarySimulator()
-amp2 = sim.run(cir_opt)
-amp1 = sim.run(cir)
+# #####################################################################
+# sim = UnitarySimulator()
+# amp2 = sim.run(cir_opt)
+# amp1 = sim.run(cir)
 
-import numpy as np
-import scipy.stats
-p = np.asarray(abs(amp1))
-q = np.asarray(abs(amp2))
+# import numpy as np
+# import scipy.stats
+# p = np.asarray(abs(amp1))
+# q = np.asarray(abs(amp2))
 
-m = np.asarray(quafu_amp)
-n = np.asarray(quafu_amp2)
+# m = np.asarray(quafu_amp)
+# n = np.asarray(quafu_amp2)
 
 
-def KL_divergence(p, q):
-    return scipy.stats.entropy(p, q, base=2)
-print((KL_divergence(p, q) + KL_divergence(q, p)) /2)
-print((KL_divergence(p, n) + KL_divergence(n, p)) /2)
-print((KL_divergence(p, m) + KL_divergence(m, p)) /2)
+# def KL_divergence(p, q):
+    # return scipy.stats.entropy(p, q, base=2)
+# print((KL_divergence(p, q) + KL_divergence(q, p)) /2)
+# print((KL_divergence(p, n) + KL_divergence(n, p)) /2)
+# print((KL_divergence(p, m) + KL_divergence(m, p)) /2)
--- wr_unit_test/machine-benchmark/quafu_test.py
+++ wr_unit_test/machine-benchmark/quafu_test.py
@@ -7,6 +7,8 @@ user.save_apitoken('wRyYinzRHl-VDBRQkMWvi0GcQLpKUQVdMhou2iDtAGL.9JDMwczNxgjN2EjO
 #build qasm
 import os
 from copy import deepcopy
+import numpy as np
+import scipy.stats
 from QuICT.core.circuit.circuit import Circuit
 from QuICT.qcda.mapping.impl_wrapper import MCTSMapping
 from QuICT.qcda.mapping.mcts_refactor import MCTSMapping as MCTSMappingRefactor
@@ -15,21 +17,26 @@ from QuICT.simulation.unitary import *
 from QuICT.core.utils.gate_type import GateType
 from QuICT.core.layout.layout import Layout
 
-if __name__=="__main__":
-    layout = Layout.load_file(
-        os.path.dirname(os.path.abspath(__file__)) + 
-        f"/line5.layout"
-    )
+layout = Layout.load_file(
+    os.path.dirname(os.path.abspath(__file__)) + 
+    f"/line5.layout"
+)
 
-    single_typelist = [GateType.h, GateType.rx, GateType.ry, GateType.rz] 
-    double_typelist = [GateType.cx]
-    len_s, len_d = len(single_typelist), len(double_typelist)
-    prob = [0.8 / len_s] * len_s + [0.2 / len_d] * len_d
+single_typelist = [GateType.h, GateType.rx, GateType.ry, GateType.rz] 
+double_typelist = [GateType.cx]
+len_s, len_d = len(single_typelist), len(double_typelist)
+prob = [0.8 / len_s] * len_s + [0.2 / len_d] * len_d
 
-    qubit_num = 5
-    cir = Circuit(qubit_num)
-    cir.random_append(rand_size=50, typelist=single_typelist + double_typelist, probabilities=prob, random_params=True)
+qubit_num = 5
+circuit = Circuit(qubit_num)
+circuit.random_append(rand_size=50, typelist=single_typelist + double_typelist, probabilities=prob, random_params=True)
 
+def simu_cir(cir):
+    sim = UnitarySimulator()
+    amp = sim.run(cir)
+    return amp
+
+def circuit_opt(cir):
     # mapping
     MCTSMapping = MCTSMappingRefactor
     cir_map = MCTSMapping(layout).execute(deepcopy(cir))
@@ -38,42 +45,80 @@ if __name__=="__main__":
     circuit_map.gate_decomposition()
     #opt
     cir_opt = AutoOptimization().execute(circuit_map)
-
-    # print(circuit_map.qasm())
-    # print(cir_opt.qasm())
-    # print(circuit_map.depth(),cir_opt.depth())
+    return cir_opt
     
-#111111111111111111111111111111111111
+def quafu_run(cir, name:str):
     qc = QuantumCircuit(5)
-    test_cir = circuit_map.qasm()
+    test_cir = cir.qasm()
     qc.from_openqasm(test_cir)
 
     from quafu import Task
     task = Task()
     task.load_account()
     task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
-    res = task.send(qc, name="1")
-
+    res = task.send(qc, name)
+    counts = res.counts
+    
     quafu_dict = res.amplitudes
     quafu_amp = [0] * (2 ** 5)
     for key, value in quafu_dict.items():
         quafu_amp[int(key, 2)] = value
+    return quafu_amp, counts
 
-#222222222222222222222222222222222222
-    qc = QuantumCircuit(5)
-    test_cir = cir_opt.qasm()
-    qc.from_openqasm(test_cir)
 
-    from quafu import Task
-    task = Task()
-    task.load_account()
-    task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
-    res = task.send(qc, name="11")
 
-    quafu_dict2 = res.amplitudes
-    quafu_amp2 = [0] * (2 ** 5)
-    for key, value in quafu_dict2.items():
-        quafu_amp2[int(key, 2)] = value
+
+run1 = quafu_run(circuit,"circuit")
+amp1 = simu_cir(circuit)
+
+cir_opt = circuit_opt(circuit)
+run2 = quafu_run(cir_opt,"circuit_opt")
+amp2 = simu_cir(cir_opt)
+
+p = np.asarray(abs(amp1))
+q = np.asarray(run1)
+
+def KL_divergence(p, q):
+    return scipy.stats.entropy(p, q, base=2)
+
+print((KL_divergence(p, q) + KL_divergence(q, p)) /2)
+print(abs(amp1))
+print(abs(amp2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #222222222222222222222222222222222222
+#     qc = QuantumCircuit(5)
+#     test_cir = cir_opt.qasm()
+#     qc.from_openqasm(test_cir)
+
+#     from quafu import Task
+#     task = Task()
+#     task.load_account()
+#     task.config(backend="ScQ-P10", shots=3000, compile=False, priority=2)
+#     res = task.send(qc, name="11")
+
+#     quafu_dict2 = res.amplitudes
+#     quafu_amp2 = [0] * (2 ** 5)
+#     for key, value in quafu_dict2.items():
+#         quafu_amp2[int(key, 2)] = value
 
 #3333333333333333333333333333333333333333
     # qc = QuantumCircuit(5)
@@ -91,23 +136,23 @@ if __name__=="__main__":
     # for key, value in quafu_dict3.items():
     #     quafu_amp3[int(key, 2)] = value
 
-    sim = UnitarySimulator()
-    amp1 = sim.run(circuit_map)
-    count_quict = sim.sample(3000)
-    # amp2 = sim.run(cir_opt)
+    # sim = UnitarySimulator()
+    # amp1 = sim.run(circuit_map)
+    # count_quict = sim.sample(3000)
+    # # amp2 = sim.run(cir_opt)
 
-    import numpy as np
-    import scipy.stats
-    p = np.asarray(abs(amp1))
-    q = np.asarray(quafu_amp)
-    n = np.asarray(quafu_amp2)
-    m = np.asarray(quafu_amp3)
+    # import numpy as np
+    # import scipy.stats
+    # p = np.asarray(abs(amp1))
+    # q = np.asarray(quafu_amp)
+    # n = np.asarray(quafu_amp2)
+    # # m = np.asarray(quafu_amp3)
 
-    def KL_divergence(p, q):
-        return scipy.stats.entropy(p, q, base=2)
+    # def KL_divergence(p, q):
+    #     return scipy.stats.entropy(p, q, base=2)
 
-    print((KL_divergence(p, q) + KL_divergence(q, p)) /2)
-    print((KL_divergence(p, n) + KL_divergence(n, p)) /2)
-    print((KL_divergence(p, m) + KL_divergence(m, p)) /2)
+    # print((KL_divergence(p, q) + KL_divergence(q, p)) /2)
+    # print((KL_divergence(p, n) + KL_divergence(n, p)) /2)
+    # print((KL_divergence(p, m) + KL_divergence(m, p)) /2)
 
 
