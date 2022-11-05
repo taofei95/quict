f188c62761a1762cbc7a3fbdc1941606b0008c1a
GaryChan <chenzihang15@mails.ucas.ac.cn>
2022-11-05 17:20:54 +0800

    template example and demo update

--- example/python/template_optimization.py
+++ example/python/template_optimization.py
@@ -1,29 +1,23 @@
+import random
+
 from QuICT.core import Circuit
 from QuICT.core.utils import GateType
-from QuICT.qcda.optimization.template_optimization.templates import (template_nct_2a_2,
-                                                                     template_nct_4a_3,
-                                                                     template_nct_5a_3,
-                                                                     template_nct_6a_1,
-                                                                     template_nct_9c_5,
-                                                                     template_nct_9d_4)
+from QuICT.lib.circuitlib import CircuitLib
 from QuICT.qcda.optimization.template_optimization import TemplateOptimization
 
 
 if __name__ == '__main__':
     circuit = Circuit(5)
-    circuit.random_append(50, typelist=[GateType.cx])
+    typelist = [GateType.x, GateType.cx, GateType.ccx,
+                GateType.h, GateType.s, GateType.t, GateType.sdg, GateType.tdg]
+    circuit.random_append(200, typelist=typelist)
     circuit.draw(filename='0.jpg')
 
-    templates = [template_nct_2a_2(),
-                 template_nct_4a_3(),
-                 template_nct_5a_3(),
-                 template_nct_6a_1(),
-                 template_nct_9c_5(),
-                 template_nct_9d_4()]
-    while True:
-        TO = TemplateOptimization(templates)
-        circuit_opt = TO.execute(circuit)
-        if circuit_opt.size() == circuit.size():
-            break
-        circuit = circuit_opt
-    circuit_opt.draw(filename='1.jpg')
+    template_list = CircuitLib.load_template_circuit()
+    TO = TemplateOptimization(
+        template_list=random.sample(template_list, 10),
+        heuristics_qubits_param=[10],
+        heuristics_backward_param=[3, 1]
+    )
+    circ_optim = TO.execute(circuit)
+    circ_optim.draw(filename='1.jpg')
