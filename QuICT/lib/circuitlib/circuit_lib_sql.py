fd8a2725b40c136292e444ed13a6afa6082779eb
wangrui <wangrui@example.com>
2022-11-15 16:04:31 +0800

    unpdate sql for random circuit

--- QuICT/lib/circuitlib/circuit_lib_sql.py
+++ QuICT/lib/circuitlib/circuit_lib_sql.py
@@ -37,7 +37,6 @@ class CircuitLibDB:
             "circuit_qasm",
             "template"
         )
-
         for file in filter(lambda x: x.endswith('.qasm'), os.listdir(file_path)):
             _, width, size, depth, _ = file.split("_")
             width = int(width[1:])
@@ -54,7 +53,37 @@ class CircuitLibDB:
 
         print(self._cursor.lastrowid)
 
+    def add_random_circuit(self):
+        file_path = os.path.join(
+            self._file_path,
+            "circuit_qasm",
+            "random"
+        )
+
+        for classify in os.listdir(file_path):
+            if classify == ".keep":
+                continue
+
+            folder = os.path.join(file_path, classify)
+            for file in filter(lambda x: x.endswith('.qasm'), os.listdir(folder)):
+                width, size, depth = file.split("_")
+                width = int(width[1:])
+                size = int(size[1:])
+                depth = int(depth[1:-5])
+
+                self._cursor.execute(
+                    "INSERT INTO CIRCUIT_LIB(NAME, TYPE, CLASSIFY, WIDTH, SIZE, DEPTH)" +
+                    f"VALUES (\'{file}\', \'random\', \'{classify}\', " +
+                    f"\'{width}\', \'{size}\', \'{depth}\')"
+                )
+
+        self._connect.commit()
+
+        print(self._cursor.lastrowid)
+
 
 clsql = CircuitLibDB()
 # clsql.clean()
 clsql.add_template_circuit()
+clsql.add_random_circuit()
+
