18e553c72048231b3e460b5a970d471fd0479d00
likaiqi <yinyangshiqiqiaiai@163.com>
2022-11-17 16:57:49 +0800

    rm useless script

--- QuICT/cloud/cli/script.sh
+++
@@ -1,8 +0,0 @@
-# Step 1: get which python
-$pythonpath = "which python" --> in terminal
-
-# Step 2: mv quict.py -> /bin/quict
-mv quict.py path/to/python/../bin/quict
-
-# Step 3: add chomd +x for quict
-chomd +x /bin/quict
