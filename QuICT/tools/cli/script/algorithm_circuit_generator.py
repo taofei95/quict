0b396c45b6993f689a7162ee7ff522b11c00df33
likaiqi <yinyangshiqiqiaiai@163.com>
2022-12-09 18:03:58 +0800

    update circuit script

--- QuICT/tools/cli/script/algorithm_circuit_generator.py
+++ QuICT/tools/cli/script/algorithm_circuit_generator.py
@@ -1,22 +1,19 @@
 import sys
 
-from QuICT.lib.circuitlib import CircuitLib
+from QuICT.tools.circuit_library import CircuitLib
 
 
 def alg_circuit_genertor(alg: str, qubits: str, output_path: str):
     # Convert qubits from str into List[int]
     str_q_list = qubits.split("-")
     int_q_list = [int(sq) for sq in str_q_list]
+    circuit_library = CircuitLib(output_type="file", output_path=output_path)
 
-    cir_list = CircuitLib().get_circuit("algorithm", alg, int_q_list)
-    for cir in cir_list:
-        file_name = f"{alg}_{cir.width()}.qasm"
-        with open(f"{output_path}/{file_name}", "w+") as f:
-            f.write(cir.qasm())
+    circuit_library.get_algorithm_circuit(alg, max_size=int_q_list)
+    # get_circuit(type, classify, width: list/slice?[list of qubits], max_size: int, max_depth: int [limitation the circuit's scale])
 
 
 if __name__ == "__main__":
-    print(sys.argv[1:])
     alg_circuit_genertor(
         *sys.argv[1:]
     )
--- QuICT/tools/cli/script/random_circuit_generator.py
+++ QuICT/tools/cli/script/random_circuit_generator.py
@@ -1,45 +1,22 @@
 import sys
 
 from QuICT.core import Circuit
-from QuICT.core.gate import GateType
+from QuICT.tools.circuit_library import CircuitLib
 
 
 def random_circuit_generator(
+    classify: str,
     qubits: str,
-    size: str,
-    random_param: bool,
-    instruction_set: str,
+    max_size: int,
+    max_depth: int,
     output_path: str
 ):
-    google_set = [GateType.sx, GateType.sy, GateType.sw, GateType.rx, GateType.ry, GateType.fsim]
-    ibmq_set = [GateType.rz, GateType.sx, GateType.x, GateType.cx]
-    ionq_set = [GateType.rx, GateType.ry, GateType.rz, GateType.rxx]
-    ustc_set = [GateType.rx, GateType.ry, GateType.rz, GateType.h, GateType.x, GateType.cx]
-
-    iset_mapping = {
-        "Google": google_set,
-        "USTC": ustc_set,
-        "IBMQ": ibmq_set,
-        "IONQ": ionq_set
-    }
-
-    gate_set = iset_mapping[instruction_set] if instruction_set != "random" else None
-    prob = [3 / (4 * (len(gate_set) - 1))] * (len(gate_set) - 1) + [1 / 4] if instruction_set != "random" else \
-        None
-
     # Convert qubits and size from str into List[int]
     str_q_list = qubits.split("-")
     int_q_list = [int(sq) for sq in str_q_list]
-    str_s_list = size.split("-")
-    int_s_list = [int(sq) for sq in str_s_list]
-    for q in int_q_list:
-        for s in int_s_list:
-            cir_file_name = f"{instruction_set}_{q}_{s}.qasm"
-            cir = Circuit(q)
-            cir.random_append(s, gate_set, random_param, prob)
+    circuit_library = CircuitLib(output_type="file", output_path=output_path)
 
-            with open(f"{output_path}/{cir_file_name}", "w+") as f:
-                f.write(cir.qasm())
+    circuit_library.get_random_circuit(classify, int_q_list, max_size, max_depth)
 
 
 if __name__ == "__main__":
