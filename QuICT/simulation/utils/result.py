38991a8feb5d549e7de0cbeeec8eb6a877e883eb
likaiqi <yinyangshiqiqiaiai@163.com>
2022-12-13 18:08:51 +0800

    refactoring simulator and result

--- QuICT/simulation/utils/result.py
+++ QuICT/simulation/utils/result.py
@@ -17,7 +17,8 @@ class Result:
         precision: str,
         circuit_record: bool,
         amplitude_record: bool,
-        options: dict
+        options: dict,
+        output_path: str = None
     ):
         self.device = device
         self.backend = backend
@@ -32,6 +33,8 @@ class Result:
 
         # prepare output path
         self._dump_folder = True if self._amplitude_record or self._circuit_record else False
+        if self._dump_folder:
+            self._output_path = self._prepare_output_file(output_path)
 
     def __str__(self):
         return f"Device: {self.device}\nBackend: {self.backend}\n" + \
@@ -53,10 +56,11 @@ class Result:
             }
         }
 
-    def _prepare_output_file(self):
+    def _prepare_output_file(self, output_path):
         """ Prepare output path. """
-        curr_path = os.getcwd()
-        output_path = os.path.join(curr_path, "output", self.id)
+        if output_path is None:
+            curr_path = os.getcwd()
+            output_path = os.path.join(curr_path, "output")
 
         if not os.path.exists(output_path):
             os.makedirs(output_path)
@@ -76,17 +80,14 @@ class Result:
             self.counts[bit_idx] = result[i]
 
         if self._dump_folder:
-            with open(f"{self.output_path}/result.log", "w") as of:
+            with open(f"{self._output_path}/result_{self.id}.log", "w") as of:
                 of.write(str(self.__dict__()))
 
     def record_circuit(self, circuit, circuit_name):
         """ dump the circuit. """
-        self.id = circuit_name
-        if self._dump_folder:
-            self.output_path = self._prepare_output_file()
-
+        self.id = circuit.name.split('_')[1]
         if self._circuit_record:
-            with open(f"{self.output_path}/circuit.qasm", "w") as of:
+            with open(f"{self._output_path}/circuit_{self.id}.qasm", "w") as of:
                 of.write(circuit.qasm())
 
     def record_amplitude(self, amplitude):
@@ -95,7 +96,7 @@ class Result:
             amplitude = amplitude.get()
 
         if self._amplitude_record:
-            np.savetxt(f"{self.output_path}/amplitude.txt", amplitude)
+            np.savetxt(f"{self._output_path}/amp_{self.id}.txt", amplitude)
 
         if self.backend == "density_matrix":
             self.density_matrix = amplitude.copy()
