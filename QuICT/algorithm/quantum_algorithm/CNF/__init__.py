f0708c726f39fbf4726a4be6ea8a98b5a880318b
Leo0x2A <1172077628@qq.com>
2022-12-20 11:40:28 +0800

    refactor: CNF

--- QuICT/algorithm/quantum_algorithm/CNF/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf8 -*-
-# @TIME    : 2022/7
-# @Author  : Guo Cheng
-# @File    : __init__.py
-
-from .cnf import CNFSATOracle
-#from .search_with_prior_knowledge import GroverWithPriorKnowledge
-#from .partial_grover import PartialGrover
