9ed7fb0aeb327eac9d3784d7c73db22ba091456d
Ziying Guo <guoziying@ict.ac.cn>
2023-01-10 17:14:19 +0800

    update quantum walk search

--- QuICT/algorithm/quantum_algorithm/__init__.py
+++ QuICT/algorithm/quantum_algorithm/__init__.py
@@ -1,4 +1,5 @@
 from .grover import *
 from .shor import *
+
 # from .amplitude_estimate import QAE, OracleInfo, StatePreparationInfo
 from .quantum_walk import Graph, QuantumWalk, QuantumWalkSearch
