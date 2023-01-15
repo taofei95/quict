620b7be6ad5dd52e96bd6c7ae51b0ef6fc0f1210
GaryChan <chenzihang15@mails.ucas.ac.cn>
2023-01-15 17:49:47 +0800

    halved qubits

--- QuICT/algorithm/quantum_chemistry/model/hartree_fock_net.py
+++ QuICT/algorithm/quantum_chemistry/model/hartree_fock_net.py
@@ -1,12 +1,12 @@
 import torch
+import numpy as np
 
 from QuICT.algorithm.quantum_machine_learning.ansatz_library import Thouless
-from QuICT.algorithm.quantum_machine_learning.model.VQA import VQENet
-from QuICT.algorithm.quantum_machine_learning.utils import Hamiltonian
+from QuICT.algorithm.quantum_machine_learning.utils import GpuSimulator, Hamiltonian
 
 
-class HartreeFockVQENet(VQENet):
-    """The network used by Hartree-Fock VQE with Thouless ansatz
+class HartreeFockVQENet(torch.nn.Module):
+    """The network used by restricted Hartree-Fock VQE with Thouless ansatz
     
     Reference:
         https://arxiv.org/abs/2004.04174
@@ -27,10 +27,16 @@ class HartreeFockVQENet(VQENet):
             device (torch.device, optional): The device to which the HartreeFockVQENet is assigned.
                 Defaults to torch.device("cuda:0").
         """
-        self.orbitals = orbitals
-        self.electrons = electrons
+        super().__init__()
+        # In restricted Hartree-Fock VQE, we assert the electrons are always in pairs.
+        self.orbitals = orbitals // 2
+        self.electrons = electrons // 2
+        # Thouless ansatz
         self.ansatz = Thouless(device)
-        super().__init__(orbitals, electrons, hamiltonian, device)
+        self.hamiltonian = hamiltonian
+        self.device = device
+        self.simulator = GpuSimulator()
+        self.define_network()
 
     def define_network(self):
         self.params = torch.nn.Parameter(
@@ -54,3 +60,52 @@ class HartreeFockVQENet(VQENet):
         else:
             state = self.simulator.forward(ansatz, state)
         return state
+
+    def loss_func(self, state):
+        """The loss function for VQE, which aims to minimize the expectation of H.
+
+        Args:
+            state (torch.Tensor): The state vector.
+
+        Returns:
+            torch.Tensor: Loss, which is equal to the expectation of H.
+        """
+        if isinstance(state, np.ndarray):
+            state = torch.from_numpy(state).to(self.device)
+        assert state.shape[0] == 1 << self.orbitals
+        state = self._expand_state(state)
+
+        ansatz_list = self.hamiltonian.construct_hamiton_ansatz(
+            2 * self.orbitals, self.device
+        )
+        coefficients = self.hamiltonian.coefficients
+        state_vector = torch.zeros(1 << (2 * self.orbitals), dtype=torch.complex128).to(
+            self.device
+        )
+        for coeff, ansatz in zip(coefficients, ansatz_list):
+            sv = ansatz.forward(state)
+            state_vector += coeff * sv
+        loss = torch.sum(state.conj() * state_vector).real
+
+        return loss
+
+    def _expand_state(self, state: torch.Tensor):
+        """
+        Expand the restricted state back to the original one
+
+        Args:
+            state(torch.Tensor): the restricted state vector
+
+        Returns:
+            torch.Tensor: expanded state vector
+        """
+        size = state.shape[0]
+        expanded = torch.zeros(size * size, dtype=torch.complex128, device=self.device)
+        for i in range(size):
+            i_bin = np.binary_repr(i, width=self.orbitals)
+            i_double = ''
+            for n in i_bin:
+                i_double += (n + n)
+            i_double = int(i_double, 2)
+            expanded[i_double] = state[i]
+        return expanded
