05e9d401c87bec3d2b6942d24c4b797a3deb9790
GaryChan <chenzihang15@mails.ucas.ac.cn>
2023-02-08 17:02:24 +0800

    unitary phase

--- unit_test/qcda/synthesis/unitary_decomposition_tests/test_unitary_decomposition_base_2.py
+++ unit_test/qcda/synthesis/unitary_decomposition_tests/test_unitary_decomposition_base_2.py
@@ -7,7 +7,7 @@ from QuICT.qcda.synthesis.unitary_decomposition import UnitaryDecomposition
 def test_unitary_decomposition_base_2():
     for qubit_num in range(2, 6):
         mat1 = unitary_group.rvs(1 << qubit_num)
-        UD = UnitaryDecomposition(recursive_basis=2)
+        UD = UnitaryDecomposition(True, recursive_basis=2)
         gates, _ = UD.execute(mat1)
         mat2 = gates.matrix()
         assert np.allclose(mat1, mat2)
