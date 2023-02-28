252a9ee30c0bd4d62c44d01b29fc527d991cfd73
likaiqi <yinyangshiqiqiaiai@163.com>
2023-02-28 16:47:03 +0800

    fixed cnf and benchmark bugs

--- example/algorithm/quantum_walk_search.py
+++ example/algorithm/quantum_walk_search.py
@@ -5,4 +5,4 @@ from QuICT.algorithm.tools.drawer.graph_drawer import *
 simulator = StateVectorSimulator()
 search = QuantumWalkSearch(simulator)
 sample = search.run(index_qubits=4, targets=[4], a_r=5 / 8, a_nr=1 / 8)
-draw_samples_with_auxiliary(sample, 4, 2)
+draw_samples_with_auxiliary(sample, 4, 2, save_path=".")
