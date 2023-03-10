805bbf564f1341327e30880282b5e73b6d6e93de
likaiqi <luke.qi@live.com>
2023-03-10 11:07:45 +0800

    add interface for quict-sim in QuICT

--- QuICT/simulation/state_vector/__init__.py
+++ QuICT/simulation/state_vector/__init__.py
@@ -1,1 +1,7 @@
 from .statevector_simulator import StateVectorSimulator
+
+try:
+    from QuICT_sim.cpu_simulator import CircuitSimulator as HPStateVecotrSimulator
+except:
+    HPStateVecotrSimulator = None
+    print("Please install quict_sim first, you can use 'pip install quict_sim' to install. ")
