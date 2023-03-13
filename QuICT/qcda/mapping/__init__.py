605a44440cf33c347fadd1bd21d039be3be335fd
Ziying Guo <guoziying@ict.ac.cn>
2023-03-13 13:45:58 +0800

    add QuICT-ml to init

--- QuICT/qcda/mapping/__init__.py
+++ QuICT/qcda/mapping/__init__.py
@@ -1,2 +1,11 @@
 from .mcts import MCTSMapping
 from .sabre import SABREMapping
+
+try:
+    from QuICT_ml.rl_mapping import rl_mapping as RLMapping
+
+except:
+    RLMapping = None
+    print(
+        "Please install pytorch, torch-geometric, torch-sparse, tensorboard, cupy and quict_ml first, you can use 'pip install quict-ml' to install quict_ml. "
+    )
