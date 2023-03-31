fixed some bugs and precision reset

QuICT/core/gate/utils/gate_matrix.py
@@ -4,13 +4,14 @@ from QuICT.core.utils import GateType
 
 
 class GateMatrixGenerator:
-    def get_matrix(self, gate, is_get_target: bool = False, special_array_generator=None):
+    def get_matrix(self, gate, precision: str = "double", is_get_target: bool = False, special_array_generator=None):
         # Step 1: Assigned array generator
         self._array_generator = special_array_generator if special_array_generator is not None else np
 
         # Step 2: Get based matrix's value
         gate_type = gate.type
-        gate_precision = np.complex128 if gate.precision == "double" else np.complex64
+        _precision = gate.precision if precision is None else precision
+        gate_precision = np.complex128 if _precision == "double" else np.complex64
         gate_params = gate.params
         if gate_params == 0:
             based_matrix = self.based_matrix(gate_type, gate_precision)
@@ -431,7 +432,9 @@ class InverseGate:
     def get_inverse_gate(cls, gate_type: GateType, pargs: list) -> tuple:
         if len(pargs) == 0:
             if gate_type in cls.__GATE_INVERSE_MAP.keys():
-                return cls.__GATE_INVERSE_MAP[gate_type], pargs
+                inverse_args = cls.__GATE_INVERSE_MAP[gate_type]
+
+                return inverse_args if isinstance(inverse_args, tuple) else (inverse_args, pargs)
         else:
             inv_params = None
             if gate_type in cls.__INVERSE_GATE_WITH_NEGATIVE_PARAMS:

QuICT/simulation/state_vector/statevector_simulator.py
@@ -13,9 +13,7 @@ from QuICT.core.gate import BasicGate, CompositeGate
 from QuICT.core.utils import GateType
 from QuICT.simulation.utils import GateSimulator
 from QuICT.tools.exception.core import ValueError, TypeError
-from QuICT.tools.exception.simulation import (
-    SampleBeforeRunError, StateVectorUnmatchedError
-)
+from QuICT.tools.exception.simulation import SampleBeforeRunError
 
 
 class StateVectorSimulator:
@@ -46,15 +44,7 @@ class StateVectorSimulator:
 
     @vector.setter
     def vector(self, vec):
-        if self._device == "GPU":
-            with self._array_helper.cuda.Device(self._device_id):
-                if type(vec) is np.ndarray:
-                    self._vector = self._array_helper.array(vec)
-                else:
-                    self._vector = vec
-        else:
-            assert isinstance(vec, np.ndarray)
-            self._vector = vec
+        self._vector = self._gate_calculator.validate_state_vector(vec, self._qubits)
 
     @property
     def device(self):
@@ -74,38 +64,23 @@ class StateVectorSimulator:
             raise ValueError("StateVectorSimulation.precision", "[single, double]", precision)
 
         self._device = device
-        self._precision = np.complex128 if precision == "double" else np.complex64
+        self._precision = precision
         self._device_id = gpu_device_id
         self._sync = sync
-        self._gate_calculator = GateSimulator(self._device, self._device_id, self._sync)
-
-        if self._device == "GPU":
-            import cupy as cp
-
-            self._array_helper = cp
-        else:
-            self._array_helper = np
+        self._gate_calculator = GateSimulator(self._device, self._precision, self._device_id, self._sync)
 
     def initial_circuit(self, circuit: Circuit):
         """ Initial the qubits, quantum gates and state vector by given quantum circuit. """
         self._circuit = circuit
         self._qubits = int(circuit.width())
-        self._pipeline = []
-
-        # TODO: think about precision
-        if self._precision != circuit._precision:
-            circuit.convert_precision()
-
         self._pipeline = circuit.fast_gates
 
     def initial_state_vector(self, all_zeros: bool = False):
         """ Initial qubits' vector states. """
-        vector_size = 1 << int(self._qubits)
-        self._vector = self._array_helper.zeros(vector_size, dtype=self._precision)
-        if self._device == "CPU" and not all_zeros:
-            self._vector[0] = self._precision(1)
-        elif self._device == "GPU" and not all_zeros:
-            self._vector.put(0, self._precision(1))
+        if not all_zeros:
+            self._vector = self._gate_calculator.get_allzero_state_vector(self._qubits)
+        else:
+            self._vector = self._gate_calculator.get_empty_state_vector(self._qubits)
 
     def run(
         self,
@@ -125,9 +100,7 @@ class StateVectorSimulator:
         """
         self.initial_circuit(circuit)
         if state_vector is not None:
-            assert 2 ** self._qubits == state_vector.size, \
-                StateVectorUnmatchedError("The state vector should has the same qubits with the circuit.")
-            self.vector = self._array_helper.array(state_vector, dtype=self._precision)
+            self._vector = self._gate_calculator.validate_state_vector(state_vector, self._qubits)
         elif not use_previous:
             self.initial_state_vector()
 
@@ -226,10 +199,9 @@ class StateVectorSimulator:
 
             complex_multiply(value, *default_parameters)
 
-    # TODO: multi-gpu kernel function
     def apply_zeros(self):
         """ Set state vector to be zero. """
-        self._vector = self._array_helper.zeros_like(self.vector)
+        self._vector = self._gate_calculator.get_empty_state_vector(self._qubits)
 
     def sample(self, shots: int = 1, target_qubits: list = None) -> list:
         """ Sample the measured result from current state vector, please first run simulator.run().

QuICT/simulation/utils/apply_gate.py
@@ -4,16 +4,19 @@ from QuICT.core.gate import BasicGate, GateMatrixGenerator
 from QuICT.core.utils import GateType, MatrixType
 from QuICT.ops.utils import LinAlgLoader
 from QuICT.ops.linalg.cpu_calculator import (
-    matrix_dot_vector, diagonal_matrix, swap_matrix, reverse_matrix, measure_gate_apply, reset_gate_apply
+    matrix_dot_vector, diagonal_matrix, swap_matrix, reverse_matrix,
+    measure_gate_apply, reset_gate_apply, get_measured_probability
 )
 from QuICT.tools.exception.core import GateQubitAssignedError
 from QuICT.tools.exception.simulation import GateTypeNotImplementError, GateAlgorithmNotImplementError
 
 
 class GateSimulator:
-    def __init__(self, device, gpu_device_id: int = 0, sync: bool = True):
+    def __init__(self, device, precision: str = "double", gpu_device_id: int = 0, sync: bool = True):
         self._gate_matrix_generator = GateMatrixGenerator()
         self._device = device
+        self._precision = precision
+        self._dtype = np.complex128 if precision == "double" else np.complex64
         self._gpu_device_id = gpu_device_id
         self._sync = sync
         if self._device == "GPU":
@@ -21,12 +24,39 @@ class GateSimulator:
 
             self._array_helper = cp
             self._algorithm = LinAlgLoader(device="GPU", enable_gate_kernel=True, enable_multigpu_gate_kernel=False)
+        else:
+            self._array_helper = np
 
+    ####################################################################
+    ############          State Vector Generator            ############
+    ####################################################################
+    def get_empty_state_vector(self, qubits: int):
+        return self._array_helper.zeros(1 << qubits, dtype=self._dtype)
+
+    def get_allzero_state_vector(self, qubits: int):
+        state_vector = self.get_empty_state_vector(qubits)
+        state_vector[0] = self._dtype(1)
+
+        return state_vector
+
+    def validate_state_vector(self, state_vector, qubits: int):
+        assert 1 << qubits == state_vector.size, "The state vector should has the same qubits with the circuit."
+        if not type(state_vector) is self._array_helper.ndarray:
+            state_vector = self._array_helper.array(state_vector, dtype=self._dtype)
+
+        if state_vector.dtype != self._dtype:
+            state_vector = state_vector.astype(self._dtype)
+
+        return state_vector
+
+    ####################################################################
+    ############           Gate Matrix Generator            ############
+    ####################################################################
     def _get_gate_matrix(self, gate: BasicGate):
         if self._device == "CPU":
-            return self._gate_matrix_generator.get_matrix(gate)
+            return self._gate_matrix_generator.get_matrix(gate, precision=self._precision)
         else:
-            return self._gate_matrix_generator.get_matrix(gate, special_array_generator=self._array_helper)
+            return self._gate_matrix_generator.get_matrix(gate, precision=self._precision, special_array_generator=self._array_helper)
 
     def apply_gate(
         self,
@@ -59,10 +89,11 @@ class GateSimulator:
     ):
         matrix_type = gate.matrix_type
         args_num = gate.controls + gate.targets
+        matrix = self._get_gate_matrix(gate)
         control_idx = np.array(cargs, dtype=np.int64)
         target_idx = np.array(targs, dtype=np.int64)
         default_params = (
-            state_vector, qubits, gate.matrix, args_num, control_idx, target_idx
+            state_vector, qubits, matrix, args_num, control_idx, target_idx
         )
 
         if matrix_type in [MatrixType.diag_diag, MatrixType.diagonal, MatrixType.control]:
@@ -78,7 +109,7 @@ class GateSimulator:
             matrix_dot_vector(
                 state_vector,
                 qubits,
-                gate.matrix,
+                matrix,
                 gate.controls + gate.targets,
                 np.append(target_idx, control_idx)
             )