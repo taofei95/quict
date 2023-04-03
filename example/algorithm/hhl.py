ffbec6adf407610b7a279144c567fc8ce44381a3
b0sseee <b0sseee@163.com>
2023-04-03 22:31:02 +0800

    Supplement unit_test

--- example/algorithm/hhl.py
+++ example/algorithm/hhl.py
@@ -1,23 +1,40 @@
 import numpy as np
+from scipy import stats, sparse
 
 from QuICT.algorithm.quantum_algorithm.hhl import HHL
 
 from QuICT.simulation.state_vector import StateVectorSimulator
 
-from example.algorithm.random_sparse import RS
 
-n = 2 ** 5
-A = RS(n, 1).matrix()
-b = np.ones(n, dtype=np.complex128)
+def random_matrix(size):
+    rvs = stats.norm().rvs
+    while(1):
+        X = sparse.random(
+            size, size, density=1, data_rvs=rvs,
+            dtype=np.complex128)
+        A = X.todense()
+        v = np.linalg.eigvals(A)
+        A = np.round(A, 3)
+        if np.linalg.det(A) != 0 and np.log2(max(abs(v)) / min(abs(v))) < 6:
+            return np.array(A)
+        
+def random_vector(size):
+    return np.complex128(np.round(np.random.rand(size), 3) -  np.full(size, 0.5))
 
-slt = np.linalg.solve(A, b)
-slt /= np.linalg.norm(slt)
-hhl_u = HHL(StateVectorSimulator(device="GPU")).run(
-        matrix=A,
-        vector=b)
-if hhl_u is not None:
-        hhl_u /= np.linalg.norm(hhl_u)
-        print(f"solution     = {slt.real}\n" +
-            f"hhl(unitary) = {hhl_u.real}")
-else: 
-        print("Failed.")
+n = 2 ** 4
+A = random_matrix(n)
+b = random_vector(n)
+
+# slt = np.linalg.solve(A, b)
+# slt /= np.linalg.norm(slt)
+# hhl_u = HHL(StateVectorSimulator(device="GPU")).run(
+#         matrix=A,
+#         vector=b)
+# if hhl_u is not None:
+#         hhl_u /= np.linalg.norm(hhl_u)
+#         print(f"solution     = {slt.real}\n" +
+#             f"hhl(unitary) = {hhl_u.real}")
+# else: 
+#         print("Failed.")
+
+print(1 << 2 + 1)
