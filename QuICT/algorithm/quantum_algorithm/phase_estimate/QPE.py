eae154f910c870791b7663bf7f0b4d409a1be955
PSR <pengsirui18@mails.ucas.ac.cn>
2023-04-06 13:50:26 +0800

    style: update according to pr comments

--- QuICT/algorithm/quantum_algorithm/phase_estimate/QPE.py
+++ QuICT/algorithm/quantum_algorithm/phase_estimate/QPE.py
@@ -1,51 +1,73 @@
 from QuICT.core import Circuit
-from QuICT.core.gate import *
+from QuICT.core.gate import CompositeGate, H, Swap, IQFT
 from QuICT.simulation.state_vector import StateVectorSimulator
+from QuICT.tools import Logger
 import numpy as np
 
 
+logger = Logger("QPE")
+
+
 class QPE:
-    def __init__(
+    def __init__(self, simulator=StateVectorSimulator()) -> None:
+        self.simulator = simulator
+        self._circuit_cache = None
+
+    def circuit(
         self,
         workbits: list,
         trickbits: list,
         workbits_state_preparation: CompositeGate,
         controlled_unitary,
-        simulator=StateVectorSimulator()
-    ) -> None:
-        self.simulator = simulator
-        self.trickbits = trickbits
-        self.n = len(workbits)
-        self.n_phase = len(trickbits)
+    ):
+        """QPE circuit construction.
+
+        Args:
+            workbits (list): indices for working qubits
+            trickbits (list): indices for phase estimation ancillae
+            workbits_state_preparation (CompositeGate): composite gate that prepares initial state in working qubits
+            controlled_unitary (int->CompositeGate): a function that returns a composite gate, input parameter as its\
+            repetition times
+
+        Returns:
+            Circuit: the QPE circuit (without measure)
+        """
+        n = len(workbits)
+        n_phase = len(trickbits)
         # contruct circuit
-        circ = Circuit(self.n + self.n_phase)
+        circ = Circuit(n + n_phase)
         # Hadamard Transform
         for idx in trickbits:
             H | circ(idx)
         # controlled unitary, EXPECT idx 0 to be the control bit
         workbits_state_preparation | circ(workbits)
-        for k in range(self.n_phase):
-            controlled_unitary(1<<(self.n_phase-1-k)) | circ(
-                [trickbits[k]] + workbits
-            )
+        for k in range(n_phase):
+            controlled_unitary(1 << (n_phase - 1 - k)) | circ([trickbits[k]] + workbits)
         # IQFT
-        for k in range(self.n_phase // 2):
-            Swap | circ([trickbits[k], trickbits[self.n_phase - 1 - k]])
-        IQFT.build_gate(self.n_phase) | circ(trickbits)
-        # for idx in trickbits:
-        #     Measure | circ(idx)
-        self._circuit_cache = circ
+        for k in range(n_phase // 2):
+            Swap | circ([trickbits[k], trickbits[n_phase - 1 - k]])
+        IQFT.build_gate(n_phase) | circ(trickbits)
+        return circ
 
-    def circuit(self):
-        return self._circuit_cache
-    
-    def run(self, shots = 100):
-        self.simulator.run(self.circuit())
+    def run(
+        self,
+        workbits: list,
+        trickbits: list,
+        workbits_state_preparation: CompositeGate,
+        controlled_unitary,
+        shots=100,
+    ):
+        self.simulator.run(
+            self.circuit(
+                workbits, trickbits, workbits_state_preparation, controlled_unitary
+            )
+        )
         result = self.simulator.sample(shots)
-        observed = bin(np.argmax(np.array(result)))[2:].rjust(self.n+self.n_phase, '0')
+        n = len(workbits)
+        n_phase = len(trickbits)
+        observed = bin(np.argmax(np.array(result)))[2:].rjust(n + n_phase, "0")
         trickbits_observed = ""
-        for bit in self.trickbits:
+        for bit in trickbits:
             trickbits_observed += observed[bit]
-        print(trickbits_observed)
-        return int(trickbits_observed, 2) / (1<<self.n_phase)
-
+        logger.info(f"most possible trickbits: {trickbits_observed}")
+        return int(trickbits_observed, 2) / (1 << n_phase)
--- unit_test/algorithm/quantum_algorithm/QPE_unit_test.py
+++ unit_test/algorithm/quantum_algorithm/QPE_unit_test.py
@@ -1,22 +1,24 @@
-import pytest
 import math
+import numpy as np
 
 from QuICT.algorithm.quantum_algorithm import QPE
-
-from QuICT.core.gate import *
+from QuICT.core.gate import X, CompositeGate, CRz, CZ, CH, CX, H
 from QuICT.simulation.state_vector import StateVectorSimulator
 
-def test_QPE_0():
+
+def test_QPE_Rz_phase():
     for proportion in [0.25, 0.5, 0.75]:
         n = 3
         m = 2
         workbits = list(range(m))
-        trickbits = list(range(m,m+n))
-        constructor = lambda power: CRz(power*proportion*4*math.pi)
+        trickbits = list(range(m, m + n))
+        constructor = lambda power: CRz(power * proportion * 4 * math.pi)
         state_prep = CompositeGate()
         with state_prep:
             X | state_prep(0)
-        proportion_esti = QPE(workbits, trickbits, state_prep, constructor, StateVectorSimulator(device="GPU")).run()
+        proportion_esti = QPE(StateVectorSimulator()).run(
+            workbits, trickbits, state_prep, constructor
+        )
         print(f"phase/2π: {proportion_esti}")
         assert math.isclose(proportion, proportion_esti)
 
@@ -24,6 +26,7 @@ def test_QPE_0():
 # f(x)=x1⊕x2...⊕xn
 def _example_grover_operator(n, power):
     from QuICT.core.gate.backend import MCTOneAux
+
     cgate = CompositeGate()
     # S_chi
     for i in range(1, n + 1):
@@ -43,24 +46,27 @@ def _example_grover_operator(n, power):
     # A
     for i in range(1, n + 1):
         CH | cgate([0, i])
-    
+
     # repeat
     cgate_final = CompositeGate()
     for _ in range(power):
         cgate | cgate_final
     return cgate_final
 
-def test_QPE_1():
-    for m in [3,4,5]:
+
+def test_QPE_counting():
+    for m in [3, 4, 5]:
         n = 4
         workbits = list(range(m))
-        trickbits = list(range(m,m+n))
-        constructor = lambda power: _example_grover_operator(m-1, power)
+        trickbits = list(range(m, m + n))
+        constructor = lambda power: _example_grover_operator(m - 1, power)
         state_prep = CompositeGate()
         with state_prep:
             for i in workbits:
                 H | state_prep(i)
-        proportion_esti = QPE(workbits, trickbits, state_prep, constructor, StateVectorSimulator(device="GPU")).run()
+        proportion_esti = QPE(StateVectorSimulator()).run(
+            workbits, trickbits, state_prep, constructor
+        )
         amp = 1 - np.sin(np.pi * proportion_esti) ** 2
         print(f"amplitude: {amp:.3f}")
-        assert abs(0.5 - amp) < 1/2**n
+        assert abs(0.5 - amp) < 1 / 2**n
