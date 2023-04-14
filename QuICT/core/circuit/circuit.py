c6a819a81cc5433540344394a123be73c3b5f11f
likaiqi <yinyangshiqiqiaiai@163.com>
2023-04-14 14:15:16 +0800

    fixed circuit to cgate bugs

--- QuICT/core/circuit/circuit.py
+++ QuICT/core/circuit/circuit.py
@@ -293,6 +293,8 @@ class Circuit(CircuitBased):
 
         if isinstance(gates, Circuit):
             gates = gates.to_compositegate()
+            if gates.width() != len(gate_qidxes):
+                gate_qidxes = [gate_qidxes[idx] for idx in gates.qubits]
 
         self._gates.append((gates, gate_qidxes, gates.size()))
         self._pointer = None
