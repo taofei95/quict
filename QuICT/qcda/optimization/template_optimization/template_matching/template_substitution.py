c9ffc413533c991fc3e572a42530e713521a5c13
Li Longcheng <lilongcheng18@mails.ucas.ac.cn>
2023-04-20 10:20:49 +0800

    temporary CircuitCost for template_substitution

--- QuICT/qcda/optimization/template_optimization/template_matching/template_substitution.py
+++ QuICT/qcda/optimization/template_optimization/template_matching/template_substitution.py
@@ -2,15 +2,87 @@ from functools import cached_property
 from itertools import chain
 from typing import List
 
+from QuICT.core.utils import GateType
+
 try:
     from collections.abc import Iterable
 except ImportError:
     from collections import Iterable
 
 from QuICT.core import Circuit
-from QuICT.qcda.utility.circuit_cost import StaticCircuitCost as CircuitCost
 
 from .matching_dag_circuit import Match, MatchingDAGCircuit, MatchingDAGNode
+
+
+class CircuitCostMeasure(object):
+    """
+    Measure of gate cost in quantum circuit.
+    """
+
+    NISQ_GATE_COST = {
+        GateType.id: 0, GateType.x: 1, GateType.y: 1, GateType.z: 1, GateType.h: 1,
+        GateType.t: 1, GateType.tdg: 1, GateType.s: 1, GateType.sdg: 1, GateType.u1: 1,
+        GateType.u2: 2, GateType.u3: 2, GateType.rx: 1, GateType.ry: 1, GateType.rz: 1,
+        GateType.cx: 2, GateType.cy: 4, GateType.cz: 4, GateType.ch: 8, GateType.swap: 6,
+        GateType.cswap: 8, GateType.rxx: 9, GateType.ryy: 9, GateType.rzz: 5,
+        GateType.cu3: 10, GateType.ccx: 21, GateType.measure: 9
+    }
+
+    # FIXME fill other gates
+    FT_GATE_COST = {
+        GateType.t: 1, GateType.tdg: 1
+    }
+
+    def __init__(self, cost_dict=None, target_device='nisq'):
+        """
+        Args:
+            cost_dict(Dict[GateType, int]): Cost of each gate type. It a gate type is not specified,
+                its cost is treated as 0.
+            target_device(str): Type of target device. Support 'nisq' and 'fault_tolerant'. It will
+                be ignored if `cost_dict` is given.
+        """
+        if cost_dict is not None:
+            self.cost_dict = cost_dict
+        elif target_device == 'nisq':
+            self.cost_dict = self.NISQ_GATE_COST
+        elif target_device == 'fault_tolerant':
+            self.cost_dict = self.FT_GATE_COST
+        else:
+            assert f'target device {target_device} not supported. It must be nisq or fault_tolerant'
+
+    def __getitem__(self, gate_type):
+        """
+        Get cost of a gate type. Subscript can be a GateType or string of a gate type.
+
+        Args:
+            gate_type(GateType/str): Gate type
+
+        Returns:
+            int: Cost of the gate type
+        """
+        if isinstance(gate_type, str):
+            gate_type = GateType(gate_type)
+
+        if gate_type in self.cost_dict:
+            return self.cost_dict[gate_type]
+        else:
+            return 0
+
+    def cost(self, circuit):
+        """
+        Calculate the cost of a circuit.
+
+        Args:
+            circuit(DAGCircuit/Circuit/CompositeGate/Iterable[BasicGate]/Iterable[DAGCircuitNode]): the circuit
+
+        Returns:
+            int: Cost of the circuit
+        """
+
+        if isinstance(circuit, Iterable):
+            return sum(self[n.gate.type] for n in circuit)
+        else:
+            return sum(self[n.gate.type] for n in circuit.gates)
 
 
 class Substitution:
@@ -21,7 +93,7 @@ class Substitution:
                  circuit: MatchingDAGCircuit,
                  template: MatchingDAGCircuit,
                  match: Match,
-                 measure: CircuitCost):
+                 measure: CircuitCostMeasure):
         """
         Args:
             circuit(MatchingDAGCircuit): the circuit to match
@@ -122,7 +194,7 @@ class TemplateSubstitution:
                        circuit: MatchingDAGCircuit,
                        template: MatchingDAGCircuit,
                        match_list: List[Match],
-                       measure: CircuitCost):
+                       measure: CircuitCostMeasure):
         """
         Greedily calculate a maximal and compatible sub list for found matches.
         """
@@ -160,7 +232,7 @@ class TemplateSubstitution:
                 circuit: MatchingDAGCircuit,
                 template: MatchingDAGCircuit,
                 match_list: List[Match],
-                measure: CircuitCost):
+                measure: CircuitCostMeasure):
 
         sub_list = cls._calc_sub_list(circuit, template, match_list, measure)
 
