ad5268c2ee57995d73ffd553385e95a0eefaf19f
b0sseee <b0sseee@163.com>
2023-04-20 16:41:03 +0800

    modify MSE

--- example/algorithm/hhl.py
+++ example/algorithm/hhl.py
@@ -4,22 +4,41 @@ from QuICT.algorithm.quantum_algorithm.hhl import HHL
 from QuICT.simulation.state_vector import StateVectorSimulator
 
 
+def matrix(bits, cond_bits):
+    n = 1 << bits
+    eps = 1e-8
+    while 1:
+        m = np.random.rand(n, n)
+        if np.abs(np.linalg.det(m)) > eps:
+            ev = np.abs(np.linalg.eigvals(m))
+            if np.max(ev) / np.min(ev) < (1 << cond_bits):
+                return m
+
+
+def vector(bits):
+    n = 1 << bits
+    eps = 1e-8
+    while 1:
+        v = np.random.rand(n)
+        if abs(v.dot(v)) != eps:
+            return v
+
+
 def MSE(x, y):
     n = len(x)
-    res0 = np.linalg.norm(x + y) / n
-    res1 = np.linalg.norm(x - y) / n
+    res0 = np.dot(x - y, x - y) / n
+    res1 = np.dot(x + y, x + y) / n
     return min(res0, res1)
 
 
-A = np.array([[1.0 + 0j, 2.0 + 0j],
-              [3.0 + 0j, 2.0 + 0j]])
-b = np.array([1.0 + 0j, -2.0 + 0j])
+A = matrix(2, 2)
+b = vector(2)
 
 slt = np.linalg.solve(A, b)
 slt /= np.linalg.norm(slt)
 
 hhl_a = HHL(StateVectorSimulator(device="GPU")).run(
-            A, b, phase_qubits=6, measure=False
+            A, b, phase_qubits=7, measure=False
         )
 hhl_a /= np.linalg.norm(hhl_a)
 
@@ -28,7 +47,7 @@ hhl = None
 HHL_m = HHL(StateVectorSimulator(device="GPU"))
 while(hhl is None):
     hhl = HHL_m.run(
-            A, b, phase_qubits=6
+            A, b, phase_qubits=7
         )
     time += 1
 
