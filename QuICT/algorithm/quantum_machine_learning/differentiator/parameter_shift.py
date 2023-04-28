852d498cb838384617cfd755b6e6d21720dde51f
denghaipeng <d2406785052@163.com>
2023-04-28 13:59:47 +0800

    modified parameter_shift.py

--- QuICT/algorithm/quantum_machine_learning/differentiator/parameter_shift.py
+++ QuICT/algorithm/quantum_machine_learning/differentiator/parameter_shift.py
@@ -1,22 +1,65 @@
 import numpy as np
+from QuICT.core.gate import *
 from QuICT.core import Circuit
-from QuICT.simulation.simulator import Simulator
-class ParameterShift: 
-    def __init__(self,cir:Circuit,sim :Simulator) -> None:
+from QuICT.algorithm.quantum_machine_learning.utils import Hamiltonian
+from QuICT.simulation.state_vector.statevector_simulator import StateVectorSimulator
+from QuICT.algorithm.quantum_machine_learning.differentiator.adjoint.adjoint_differentiator import AdjointDIfferentiator
+from QuICT.algorithm.quantum_machine_learning.utils.expectation import Expectation
+def get_expe(sv,hamiltonian,n_qubit):
+        bra = sv.copy().conj()
+        ket = sv.copy()
+        e_val = np.dot(
+            np.dot(
+                bra,
+                hamiltonian.get_hamiton_matrix(
+                    n_qubits=n_qubit
+                ),
+            ),
+            ket,
+        ).real
+        # e_val = np.dot(e_val, ket).real
+        return e_val
+
+class ParameterShift(AdjointDifferentiator): 
+    def __init__(self) -> None:
+        super().__init__()
         """
         Args:
         cir(Circuit): the circuit which is needed to calculate gradient
         """
-        self._cir =cir
-        self._sim =sim
-    def get_grad(self,idx_gate:int,idx_param:int):
-        e_shift = np.array( [0.0 ,0.0]  )
-        for i in range(2):
-            e_shift[i]=self.get_param_shift(idx_gate,idx_param,i)
-        grad = 1.0*(e_shift[1]-e_shift[0])
-   
-        return grad
-    def get_param_shift(self,idx_gate:int,idx_param:int,shift_type:int):
+        self._gate_calculator = StateVectorSimulator(
+            self._device, self._precision, self._device_id, self._sync
+        )
+    def run(
+        self,
+        circuit: Circuit,
+        variables: Variable,
+        state_vector: np.ndarray,
+        expectation_op: Hamiltonian,
+    ):
+        self._circuit = circuit
+        self._variables = variables
+        self._expectation_op =  expectation_op
+
+        for gate in self._circuit.fast_gates:
+            if gate.variables == 0:
+                continue
+            for  i in range(gate.variables):
+                e_shift = np.array( [0.0 ,0.0]  )
+                for j in range(2):
+                    e_shift[j]=self.get_param_shift(gate,i,j)
+                grad = 1.0*(e_shift[1]-e_shift[0])
+
+                gate.pargs[i].grads = (
+                    grad
+                    if abs(gate.pargs[i].grads) < 1e12
+                    else grad*gate.pargs[i].grads
+                )
+                index_global = gate.pargs[i].index
+                self._variables.grads[index_global] += gate.pargs[i].grads
+
+    
+    def get_param_shift(self,gate,idx_param:int,shift_type:int):
         """
         Args:
         shift_type(int): 0 for left shift and 1 for right shift
@@ -24,23 +67,17 @@ class ParameterShift:
         if shift_type !=0 and shift_type != 1:
             raise  ValueError("shift_type must be 0 or 1")
         i =idx_param
-        gate = self._cir.gates[idx_gate]
         sim = self._sim
         shift = 0.5
         shift_type = 1.0 if shift_type==1 else -1.0
-        param_list = gate.pargs.copy()
-        old_param = param_list[i]
-        shift_param = param_list[i]+shift*shift_type
 
-        param_list[i] = shift_param
-        gate.pargs = param_list.copy()
-        self._cir.replace_gate(idx_gate, gate)
-        e_val,_ = sim.forward(self._cir)
-
-        param_list[i] = old_param
-        gate.pargs = param_list.copy()
-        self._cir.replace_gate(idx_gate, gate)
+        old_param = gate.pargs[i].copy()
+        gate.pargs[i]= gate.pargs[i]+shift*shift_type
+        sv = sim.run(self._circuit)
+        e_val= get_expe(sv,hamiltonian=self._expectation_op,n_qubit=self._circuit.width())
+        gate.pargs[i] = old_param
 
         return e_val
+
 
     
