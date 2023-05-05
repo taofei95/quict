a7fc1f0250a2e5c2b1a70bca85ec322ed01006a0
b0sseee <b0sseee@163.com>
2023-05-05 16:13:51 +0800

    #

---
+++ QuICT/algorithm/quantum_algorithm/qsvt/__init__.py
@@ -0,0 +1,1 @@
+from .qsvt import QuantumSigularValueTransform
---
+++ QuICT/algorithm/quantum_algorithm/qsvt/qsvt.py
@@ -0,0 +1,11 @@
+
+
+class QuantumSigularValueTransform:
+    def  __init__(self) -> None:
+        pass
+
+    def circuit(self):
+        pass
+
+    def run(self):
+        pass
