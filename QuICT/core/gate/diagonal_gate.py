7e943f2d6294423ef915bc83ace8d03a6700fef1
GaryChan <chenzihang15@mails.ucas.ac.cn>
2023-07-18 11:01:46 +0800

    Stage 1

--- QuICT/core/gate/diagonal_gate.py
+++ QuICT/core/gate/diagonal_gate.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-from QuICT.core.gate import CompositeGate
+from QuICT.core.gate import CompositeGate, CX
+from QuICT.tools import Logger
+
+
+_logger = Logger("DiagonalGate")
 
 
 class DiagonalGate(object):
@@ -16,7 +20,11 @@ class DiagonalGate(object):
             target(int): number of target qubits
             aux(int, optional): number of auxiliary qubits
         """
+        self._logger = _logger
         self.target = target
+        if np.mod(aux, 2) != 0:
+            self._logger.warn('Algorithm serves for even number of auxiliary qubits. One auxiliary qubit is dropped.')
+            aux = aux - 1
         self.aux = aux
 
     def __call__(self, angles):
@@ -29,18 +37,36 @@ class DiagonalGate(object):
         """
         assert len(angles) == 2 ** self.target, ValueError('Incorrect number of angles')
         if self.aux == 0:
-            return self.no_aux_qubit()
+            return self.no_aux_qubit(angles)
         else:
-            return self.with_aux_qubit()
+            return self.with_aux_qubit(angles)
 
-    def no_aux_qubit(self):
+    def no_aux_qubit(self, angles):
         """
+        Args:
+            angles(listLike): list of (2 ** target) angles of rotation in the diagonal gate
+
         Returns:
             CompositeGate: diagonal gate without auxiliary qubit
         """
 
-    def with_aux_qubit(self):
+    def with_aux_qubit(self, angles):
         """
+        Args:
+            angles(listLike): list of (2 ** target) angles of rotation in the diagonal gate
+
         Returns:
             CompositeGate: diagonal gate with auxiliary qubit at the end of qubits
         """
+        n = self.target
+        m = self.aux
+        gates = CompositeGate()
+
+        # Stage 1: Prefix Copy
+        t = np.floor(np.log2(m / 2))
+        copies = np.floor(m / (2 * t))
+        for j in range(copies):
+            for i in range(t):
+                CX & [i, n + i * copies + j] | gates
+
+        # Stage 2: Gray Initial
