8d695e7a386a335ba83bace47faecbb34120db22
Ziying Guo <guoziying@ict.ac.cn>
2023-07-19 17:32:17 +0800

    del parameter shift differentiator

--- QuICT/algorithm/quantum_machine_learning/differentiator/differentiator.py
+++ QuICT/algorithm/quantum_machine_learning/differentiator/differentiator.py
@@ -7,14 +7,11 @@ from QuICT.algorithm.quantum_machine_learning.utils import Hamiltonian
 from QuICT.algorithm.quantum_machine_learning.differentiator.adjoint import (
     AdjointDifferentiator,
 )
-from QuICT.algorithm.quantum_machine_learning.differentiator.parameter_shift import (
-    ParameterShift,
-)
 
 
 class Differentiator:
     __DEVICE = ["CPU", "GPU"]
-    __BACKEND = ["adjoint", "parameter_shift"]
+    __BACKEND = ["adjoint"]
     __PRECISION = ["single", "double"]
     __OPTIONS_DICT = {
         "adjoint": ["gpu_device_id"],
@@ -63,10 +60,6 @@ class Differentiator:
     def _load_differentiator(self):
         if self._backend == "adjoint":
             differentiator = AdjointDifferentiator(
-                self._device, self._precision, **self._options
-            )
-        elif self._backend == "parameter_shift":
-            differentiator = ParameterShift(
                 self._device, self._precision, **self._options
             )
         else:
