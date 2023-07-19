af43827d2db66fdc59b1aeae88cbcda1ce37bc38
Ziying Guo <guoziying@ict.ac.cn>
2023-07-19 15:46:13 +0800

    add binary cross entropy loss

--- QuICT/algorithm/quantum_machine_learning/utils/loss.py
+++ QuICT/algorithm/quantum_machine_learning/utils/loss.py
@@ -18,13 +18,19 @@ class Loss(ABC):
     def __call__(self, pred, target):
         raise NotImplementedError
 
-    @abstractmethod
     def gradient(self):
-        raise NotImplementedError
+        """Calculate the gradient of the loss function to the predicted values."""
+        assert (
+            self._pred is not None and self._target is not None
+        ), "Must call loss function first."
+        fun_grad = grad(self.__call__)
+        gradient = fun_grad(self._pred, self._target)
+        gradient[self._pred - self._target == 0] = 0
+        return gradient
 
 
 class HingeLoss(Loss):
-    """c"""
+    """Compute the Hinge Loss."""
 
     def __init__(self):
         super().__init__()
@@ -43,15 +49,6 @@ class HingeLoss(Loss):
         self._target = target
         loss = np.clip(1 - pred * target, a_min=0.0, a_max=None)
         return np.mean(loss)
-
-    def gradient(self):
-        """Calculate the gradient of the hinge loss to the predicted values."""
-        assert (
-            self._pred is not None and self._target is not None
-        ), "Must call loss function first."
-        fun_grad = grad(self.__call__)
-        gradient = fun_grad(self._pred, self._target)
-        return gradient
 
 
 class MSELoss(Loss):
@@ -75,11 +72,27 @@ class MSELoss(Loss):
         loss = (pred - target) ** 2
         return np.mean(loss)
 
-    def gradient(self):
-        """Calculate the gradient of the hinge loss to the predicted values."""
-        assert (
-            self._pred is not None and self._target is not None
-        ), "Must call loss function first."
-        fun_grad = grad(self.__call__)
-        gradient = fun_grad(self._pred, self._target)
-        return gradient
+
+class BCELoss(Loss):
+    """Compute the Binary Cross Entropy Loss.
+    
+    **Note that the target y should be numbers between 0 and 1.**
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, pred, target):
+        """Call the Binary Cross Entropy loss.
+
+        Args:
+            pred (np.ndarry): The predicted values.
+            target (np.ndarry): The ground truth.
+
+        Returns:
+            np.float: The BCE loss.
+        """
+        self._pred = pred
+        self._target = target
+        loss = -target * np.log(pred + 1e-12) - (1 - target) * np.log(1 - pred + 1e-12)
+        return np.mean(loss)
