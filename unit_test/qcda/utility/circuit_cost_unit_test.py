ad2a23919ad7df5ad14f62db868ee3e0c23a8fdd
Li Longcheng <lilongcheng18@mails.ucas.ac.cn>
2023-07-25 21:02:54 +0800

    add circuit_cost_unit_test

---
+++ unit_test/qcda/utility/circuit_cost_unit_test.py
@@ -0,0 +1,16 @@
+from QuICT.core import Circuit
+from QuICT.core.gate import *
+from QuICT.qcda.utility.circuit_cost import FidelityCircuitCost
+from QuICT.core.virtual_machine.quantum_machine import OriginalKFC6130
+
+
+def test_fidelity_circuit_cost():
+    n_qubit = 4
+    n_gate = 10
+    circ = Circuit(n_qubit)
+    circ.random_append(n_gate, typelist=[GateType.cz, GateType.u3])
+    cost = FidelityCircuitCost(backend=OriginalKFC6130)
+    print(cost.evaluate(circ))
+
+
+
