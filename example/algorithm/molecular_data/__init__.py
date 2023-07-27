6dd444a61a8d984e2157c53691e83d2ea12eba2f
Ziying <guoziying@ict.ac.cn>
2023-07-27 14:36:51 +0800

    remove VQE

--- example/algorithm/molecular_data/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Molecular data relating to the coefficients
-"""
-
-from .moleculardata import MolecularData
-from .hamiltonian import obi_basis_rotation, tbi_basis_rotation, generate_hamiltonian
