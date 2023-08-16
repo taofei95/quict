3fa354735587d39177e15df17ecbb254c4991244
kanggeyinxin <tianyi2020@bupt.edu.cn>
2023-08-16 14:43:37 +0800

    add some information about adders

--- QuICT/algorithm/arithmetic/adder/hl_adder.py
+++ QuICT/algorithm/arithmetic/adder/hl_adder.py
@@ -2,7 +2,7 @@ from QuICT.core.gate import H, CX, T, T_dagger, CompositeGate
 
 
 class PG1(CompositeGate):
-    """ Implement a PG1 Gate. """
+    """ Implement a PG1 Gate which is a component of the following adders. """
 
     def __init__(self, name: str = None):
         """
@@ -31,7 +31,7 @@ class PG1(CompositeGate):
 
 
 class TR2(CompositeGate):
-    """ Implement a TR2 Gate. """
+    """ Implement a TR2 Gate which is a component of the following adders. """
 
     def __init__(self, name: str = None):
         """
@@ -59,20 +59,37 @@ class TR2(CompositeGate):
         H | self(0)
 
 
-class ADD(CompositeGate):
+class HLCarryAdder(CompositeGate):
     """
-        Implement a Quantum Adder Gates.
-        The first qubit must be set to |0> to get the right carry qubit in the result.
-        If the first qubit is |1>, the carry qubit in the result will be reversed.
+        Implement a quantum-quantum adder circuits using in total "2n + 1" qubits.
+
+        Input:
+            The first qubit must be set to |0> to get the right carry qubit in the result.
+            If the first qubit is |1>, the carry qubit in the result will be reversed.
+            The following "n" qubits are the first addend and
+            the last "n" qubits are the second addend.
+
+        Output:
+            The first "n + 1" qubits are the sum in which the first qubit is the carry qubit.
+            The last "n" qubits are reserved for the second addend.
+
+        Based on papers "Design of Efficient Reversible Logic-Based Binary and BCD Adder Circuits"
+        by Himanshu Thapliyal and Nagarajan Ranganayhan: https://arxiv.org/pdf/1712.02630.pdf
+        and "Efficient quantum arithmetic operation circuits for quantum image processing"
+        by Hai-Sheng Li, Ping Fan, Haiying Xia, Huiling Peng and Gui-Lu Long:
+        https://doi.org/10.1007/s11433-020-1582-8
     """
 
     def __init__(self, figures: int, name: str = None):
         """
-        Args:
-            figures (int): The qubits' figures in addend.
-            name (str, optional): The name of ADD gates. Defaults to None.
+            Construct the adder circuit that compute the sum of two quantum registers
+            of size "figures".
+
+            Args:
+                figures (int): The qubits' figures in addend.
+                name (str, optional): The name of HLCarryAdder gates. Defaults to None.
         """
-        assert figures >= 1, "ADD Gate need at least one figures in addend."
+        assert figures >= 1, "HLCarryAdder Gate need at least one figures in addend."
         super().__init__(name)
         self.add_build(figures)
 
@@ -97,16 +114,36 @@ class ADD(CompositeGate):
         CX | self([1 + figures, 1])
 
 
-class MADD(CompositeGate):
-    """ Implement a Quantum Modular Adder Gates. """
+class HLModAdder(CompositeGate):
+    """
+        Implement a quantum-quantum modular adder circuits Gates using in total "2n" qubits.
+
+        Input:
+            The first "n" qubits are the first addend.
+            The last "n" qubits are the second addend.
+        Output:
+            The first "n" qubits are the sum.
+            The last "n" qubits are reserved for the second addend.
+
+        Based on paper "Efficient quantum arithmetic operation circuits for quantum image processing"
+        by Hai-Sheng Li, Ping Fan, Haiying Xia, Huiling Peng and Gui-Lu Long:
+        https://doi.org/10.1007/s11433-020-1582-8
+    """
 
     def __init__(self, figures: int, name: str = None):
         """
-        Args:
-            figures (int): The qubits' figures in addend.
-            name (str, optional): The name of MADD gates. Defaults to None.
+            Construct the modular adder circuit that does:
+
+            s = (a + b) mod 2^n
+
+            a and b are two number stored in quantum register of size 'figures'
+            n = 'figures'
+
+            Args:
+                figures (int): The qubits' figures in addend.
+                name (str, optional): The name of HLModAdder gates. Defaults to None.
         """
-        assert figures >= 1, "MADD Gate need at least one figures in addend."
+        assert figures >= 1, "HLModAdder Gate need at least one figures in addend."
         super().__init__(name)
         self.madd_build(figures)
 
