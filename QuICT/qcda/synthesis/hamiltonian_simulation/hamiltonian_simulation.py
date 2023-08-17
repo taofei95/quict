35f51510101b262a5b8b9d772ade1d696ffe86c7
zhangpan <13110712+pzzzhang@user.noreply.gitee.com>
2023-08-17 06:14:51 +0000

    add docstring, fix style
    Signed-off-by: zhangpan <13110712+pzzzhang@user.noreply.gitee.com>

--- QuICT/qcda/synthesis/hamiltonian_simulation/hamiltonian_simulation.py
+++ QuICT/qcda/synthesis/hamiltonian_simulation/hamiltonian_simulation.py
@@ -1,52 +1,48 @@
-from unitary_matrix_encoding import permute_bit_string,check_hamiltonian, UnitaryMatrixEncoding, product_gates, padding_coefficient_array
+import math
+from unitary_matrix_encoding import permute_bit_string, check_hamiltonian, UnitaryMatrixEncoding, product_gates, padding_coefficient_array
 from quict_polynomial import Poly
 from QuICT.qcda.synthesis import QuantumStatePreparation
 from QuICT.core import Circuit
 from QuICT.core.gate import *
 import numpy as np
-import itertools
-
 from quantum_signal_processing import QuantumSignalProcessing, SignalAngleFinder
+
+
 def prepare_eigenvector_gate(input_eigenvector):
+    """
+    Given 2*n size state vector, find gates act on [1,0....., 0{n}] such that produce this vectir
+    :param input_eigenvector: array of 1-D eigenvector
+    :return: list of corresponded state preparation gates
+    """
     gates_list = []
     for i in range(len(input_eigenvector)):
         qsp = QuantumStatePreparation('uniformly_gates')
-        gates.append(qsp.execute(input_eigenvector[i]))
+        gates_list.append(qsp.execute(input_eigenvector[i]))
     return gates_list
-def control_z(binary_string):
+
+
+def int_reflection(binary_string):
+    """
+    In the standard basis, reflect the matrix elements |binary_string><binary_string|
+    generate control z composite gates.
+
+    :param binary_string: input binary string.
+    :return: Composite gate
+    """
     m = len(binary_string)
     composite_gate = CompositeGate()
-    if m==1:
-        if binary_string=="0":
-            X|composite_gate(0)
-        Z|composite_gate(0)
-        if binary_string=="0":
-            X|composite_gate(0)
-        return composite_gate
-    if m == 2:
-        for i in range(len(binary_string)):
-            if binary_string[i]=="0":
-                X|composite_gate(i)
-        CZ|composite_gate([0,1])
-        for i in range(len(binary_string)):
-            if binary_string[i]=="0":
-                X|composite_gate(i)
-
-
-        return composite_gate
+    x_composite_gate = CompositeGate()
+    for i in range(len(binary_string)):
+        if binary_string[i] == "0":
+            X | x_composite_gate(i)
+    x_composite_gate | composite_gate
+    if m == 1:
+        Z | composite_gate(0)
+    elif m == 2:
+        CZ | composite_gate([0, 1])
     elif m == 3:
-        for i in range(len(binary_string)):
-            if binary_string[i]=="0":
-                X|composite_gate(i)
-        CCZ|composite_gate([0,1,2])
-        for i in range(len(binary_string)):
-            if binary_string[i]=="0":
-                X|composite_gate(i)
-        return composite_gate
+        CCZ | composite_gate([0, 1, 2])
     elif m > 3:
-        for i in range(m):
-            if int(binary_string[i]) == 0:
-                X | composite_gate(i)
 
         my_ccx = CCX & [0, 1, m]
         my_ccx | composite_gate
@@ -63,45 +59,80 @@ def control_z(binary_string):
             my_ccx | composite_gate
         my_ccx = CCX & [0, 1, m]
         my_ccx | composite_gate
+    x_composite_gate | composite_gate
 
-        for i in range(m):
-            if int(binary_string[i]) == 0:
-                X | composite_gate(i)
-        return composite_gate
+    return composite_gate
 ##############################################################################################
-#Following are function for truncation taylor expansion algorithm
+# Following are function for truncation taylor expansion algorithm
+
 
 def gates_B(coefficient_array):
+    """
+    The B gate of equation 8
+    """
     new_coefficient_array = padding_coefficient_array(coefficient_array)
     summed_coefficient = np.sum(new_coefficient_array)
     normalized_vector = np.sqrt(new_coefficient_array/summed_coefficient)
     B = prepare_eigenvector_gate(normalized_vector)
     B_dagger = B.inverse()
     return B, B_dagger
-def gates_R(num_qubit):
-    bit_string_array, _ = permute_bit_string(2**num_qubit-1)
 
+
+def gates_R(num_qubit):
+    """
+    generate R = I - 2P gate
+    """
+    bit_string_array, _ = permute_bit_string(2**num_qubit-1)
+    print(bit_string_array)
     R = CompositeGate()
     for binary_string in bit_string_array:
-        print(binary_string)
-        reflection_gate = control_z(binary_string)
+        reflection_gate = int_reflection(binary_string)
         reflection_gate | R
     return R
 
 
+def find_order(times_steps, error):
+    """
+    Find the minimum order makes the |summed_coefficient-2|<=2
+    :param times_steps: int
+                        num of time steps
+    :param error: float
+                    accuracy of the algorithm
+    :return: int
+            minimum order of taylor expansion
+    """
+    order = 0
+    range_min = 2-error/times_steps
+    range_max = 2+error/times_steps
+    s = 1/math.factorial(order)*np.log(2)**order
 
+    while not (range_min < s and s < range_max):
+        order += 1
+        s += 1/math.factorial(order)*np.log(2)**order
 
-
-
-
+    return order
 
 
 class HamiltonianSimulation():
-    def __init__(self, method):
-        self.method = method
-        assert (method!="QSP" or method!="TS"), "Method must be quantum_signal_processing or truncation."
-    def execute(self,time, coefficient_array, matrix_array):
+    def __init__(self):
+        pass
 
+    def TS_method(self, coefficient_array, matrix_array, time, times_steps, error: float = 0.001, max_order=20):
+        """
+        https://arxiv.org/abs/1412.4687
+        Let hamiltonian satisfy:
+        1. H = summed_{L}_{l=1}(coefficient_{l}*Unitary_{l})
+        Compute truncation taylor series hamiltonian simulation algorithm
+        :param coefficient_array: Array of coefficient in equation 1.
+
+        :param matrix_array: Array of unitary matrix in equation 1.
+        :param time: float
+
+        :param times_steps: int
+        :param error: float, Algorithm accuracy
+        :param max_order: Maximum degree of taylor expansion allowed.
+        :return: Quantum circuit
+        """
         (hamiltonian,
          coefficient_array,
          matrix_array,
@@ -110,110 +141,50 @@ class HamiltonianSimulation():
         matrix_dimension = 0
         while 2 ** matrix_dimension != len(matrix_array[0][0]):
             matrix_dimension += 1
+        # calculate order based on the error given
+        order = find_order(times_steps, error)
+        assert order < max_order, f"The max order exceed {max_order}. Adjust max allowed taylor expansion order."
+        # Algorithm
+        coefficient_array, hamilton_list = product_gates(coefficient_array, matrix_array, order, time,
+                                                         1)
+        bit_string_array, num_ancilla_qubit = permute_bit_string(
+            len(coefficient_array) - 1)
+        # make B gates, select V gates, ancilla reflection gates
+        UME = UnitaryMatrixEncoding()
+        matrix_array = []
+        matrix_array.append(hamilton_list[0])
+        for i in range(1, len(hamilton_list)):
+            temp_matrix = hamilton_list[i][0]
+            for j in range(1, len(hamilton_list[i])):
+                temp_matrix = np.matmul(temp_matrix, hamilton_list[i][j])
+            matrix_array.append(temp_matrix)
+        matrix_array = np.array(matrix_array)
+        B, B_dagger, select_v = UME.execute(coefficient_array, matrix_array)
+        R = gates_R(num_ancilla_qubit)
 
-        length = len(coefficient_array)
-###########################################step in algorithm
-        if self.method == "TS":
+        circuit = Circuit(num_ancilla_qubit + matrix_dimension)
+        # W
+        B | circuit
+        select_v | circuit
+        B_dagger | circuit
 
+        # R
+        R | circuit
 
-            order = 4
+        # W dagger
 
+        B | circuit
+        select_v.inverse() | circuit
+        B_dagger | circuit
+        # R
+        R | circuit
+        # W
+        B | circuit
+        select_v | circuit
+        B_dagger | circuit
+        # -1
 
-            coefficient_array, hamilton_list = product_gates(coefficient_array, matrix_array, order, time,
-                                                                 1)
-            bit_string_array, num_ancilla_qubit = permute_bit_string(len(coefficient_array)-1)
-        #make B gates, select V gates, ancilla reflection gates
-            UME = UnitaryMatrixEncoding()
-            matrix_array = []
-            matrix_array.append(hamilton_list[0])
-            for i in range(1, len(hamilton_list)):
-                temp_matrix = hamilton_list[i][0]
-                for j in range(1, len(hamilton_list[i])):
-
-                    temp_matrix = np.matmul(temp_matrix,hamilton_list[i][j])
-                matrix_array.append(temp_matrix)
-            matrix_array = np.array(matrix_array)
-            B, B_dagger, select_v = UME.execute(coefficient_array, matrix_array)
-            R = gates_R(num_ancilla_qubit)
-
-            circuit = Circuit(num_ancilla_qubit+matrix_dimension)
-            #W
-            B|circuit
-            select_v | circuit
-            B_dagger | circuit
-
-            #R
-            R | circuit
-
-            #W dagger
-
-            B|circuit
-            select_v.inverse() | circuit
-            B_dagger|circuit
-            #R
-            R |circuit
-            #W
-            B|circuit
-            select_v | circuit
-            B_dagger | circuit
-            #-1
-
-            whole_circuit_reflection = gates_R(num_ancilla_qubit+matrix_dimension-1)
-            whole_circuit_reflection | circuit
-            return circuit
-
-
-
-
-        if self.method=="QSP":
-
-            num_ancilla_qubit = 0
-            while 2 ** num_ancilla_qubit != length:
-                num_ancilla_qubit += 1
-
-            eigenvalues, eigenvector = np.linalg.eig(hamiltonian)
-
-            gates_list = prepare_eigenvector_gate(eigenvector)
-
-            #signal processing
-            #find poly
-            exp_normal_basis = Poly()
-            P = exp_normal_basis.normal_basis(time, 30)
-            #find angle
-            saf = SignalAngleFinder()
-            angle_array = saf.execute(P, 30)
-            #find circuit
-            signal_circuit_list = []
-            qsp = QuantumSignalProcessing(angle_array)
-            for eigenvalue in range(eigenvalues):
-                signal_circuit = qsp.signal_circuit(eigenvalue)
-                signal_circuit_list.append(signal_circuit)
-            #################################################################
-            #quantum signal processing circuit
-            circuit = Circuit()
-
-            UME =  UnitaryMatrixEncoding()
-            G, G_dagger,unitary_encoding=UME.execute(coefficient_array, matrix_array)
-            G|circuit(0)
-            unitary_encoding | circuit(0)
-            H|circuit(num_ancilla_qubit+matrix_dimension)
-
-            mct = MultiControlToffoli('no_aux')
-            m_c_t = mct(matrix_dimension)
-
-            for i in range(len(gates_list)):
-                eigenvalue_gates = gates_list[i]
-                eigenvalue_gates_dagger = eigenvalue_gates.inverse()
-                eigenvalue_gates_dagger|circuit([i+num_ancilla_qubit for i in range(matrix_dimension)])
-                for j in range(matrix_dimension):
-                    X|circuit(num_ancilla_qubit+j)
-                m_c_t | circuit([i+num_ancilla_qubit for i in range(matrix_dimension+1)])
-                signal_circuit_list[i]|circuit(num_ancilla_qubit+matrix_dimension)
-                m_c_t|  circuit([i+num_ancilla_qubit for i in range(matrix_dimension+1)])
-                for j in range(matrix_dimension):
-                    X|circuit(num_ancilla_qubit+j)
-                eigenvalue_gates | circuit([i+num_ancilla_qubit for i in range(matrix_dimension)])
-            G_dagger | circuit(0)
-            unitary_encoding.inverse() | circuit(0)
-
-            return circuit
+        whole_circuit_reflection = gates_R(
+            num_ancilla_qubit + matrix_dimension)
+        whole_circuit_reflection | circuit
+        return circuit
