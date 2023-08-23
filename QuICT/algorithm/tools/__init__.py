c0636b855826946c7ce4ff7b3621d14ea10e2786
xin-0 <xintong2@illinois.edu>
2023-08-23 10:57:12 +0800

    circuit decompose by level tool

--- QuICT/algorithm/tools/__init__.py
+++ QuICT/algorithm/tools/__init__.py
@@ -1,2 +1,2 @@
-from .pre_circuit import circuit_init
+from .pre_circuit import circuit_init, decompose
 from .post_circuit import decode_counts, decode_counts_int
--- QuICT/algorithm/tools/pre_circuit.py
+++ QuICT/algorithm/tools/pre_circuit.py
@@ -1,16 +1,17 @@
-from typing import List
+from typing import List, Union
 from numpy import binary_repr
 
 from QuICT.core import Circuit
+from QuICT.core.gate import BasicGate, CompositeGate
 from QuICT.core.gate import X
 
 
-def circuit_init(qcircuit: Circuit, qreg_list: List[int], init_val: int):
+def circuit_init(circuit: Circuit, qreg_list: List[int], init_val: int):
     """
         Init circuit with a classically given vale using basic/computational encoding in big edian convention.
 
         Args:
-            qcircuit (Circuit): circuit to be initialized.
+            circuit (Circuit): circuit to be initialized.
 
             qreg_list (List[int]): the list representing the quantum register to be initialized,
             from the most to the least siginificant bit.
@@ -25,4 +26,59 @@ def circuit_init(qcircuit: Circuit, qreg_list: List[int], init_val: int):
 
     for i, bit in enumerate(init_bin):
         if '1' == bit:
-            X | qcircuit([qreg_list[i]])
+            X | circuit([qreg_list[i]])
+
+
+def decompose(circuit: Circuit, level: int = -1) -> Circuit:
+    """
+        Decompose composite gate in a circuit.
+
+        Args:
+            circuit (Circuit): circuit to be decomposed.
+
+            level (int): decomposition level. Negative number for complete
+            decomposition. Default to be -1.
+
+        Returns:
+            Circuit: The decomposed circuit.
+    """
+    new_circ = Circuit(circuit.width())
+
+    for gate, qidxes, _ in decompose_gate_list(circuit._gates, level):
+        gate | new_circ(qidxes)
+
+    return new_circ
+
+
+def decompose_gate_list(
+    gate_list: List[Union[BasicGate, CompositeGate]],
+    level: int = -1
+) -> List[Union[BasicGate, CompositeGate]]:
+    """
+        Decompose composite gate in a list containing basic gates and composite gates.
+
+        Args:
+            gate_list (List[Union[BasicGate, CompositeGate]]): the list of gate to be
+            decomposed.
+
+            level (int): decomposition level. Negative number for complete
+            decomposition. Default to be -1.
+
+        Returns:
+            List[Union[BasicGate, CompositeGate]]: The decomposed gate list.
+    """
+    decomp_gates = []
+    for gate, qidxes, size in gate_list:
+        if size > 1 or hasattr(gate, "gate_decomposition"):
+            if level < 0:
+                temp_gate = gate.copy() & qidxes
+                decomp_gates += decompose_gate_list(temp_gate._gates)
+            elif level == 0:
+                decomp_gates.append((gate, qidxes, size))
+            else:
+                temp_gate = gate.copy() & qidxes
+                decomp_gates += decompose_gate_list(temp_gate._gates, level - 1)
+        else:
+            decomp_gates.append((gate, qidxes, size))
+
+    return decomp_gates
