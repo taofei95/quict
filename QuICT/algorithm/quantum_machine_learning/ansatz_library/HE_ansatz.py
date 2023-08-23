91368d515cbbad506c406d07801f6e6f0357c4e8
Ziying Guo <guoziying@ict.ac.cn>
2023-08-23 17:29:11 +0800

    update HE-ansatz

--- QuICT/algorithm/quantum_machine_learning/ansatz_library/HE_ansatz.py
+++ QuICT/algorithm/quantum_machine_learning/ansatz_library/HE_ansatz.py
@@ -20,25 +20,35 @@ class HEAnsatz(Ansatz):
     def readout(self):
         return self._readout
 
-    def __init__(self, n_qubits: int, d: int, layers: list, readout: list = None):
+    def __init__(
+        self,
+        n_qubits: int,
+        d: int,
+        layers: list,
+        entangler: str = "downstair",
+        readout: list = None,
+    ):
         """Initialize an HE-ansatz instance.
 
         Args:
             n_qubits (int): The number of qubits.
             d (int): The depth of HE-ansatz.
             layers (list): The list of layers. Supported layers are "CX", "CZ", "CRy", "RY", "RZ".
+            entangler (str): The type of entangler. Supported types are "downstair" and "full".Defaults to "downstair".
             readout (list, optional): The readout qubits. Defaults to None.
         """
         super(HEAnsatz, self).__init__(n_qubits)
         self._d = d
         self._layers = layers
-        self._param_layers = 0
+        assert entangler in ["downstair", "full"]
+        self._entangler = entangler
+        self._n_params = 0
         self._readout = [0] if readout is None else readout
         self._validate_layers()
 
     def __str__(self):
-        return "HEAnsatz(n_qubits={}, d={}, layers={})".format(
-            self._n_qubits, self._d, self._layers
+        return "HEAnsatz(n_qubits={}, d={}, layers={}, entangler={})".format(
+            self._n_qubits, self._d, self._layers, self._entangler
         )
 
     def init_circuit(self, params: Union[Variable, np.ndarray] = None):
@@ -50,47 +60,67 @@ class HEAnsatz(Ansatz):
         Returns:
             Circuit: The HE-ansatz ansatz.
         """
-        params = (
-            np.random.randn(self._d, self._param_layers, self._n_qubits)
-            if params is None
-            else params
-        )
+        params = np.random.randn(self._d, self._n_params) if params is None else params
         params = Variable(pargs=params) if isinstance(params, np.ndarray) else params
-        if params.shape == (self._d, self._param_layers, self._n_qubits):
+        if params.shape == (self._d, self._n_params):
             self._params = params
         else:
-            raise AnsatzShapeError(
-                str(self._d, self._param_layers, self._n_qubits), str(params.shape)
-            )
+            raise AnsatzShapeError(str(self._d, self._n_params), str(params.shape))
 
         gate_dict = {"CX": CX, "CZ": CZ, "CRy": CRy, "RY": Ry, "RZ": Rz}
         circuit = Circuit(self._n_qubits)
         for i in range(self._d):
-            param_layer = 0
+            param_id = 0
             for gate in self._layers:
                 if gate in ["RY", "RZ"]:
                     for qid in range(self._n_qubits):
-                        gate_dict[gate](params[i][param_layer][qid]) | circuit(qid)
-                    param_layer += 1
+                        gate_dict[gate](params[i][param_id]) | circuit(qid)
+                        param_id += 1
                 elif gate in ["CRy"]:
-                    for qid in range(self._n_qubits - 1):
-                        gate_dict[gate](params[i][param_layer][qid]) | circuit(
-                            [qid, qid + 1]
+                    if self._entangler == "downstair":
+                        for qid in range(self._n_qubits - 1):
+                            gate_dict[gate](params[i][param_id]) | circuit(
+                                [qid, qid + 1]
+                            )
+                            param_id += 1
+                        gate_dict[gate](params[i][param_id]) | circuit(
+                            [self._n_qubits - 1, 0]
                         )
-                    gate_dict[gate](
-                        params[i][param_layer][self._n_qubits - 1]
-                    ) | circuit([self._n_qubits - 1, 0])
-                    param_layer += 1
+                        param_id += 1
+                    else:
+                        for qid1 in range(self._n_qubits - 1):
+                            invert = False
+                            for qid2 in range(qid1 + 1, self._n_qubits):
+                                qubit_indexes = [qid2, qid1] if invert else [qid1, qid2]
+                                gate_dict[gate](params[i][param_id]) | circuit(
+                                    qubit_indexes
+                                )
+                                invert = not invert
+                                param_id += 1
                 else:
-                    for qid in range(self._n_qubits - 1):
-                        gate_dict[gate] | circuit([qid, qid + 1])
-                    gate_dict[gate] | circuit([self._n_qubits - 1, 0])
+                    if self._entangler == "downstair":
+                        for qid in range(self._n_qubits - 1):
+                            gate_dict[gate] | circuit([qid, qid + 1])
+                        gate_dict[gate] | circuit([self._n_qubits - 1, 0])
+                    else:
+                        for qid1 in range(self._n_qubits - 1):
+                            invert = False
+                            for qid2 in range(qid1 + 1, self._n_qubits):
+                                qubit_indexes = [qid2, qid1] if invert else [qid1, qid2]
+                                gate_dict[gate] | circuit(qubit_indexes)
+                                invert = not invert
 
         return circuit
 
     def _validate_layers(self):
         for layer in self._layers:
-            if layer in ["RY", "RZ", "CRy"]:
-                self._param_layers += 1
+            if layer in ["RY", "RZ"]:
+                self._n_params += self._n_qubits
+            elif layer in ["CRy"]:
+                self._n_params += (
+                    self._n_qubits
+                    if self._entangler == "downstair"
+                    else int(self._n_qubits * (self._n_qubits - 1) / 2)
+                )
             elif layer not in ["CX", "CZ"]:
                 raise AnsatzValueError('["RY", "RZ", "CRy", "CX", "CZ"]', layer)
