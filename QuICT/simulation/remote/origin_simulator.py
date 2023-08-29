0d3122fe0be044a8223456aebd56391861dcab34
wangrui <wangrui>
2023-08-29 14:34:08 +0800

    add the choose of chip_id for origin simulator

--- QuICT/simulation/remote/origin_simulator.py
+++ QuICT/simulation/remote/origin_simulator.py
@@ -15,16 +15,16 @@ class OriginSimulator:
         # Initialize
         self.qm.init_qvm(token)
 
-    def run(self, circuit: str, shots: int = 1000):
+    def run(self, circuit: str, shots: int = 1000, chip_id: int = 2):
         prog, _, _ = convert_qasm_to_qprog(circuit, self.qm)
 
+        assert chip_id in list(range(1, 4))
         # Call quantum chip
-        result = self.qm.real_chip_measure(prog, shots)
-
+        result = self.qm.real_chip_measure(prog, shots, chip_id)
         self.qm.finalize()
-        quafu_amp = [0] * (2 ** circuit.width())
+        origin_amp = [0] * (2 ** circuit.width())
         for key, value in result.items():
-            quafu_amp[int(key, 2)] = value
-        amp_result = np.array(quafu_amp)
+            origin_amp[int(key, 2)] = value
+        amp_result = np.array(origin_amp)
 
         return amp_result
