235f318257a25a918a108ae181f029f805a281af
zhangpan <13110712+pzzzhang@user.noreply.gitee.com>
2023-08-29 03:20:13 +0000

    update QuICT/qcda/synthesis/hamiltonian_simulation/__init__.py.
    Signed-off-by: zhangpan <13110712+pzzzhang@user.noreply.gitee.com>

--- QuICT/qcda/synthesis/hamiltonian_simulation/__init__.py
+++ QuICT/qcda/synthesis/hamiltonian_simulation/__init__.py
@@ -1,3 +1,5 @@
 from .hamiltonian_simulation import HamiltonianSimulation
 from .polynomial import Poly
 from .unitary_matrix_encoding import UnitaryMatrixEncoding
+from .TS_method import *
+from .Trotter import *
