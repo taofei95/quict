251b919c2089cd08a3296307f189c2c91e04ec05
Ziying Guo <guoziying@ict.ac.cn>
2023-09-18 17:11:20 +0800

    del simulator

--- QuICT/simulation/multi_nodes/__init__.py
+++
@@ -1,5 +0,0 @@
-try:
-    from QuICT_sim.multi_nodes import MultiNodesController
-except:
-    MultiNodesController = None
-    print("Please install quict_sim first, you can use 'pip install quict_sim' to install. ")
