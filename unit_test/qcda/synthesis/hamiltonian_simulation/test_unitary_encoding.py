74f8c218652104207033ea68044e5078042e1c4f
zhangpan <13110712+pzzzhang@user.noreply.gitee.com>
2023-09-18 07:04:37 +0000

    update unit_test/qcda/synthesis/hamiltonian_simulation/test_unitary_encoding.py.
    Signed-off-by: zhangpan <13110712+pzzzhang@user.noreply.gitee.com>

--- unit_test/qcda/synthesis/hamiltonian_simulation/test_unitary_encoding.py
+++ unit_test/qcda/synthesis/hamiltonian_simulation/test_unitary_encoding.py
@@ -1,8 +1,10 @@
 from QuICT.qcda.synthesis.hamiltonian_simulation.unitary_matrix_encoding import UnitaryMatrixEncoding
 import numpy as np
 from QuICT.core.gate import X,Y,Z
+
+
 def test_unitary_matrix_encoding_lcu():
-    coefficient_array = np.array([0.3,0.2])
+    coefficient_array = np.array([0.3, 0.2])
     matrix_array = np.array([X.matrix, Y.matrix])
     UME = UnitaryMatrixEncoding("LCU")
     circuit = UME.execute(coefficient_array, matrix_array, complete=True, phase_gate=True)
@@ -10,13 +12,15 @@ def test_unitary_matrix_encoding_lcu():
     circuit_matrix = circuit.matrix()
     G_matrix = G.matrix()
     unitary_encoding_matrix = unitary_encoding.matrix()
-    expected_B_matrix = np.array([[0.77459667+0.j, -0.63245553+0.j], [ 0.63245553+0.j, 0.77459667+0.j]])
-    expected_control_v_matrix = np.array([[0,1,0,0],[1,0,0,0],[0,0,0,-1j],[0,0,1j,0]])
+    expected_B_matrix = np.array([[0.77459667 + 0.j, -0.63245553 + 0.j], [ 0.63245553 + 0.j, 0.77459667 + 0.j]])
+    expected_control_v_matrix = np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, -1j], [0, 0, 1j, 0]])
     assert np.allclose(G_matrix[:,0], np.array([np.sqrt(0.3/0.5), np.sqrt(0.2/0.5)])), "The G gate isn't correct."
     assert np.allclose(unitary_encoding_matrix, expected_control_v_matrix), "The control-V gate isn't correct"
     expected_B_matrix = np.kron(expected_B_matrix, np.identity(2))
     expected_matrix = np.matmul(np.matmul(np.linalg.inv(expected_B_matrix), expected_control_v_matrix), expected_B_matrix)
     assert np.allclose(expected_matrix, circuit_matrix), "circuit matrix isn't correct"
+
+
 def test_unitary_matrix_encoding_conj():
     coefficient_array = np.array([0.3,0.2])
     matrix_array = np.array([X.matrix, Z.matrix])
