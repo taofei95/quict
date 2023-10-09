55a4c6106b4939ac8cfc077c1abae3d6da4562ae
GuoZiying <guozy96@gmail.com>
2023-10-09 10:46:01 +0800

    add comments for differentiator

--- QuICT/algorithm/quantum_machine_learning/differentiator/differentiator.py
+++ QuICT/algorithm/quantum_machine_learning/differentiator/differentiator.py
@@ -1,13 +1,55 @@
 import numpy as np
 
-from QuICT.algorithm.quantum_machine_learning.differentiator.adjoint import AdjointDifferentiator
+from QuICT.algorithm.quantum_machine_learning.differentiator.adjoint import (
+    AdjointDifferentiator,
+)
 from QuICT.algorithm.quantum_machine_learning.utils import Hamiltonian
 from QuICT.core import Circuit
 from QuICT.core.gate import *
 
 
 class Differentiator:
-    """ The high-level differentiator class, including all QuICT differentiator mode. """
+    """The high-level differentiator class, including all QuICT differentiator mode.
+
+    Args:
+        device (str, optional): The device of the simulator. One of [CPU, GPU]. Defaults to "CPU".
+        backend (str, optional): The backend for the simulator. Defaults to "adjoint".
+        precision (str, optional): The precision of simulator, one of ["single", "double"]. Defaults to "double".
+        **options (dict): other optional parameters for the simulator.
+            adjoint: [gpu_device_id] (only for gpu)
+
+    Example:
+        >>> from QuICT.core.gate import *
+        >>> from QuICT.core.circuit import Circuit
+        >>> from QuICT.simulation.state_vector import StateVectorSimulator
+        >>> from QuICT.algorithm.quantum_machine_learning.utils import Hamiltonian
+        >>> from QuICT.algorithm.quantum_machine_learning.differentiator import Differentiator
+
+        >>> simulator = StateVectorSimulator()
+        >>> differentiator = Differentiator()
+        >>> var = Variable(np.array([0.3, -1.2, 2]))
+        >>> cir = Circuit(3)
+        >>> H | cir
+        >>> Rz(var[0]) | cir(1)
+        >>> CX | cir([0, 1])
+        >>> Rxx(-1.5 * var[0]) | cir([1, 2])
+        >>> Ry(var[2]) | cir(0)
+        >>> cir.draw("command")
+                ┌───┐                    ┌───────┐
+        q_0: |0>┤ h ├─────────────■──────┤ ry(2) ├───
+                ├───┤┌─────────┐┌─┴──┐┌──┴───────┴──┐
+        q_1: |0>┤ h ├┤ rz(0.3) ├┤ cx ├┤0            ├
+                ├───┤└─────────┘└────┘│  rxx(-0.45) │
+        q_2: |0>┤ h ├─────────────────┤1            ├
+                └───┘                 └─────────────┘
+        >>> sv = simulator.run(cir)
+        >>> h = Hamiltonian([[-1, "Z0"]])
+        >>> grads, exp = differentiator.run(cir, var, sv, h)
+        >>> grads
+        [[-0.26871576  0.         -0.39756026]]
+        >>> exp
+        [0.86868501]
+    """
 
     __DEVICE = ["CPU", "GPU"]
     __BACKEND = ["adjoint"]
@@ -19,20 +61,12 @@ class Differentiator:
 
     def __init__(
         self,
-        device: str = "GPU",
+        device: str = "CPU",
         backend: str = "adjoint",
         precision: str = "double",
         **options,
     ):
-        """Initialize a differentiator.
-
-        Args:
-            device (str, optional): The device of the simulator. One of [CPU, GPU]. Defaults to "GPU".
-            backend (str, optional): The backend for the simulator. One of ["adjoint"]. Defaults to "adjoint".
-            precision (str, optional): The precision of simulator, one of ["single", "double"]. Defaults to "double".
-            **options (dict): other optional parameters for the simulator.
-                adjoint: [gpu_device_id] (only for gpu)
-        """
+        """Initialize a differentiator intance."""
         assert device in Differentiator.__DEVICE, ValueError(
             "Differentiator.device", "[CPU, GPU]", device
         )
@@ -122,7 +156,10 @@ class Differentiator:
         )
 
     def get_expectations(
-        self, circuit: Circuit, state_vector: np.ndarray, expectation_ops: list,
+        self,
+        circuit: Circuit,
+        state_vector: np.ndarray,
+        expectation_ops: list,
     ):
         """Calculate the expectation of a PQC.
 
@@ -139,7 +176,10 @@ class Differentiator:
         )
 
     def get_expectations_batch(
-        self, circuit: Circuit, state_vector_list: list, expectation_ops: list,
+        self,
+        circuit: Circuit,
+        state_vector_list: list,
+        expectation_ops: list,
     ):
         """Calculate the expectations of a batch of PQCs.
 
